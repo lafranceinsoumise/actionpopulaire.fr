2a9b95ab896f6facea181d2c8c76bcd64d65cdb9
Arthur Cheysson <arthur@cheysson.fr>
2017-04-14 20:02:43 +0200

    Switch to Postgresql

--- src/fi_api/settings.py
+++ src/fi_api/settings.py
@@ -79,11 +79,14 @@ WSGI_APPLICATION = 'fi_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'api',
+        'USER': 'api',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': ''
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
