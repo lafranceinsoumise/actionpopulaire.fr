b50ffcd3a5abf39a08f48f7b67e8fe0cad654277
Arthur Cheysson <arthur@cheysson.fr>
2017-04-14 20:30:35 +0200

    Set up travis-ci

--- src/fi_api/settings.py
+++ src/fi_api/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -79,14 +80,7 @@ WSGI_APPLICATION = 'fi_api.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'api',
-        'USER': 'api',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-        'PORT': ''
-    }
+    'default': dj_database_url.config(default="postgresql://api:password@localhost/api")
 }
 
 # Password validation
