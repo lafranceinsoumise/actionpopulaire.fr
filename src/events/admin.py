1ba304f809fea3f08e3247b5ba1236c81933b35a
Arthur Cheysson <arthur@cheysson.fr>
2017-06-15 11:20:37 +0200

    Add proper event admin form

--- src/events/admin.py
+++ src/events/admin.py
@@ -1,12 +1,46 @@
 from django.contrib import admin
+from django import forms
+from django.utils.translation import ugettext_lazy as _
+from django.contrib.gis.forms import OSMWidget
 from api.admin import admin_site
 
 from . import models
 
 
+class EventForm(forms.ModelForm):
+    class Meta:
+        model = models.Event
+        fields = (
+            'name', 'description', 'start_time', 'end_time', 'calendar', 'tags', 'location_name', 'location_address1',
+            'location_address2', 'location_city', 'location_zip', 'location_state', 'location_country', 'coordinates',
+            'contact_name', 'contact_email', 'contact_phone'
+        )
+        widgets = {
+            'coordinates': OSMWidget
+        }
+
+
 @admin.register(models.Event, site=admin_site)
 class EventAdmin(admin.ModelAdmin):
-    pass
+    fieldsets = (
+        (None, {
+            'fields': ('id', 'name',)
+        }),
+        (_('Informations'), {
+            'fields': ('description', 'start_time', 'end_time', 'calendar', 'tags',)
+        }),
+        (_('Lieu'), {
+            'fields': ('location_name', 'location_address1', 'location_address2', 'location_city', 'location_zip',
+                       'location_state', 'location_country', 'coordinates')
+        }),
+        (_('Contact'), {
+            'fields': ('contact_name', 'contact_email', 'contact_phone')
+        }),
+    )
+
+    readonly_fields = ('id',)
+
+    form = EventForm
 
 
 @admin.register(models.Calendar, site=admin_site)
