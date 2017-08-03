df142ea64ef6a73efc78d9d25f962cc37ff968d0
Guillaume Royer <perso@guilro.com>
2017-08-03 09:43:50 +0200

    tests pour formulaires d'inscription (fix #8)

---
+++ src/front/tests.py
@@ -0,0 +1,26 @@
+from django.test import TestCase
+
+from people.models import Person
+
+
+class SimpleSubscriptionFormTestCase(TestCase):
+    def test_can_post(self):
+        response = self.client.post('/inscription/', {'email': 'example@example.com', 'location_zip': '75018'})
+
+        self.assertEqual(response.status_code, 302)
+        Person.objects.get_by_natural_key('example@example.com')
+
+
+class OverseasSubscriptionForm(TestCase):
+    def test_can_post(self):
+        response = self.client.post('/inscription/etranger/', {
+            'email': 'example@example.com',
+            'location_address1': '1 ZolaStraße',
+            'location_zip': '10178',
+            'location_city': 'Berlin',
+            'location_country': 'DE'
+        })
+
+        self.assertEqual(response.status_code, 302)
+        person = Person.objects.get_by_natural_key('example@example.com')
+        self.assertEqual(person.location_city, 'Berlin')
