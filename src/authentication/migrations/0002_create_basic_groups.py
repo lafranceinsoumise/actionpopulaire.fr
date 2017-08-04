13e65690ee78d3aa1e88cd5b42f1456d87422036
Arthur Cheysson <arthur@cheysson.fr>
2017-08-04 12:35:06 +0200

    Add missing dependencies to migration creating permission groups

--- src/authentication/migrations/0002_create_basic_groups.py
+++ src/authentication/migrations/0002_create_basic_groups.py
@@ -46,7 +46,11 @@ class Migration(migrations.Migration):
     dependencies = [
         ('authentication', '0001_initial'),
         # depends on auth models to be created
-        ('auth', '0008_alter_user_username_max_length')
+        ('auth', '0008_alter_user_username_max_length'),
+        # depends on needed permissions actually existing
+        ('events', '0001_initial'),
+        ('groups', '0001_initial'),
+        ('people', '0001_initial'),
     ]
 
     operations = [
