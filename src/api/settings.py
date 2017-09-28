7e8c6a8f0f4dbe565208c21292176a9a4766a498
Guillaume Royer <perso@guilro.com>
2017-09-28 11:13:07 +0200

    remove html serializer in production

--- src/api/settings.py
+++ src/api/settings.py
@@ -218,7 +218,7 @@ AUTHENTICATION_BACKENDS = (
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
-        'rest_framework.renderers.BrowsableAPIRenderer',
+        *(['rest_framework.renderers.BrowsableAPIRenderer'] if DEBUG else []),
     ),
     'DEFAULT_PARSER_CLASSES': (
         'rest_framework.parsers.JSONParser',
