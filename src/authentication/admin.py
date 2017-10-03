510bc980bd231178b9ad4f3d1fcf0d5691d7e40e
Guillaume Royer <perso@guilro.com>
2017-10-03 15:58:20 +0200

    [admin] easier search in role admin

--- src/authentication/admin.py
+++ src/authentication/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.utils.translation import ugettext_lazy as _
 from django.shortcuts import reverse
+from django.utils.html import escape
 from django.utils.decorators import method_decorator
 from django.views.decorators.debug import sensitive_post_parameters
 
@@ -15,18 +16,18 @@ sensitive_post_parameters_m = method_decorator(sensitive_post_parameters())
 @admin.register(models.Role, site=admin_site)
 class RoleAdmin(UserAdmin):
     fieldsets = (
-        (None, {'fields': ('password', 'last_login')}),
+        (None, {'fields': ('password', 'last_login', 'link')}),
         (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
                                        'groups', 'user_permissions')}),
     )
-    readonly_fields = ('last_login',)
+    readonly_fields = ('last_login', 'link')
 
 
     list_display = ('id', 'type', 'link', 'is_staff', 'is_superuser')
     list_filter = ('type', 'is_staff', 'is_superuser', 'groups')
     filter_horizontal = ('groups', 'user_permissions',)
 
-    search_fields = ('id', )
+    search_fields = ('id', 'client__name', 'person__emails__address')
     ordering = ('id',)
 
     def link(self, obj):
@@ -35,12 +36,12 @@ class RoleAdmin(UserAdmin):
         if obj.type == obj.CLIENT_ROLE:
             return link_schema % (
                 reverse('admin:clients_client_change', args=(obj.client.pk,)),
-                _('Voir le client')
+                escape(obj.client.name)
             )
         elif obj.type == obj.PERSON_ROLE:
             return link_schema % (
                 reverse('admin:people_person_change', args=(obj.person.pk,)),
-                _('Voir la personne')
+                escape(obj.person.email)
             )
     link.allow_tags = True
     link.short_description = _('Lien vers la personne ou le client')
