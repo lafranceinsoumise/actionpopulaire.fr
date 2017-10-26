f4a357299028d515e162547bca10356d7a6c9e9e
Arthur Cheysson <arthur@cheysson.fr>
2017-10-26 16:52:49 +0200

    [front] remove bug when url with malformed uuid

--- src/front/urls.py
+++ src/front/urls.py
@@ -2,6 +2,10 @@ from django.conf.urls import url
 
 from . import views, oauth
 
+uuid = r'[0-9a-f]{8}(?:-[0-9a-f]{4}){3}-[0-9a-f]{12}'
+simple_id = r'[0-9]+'
+
+
 urlpatterns = [
     # people views
     url('^desabonnement/$', views.UnsubscribeView.as_view(), name='unsubscribe'),
@@ -15,29 +19,29 @@ urlpatterns = [
     url('^agir/confirmation/$', views.VolunteerConfirmationView.as_view(), name='confirmation_volunteer'),
     url('^message_preferences/$', views.MessagePreferencesView.as_view(), name='message_preferences'),
     url('^message_preferences/adresses/$', views.EmailManagementView.as_view(), name='email_management'),
-    url('^message_preferences/adresses/(?P<pk>[0-9a-f-]+)/supprimer/$', views.DeleteEmailAddressView.as_view(), name='delete_email'),
+    url(f'^message_preferences/adresses/(?P<pk>{simple_id})/supprimer/$', views.DeleteEmailAddressView.as_view(), name='delete_email'),
 
     # events views
     url('^evenements/$', views.EventListView.as_view(), name='list_events'),
     url('^evenements/creer/$', views.CreateEventView.as_view(), name='create_event'),
-    url('^evenements/(?P<pk>[0-9a-f-]+)/$', views.EventDetailView.as_view(), name='view_event'),
-    url('^evenements/(?P<pk>[0-9a-f-]+)/manage/$', views.ManageEventView.as_view(), name='manage_event'),
-    url('^evenements/(?P<pk>[0-9a-f-]+)/modifier/$', views.ModifyEventView.as_view(), name='edit_event'),
-    url('^evenements/(?P<pk>[0-9a-f-]+)/quitter/$', views.QuitEventView.as_view(), name='quit_event'),
-    url('^evenements/(?P<pk>[0-9a-f-]+)/annuler/$', views.CancelEventView.as_view(), name='cancel_event'),
-    url('^evenements/(?P<pk>[0-9a-f-]+)/localisation/$', views.ChangeEventLocationView.as_view(), name='change_event_location'),
+    url(f'^evenements/(?P<pk>{uuid})/$', views.EventDetailView.as_view(), name='view_event'),
+    url(f'^evenements/(?P<pk>{uuid})/manage/$', views.ManageEventView.as_view(), name='manage_event'),
+    url(f'^evenements/(?P<pk>{uuid})/modifier/$', views.ModifyEventView.as_view(), name='edit_event'),
+    url(f'^evenements/(?P<pk>{uuid})/quitter/$', views.QuitEventView.as_view(), name='quit_event'),
+    url(f'^evenements/(?P<pk>{uuid})/annuler/$', views.CancelEventView.as_view(), name='cancel_event'),
+    url(f'^evenements/(?P<pk>{uuid})/localisation/$', views.ChangeEventLocationView.as_view(), name='change_event_location'),
     url('^agenda/(?P<slug>[-a-zA-Z0-9_]+)/$', views.CalendarView.as_view(), name='view_calendar'),
 
     # groups views
     url('^groupes/$', views.SupportGroupListView.as_view(), name='list_groups'),
     url('^groupes/creer/$', views.CreateSupportGroupView.as_view(), name='create_group'),
-    url('^groupes/(?P<pk>[0-9a-f-]+)/$', views.SupportGroupDetailView.as_view(), name='view_group'),
-    url('^groupes/(?P<pk>[0-9a-f-]+)/manage/$', views.SupportGroupManagementView.as_view(), name='manage_group'),
-    url('^groupes/(?P<pk>[0-9a-f-]+)/modifier/$', views.ModifySupportGroupView.as_view(), name='edit_group'),
-    url('^groupes/(?P<pk>[0-9a-f-]+)/quitter/$', views.QuitSupportGroupView.as_view(), name='quit_group'),
-    url('^groupes/(?P<pk>[0-9a-f-]+)/localisation/$', views.ChangeGroupLocationView.as_view(), name='change_group_location'),
+    url(f'^groupes/(?P<pk>{uuid})/$', views.SupportGroupDetailView.as_view(), name='view_group'),
+    url(f'^groupes/(?P<pk>{uuid})/manage/$', views.SupportGroupManagementView.as_view(), name='manage_group'),
+    url(f'^groupes/(?P<pk>{uuid})/modifier/$', views.ModifySupportGroupView.as_view(), name='edit_group'),
+    url(f'^groupes/(?P<pk>{uuid})/quitter/$', views.QuitSupportGroupView.as_view(), name='quit_group'),
+    url(f'^groupes/(?P<pk>{uuid})/localisation/$', views.ChangeGroupLocationView.as_view(), name='change_group_location'),
 
-    url('^groupes/retirer_gestionnaire/(?P<pk>[0-9a-f-]+)/$', views.RemoveManagerView.as_view(), name='remove_manager'),
+    url(f'^groupes/retirer_gestionnaire/(?P<pk>{simple_id})/$', views.RemoveManagerView.as_view(), name='remove_manager'),
 
     url('^livrets_thematiques/$', views.ThematicBookletViews.as_view(), name="thematic_groups_list"),
 
