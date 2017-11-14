aa67268648e60988dca71b260f02d44cd6d0510a
Arthur Cheysson <arthur@cheysson.fr>
2017-11-14 20:06:57 +0100

    [front+polls] use radio buttons for polls with only one selection

--- src/polls/admin.py
+++ src/polls/admin.py
@@ -1,7 +1,11 @@
-from api.admin import admin_site
 from django.contrib import admin
+from django.utils.html import format_html
+from django.utils.translation import ugettext as _
+
+from api.admin import admin_site
 
 from polls.models import Poll, PollOption
+from front.utils import front_url
 
 
 class PollOptionInline(admin.TabularInline):
@@ -11,3 +15,14 @@ class PollOptionInline(admin.TabularInline):
 @admin.register(Poll, site=admin_site)
 class PollAdmin(admin.ModelAdmin):
     inlines = [PollOptionInline]
+
+    fields = ['title', 'link', 'start', 'end', 'rules', 'tags']
+    readonly_fields = ['link']
+
+    def link(self, object):
+        if object.pk:
+            return format_html(
+                '<a href="{url}">{text}</a>',
+                url=front_url('participate_poll', args=[object.pk]),
+                text=_("Voir la consultation")
+            )
