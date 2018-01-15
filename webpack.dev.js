f44a7916647ac6c51f19f90cd7ba1312841f897f
Guillaume Royer <perso@guilro.com>
2018-01-15 17:16:18 +0100

    add hot module replacement in webpack

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,25 @@
+const path = require('path');
 const merge = require('webpack-merge');
+const webpack = require('webpack');
 
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-  devtool: 'inline-source-map'
+  devtool: 'inline-source-map',
+  output: {
+    publicPath: 'http://localhost:3000/assets/components/',
+    filename: '[name]-[hash].js',
+  },
+  devServer: {
+    contentBase: path.join(__dirname, '/assets/components/'),
+    compress: true,
+    port: 3000,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    }
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+  ],
 });
