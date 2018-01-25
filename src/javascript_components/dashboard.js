7ef90721c5ac5fd66975dcc4651670f90e4f9121
Guillaume Royer <perso@guilro.com>
2018-01-25 14:53:04 +0100

    add use of percentage in masonry options

--- src/javascript_components/dashboard.js
+++ src/javascript_components/dashboard.js
@@ -2,5 +2,6 @@ import Masonry from 'masonry-layout';
 
 var elem = document.querySelector('.masonry');
 var msnry = new Masonry( elem, {
-  itemSelector: '.col-md-6'
+  itemSelector: '.col-md-6',
+  percentPosition: true,
 });
