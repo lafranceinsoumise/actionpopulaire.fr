075383697dbad205c75d8a5a8ce60876df15ed05
Arthur Cheysson <arthur@cheysson.fr>
2018-02-14 19:56:55 +0100

    Display description instead of labels for groups on map

--- src/carte/views.py
+++ src/carte/views.py
@@ -74,6 +74,7 @@ def get_subtype_information(subtype):
     res = {
         'id': subtype.id,
         'label': subtype.label,
+        'description': subtype.description,
         'type': subtype.type,
         'hideLabel': subtype.hide_text_label
     }
