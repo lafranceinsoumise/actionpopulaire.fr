57ae6dcdc99b2b7e49fd851567a7a4888dce0d9b
Arthur Cheysson <arthur@cheysson.fr>
2018-03-20 15:16:44 +0100

    Add links to exported csv

--- src/groups/actions/export.py
+++ src/groups/actions/export.py
@@ -2,17 +2,22 @@ import csv
 from operator import attrgetter
 import bleach
 from html import unescape
+from django.conf import settings
+from django.urls import reverse
+
+from front import urls as front_urls
 
 
 __all__ = ['group_queryset_to_csv']
 
 
 COMMON_FIELDS = ['name', 'contact_email', 'contact_phone', 'description',]
+ADDRESS_FIELDS = ['location_name', 'location_address1', 'location_zip', 'location_city']
+LINK_FIELDS = ['link', 'admin_link']
 
-address_fields = ['location_name', 'location_address1', 'location_zip', 'location_city']
 
 common_extractor = attrgetter(*COMMON_FIELDS)
-address_parts_extractor = attrgetter(*address_fields)
+address_parts_extractor = attrgetter(*ADDRESS_FIELDS)
 
 initiator_extractor = attrgetter('first_name', 'last_name', 'contact_phone', 'email')
 
@@ -21,7 +26,7 @@ initiator_template = "{} {} {} <{}>"
 
 
 def group_queryset_to_csv(queryset, output):
-    w = csv.DictWriter(output, fieldnames=COMMON_FIELDS + ['address', 'animators'])
+    w = csv.DictWriter(output, fieldnames=COMMON_FIELDS + ['address', 'animators'] + LINK_FIELDS)
     w.writeheader()
 
     for g in queryset.prefetch_related('memberships'):
@@ -30,4 +35,8 @@ def group_queryset_to_csv(queryset, output):
         d['address'] = address_template.format(*address_parts_extractor(g))
         d['animators'] = ' / '.join(
             initiator_template.format(*initiator_extractor(m.person)).strip() for m in g.memberships.all() if m.is_referent)
+
+        d['link'] = settings.FRONT_DOMAIN + reverse('manage_group', urlconf=front_urls, args=[g.id])
+        d['admin_link'] = settings.API_DOMAIN + reverse('admin:groups_supportgroup_change', args=[g.id])
+
         w.writerow(d)
