2fc0aae77500231dacb23c006c1f6c2b7a469a97
Arthur Cheysson <arthur@cheysson.fr>
2018-03-30 15:27:06 +0200

    Fix draw_people script

--- src/people/management/commands/draw_people.py
+++ src/people/management/commands/draw_people.py
@@ -9,11 +9,11 @@ from sympy.polys import Poly, rem
 
 from django.core.management.base import BaseCommand, CommandError
 from django.db.models import Count, Q
+from django.db import connection
 from django.utils import timezone
 
 from groups.models import SupportGroupSubtype
 from people.models import Person
-
 
 X = symbols('x')
 
@@ -22,14 +22,52 @@ CORR_CHARS[10] = 'A'
 CORR_CHARS[11] = 'B'
 CORR_CHARS[12] = 'C'
 
-INVERSE_CORR = {v:k for k, v in CORR_CHARS.items()}
+INVERSE_CORR = {v: k for k, v in CORR_CHARS.items()}
 
 
 def normalize_poly(p):
     return Poly.from_list((c % 13 for c in p.all_coeffs()), X)
 
 
-G = normalize_poly(Poly((X-2)*(X-4)*(X-8)))
+G = normalize_poly(Poly((X - 2) * (X - 4) * (X - 8)))
+
+
+CERTIFIED_QUERY = """
+SELECT DISTINCT person."id" FROM "people_person" person
+INNER JOIN "groups_membership" membership ON (person.id = membership.person_id)
+INNER JOIN "groups_supportgroup" supportgroup ON (membership.supportgroup_id = supportgroup.id)
+INNER JOIN "groups_supportgroup_subtypes" subtype ON (supportgroup.id = subtype.supportgroup_id)
+WHERE person."draw_participation" = TRUE
+AND person."id" NOT IN %(exclude_ids)s
+AND person."created" < %(date)s
+AND person."gender" = %(gender)s
+AND subtype."supportgroupsubtype_id" = %(subtype)s
+AND membership."created" < %(date)s;
+"""
+
+NOT_CERTIFIED_QUERY = """
+SELECT DISTINCT person."id" FROM "people_person" person
+WHERE person."draw_participation" = TRUE
+AND person."id" NOT IN %(exclude_ids)s
+AND person."created" < %(date)s
+AND person."gender" = %(gender)s
+AND NOT EXISTS (
+  SELECT 1 FROM "groups_membership" membership
+  INNER JOIN "groups_supportgroup" supportgroup ON (membership.supportgroup_id = supportgroup.id)
+  INNER JOIN "groups_supportgroup_subtypes" subtype ON (supportgroup.id = subtype.supportgroup_id)
+  WHERE membership."person_id" = person."id"
+  AND subtype."supportgroupsubtype_id" = %(subtype)s
+  AND membership."created" < %(date)s
+);
+"""
+
+
+def request_executor(request, parameters):
+    with connection.cursor() as cursor:
+        cursor.execute(request, parameters)
+        rows = cursor.fetchall()
+    for row in rows:
+        yield row[0]
 
 
 def create_code_from_int(c, length=6):
@@ -42,9 +80,9 @@ def create_code_from_int(c, length=6):
     coeffs = list(reversed(coeffs))
 
     a = Poly.from_list(coeffs, X)
-    b = rem(a * X**3, G)
+    b = rem(a * X ** 3, G)
 
-    m = normalize_poly(a * X**3 - b)
+    m = normalize_poly(a * X ** 3 - b)
 
     return ''.join(CORR_CHARS[i] for i in m.all_coeffs()).zfill(length)
 
@@ -66,13 +104,12 @@ class Command(BaseCommand):
 
     def date(self, d):
         try:
-            return timezone.datetime.strptime(d, self.date_format).replace(tzinfo=timezone.get_default_timezone())
+            return timezone.get_default_timezone().localize(timezone.datetime.strptime(d, self.date_format))
         except ValueError:
             raise argparse.ArgumentTypeError(f'{d} is not a valid date')
 
-    def draw(self, queryset, draw_count):
-        ids = set(queryset.value_list('id', flat=True))
-        return [Person.objects.get(pk=id) for id in choices(ids, k=draw_count)]
+    def draw(self, it, draw_count):
+        return [Person.objects.get(pk=id) for id in choices(list(it), k=draw_count)]
 
     def add_arguments(self, parser):
         parser.add_argument(
@@ -89,7 +126,8 @@ class Command(BaseCommand):
 
         parser.add_argument('-c', '--certified-on', dest='certified', action='store_true')
         parser.add_argument('-n', '--not-certified-on', dest='not_certified', action='store_true')
-        parser.add_argument('-p', '--previous-draw', dest='previous_draws', type=argparse.FileType('r'), action='append')
+        parser.add_argument('-p', '--previous-draw', dest='previous_draws', type=argparse.FileType('r'),
+                            action='append')
 
     def handle(self, draw_count, reference_date, outfile, certified, not_certified, previous_draws, gender, **kwargs):
         ignore_ids = set()
@@ -102,23 +140,33 @@ class Command(BaseCommand):
             starting_number = max(starting_number, max(decode(p['numero']) for p in previously_drawn) + 1)
             ignore_ids.update(p['id'] for p in previously_drawn)
 
+        ignore_ids = tuple(ignore_ids)
+
         if certified and not_certified:
             raise CommandError('Set either --certified-only OR --not-certified-only')
 
         college = 'certified' if certified else 'not-certified' if not_certified else 'all'
 
-        base_qs = Person.objects.filter(draw_participation=True, created__lt=reference_date).exclude(id__in=ignore_ids)
-
         certified_subtype = SupportGroupSubtype.objects.get(label="certifié")
-        certified_cond = Q(
-            memberships__supportgroup__subtypes=certified_subtype,
-            memberships__created__lt=reference_date
-        )
 
-        if certified:
-            base_qs = base_qs.filter(certified_cond)
         if not_certified:
-            base_qs = base_qs.exclude(certified_cond)
+            pk_iterator = lambda g: request_executor(NOT_CERTIFIED_QUERY, {
+                'date': reference_date,
+                'exclude_ids': ignore_ids,
+                'gender': g,
+                'subtype': certified_subtype.id
+            })
+        elif certified:
+            pk_iterator = lambda g: request_executor(CERTIFIED_QUERY, {
+                'date': reference_date,
+                'exclude_ids': ignore_ids,
+                'gender': g,
+                'subtype': certified_subtype.id
+            })
+
+        else:
+            base_qs = Person.objects.filter(draw_participation=True, created__lt=reference_date)
+            pk_iterator = lambda g: base_qs.filter(gender=g).values_list('id', flat=True).distinct()
 
         if not gender:
             # setting order_by 'gender' so that the created field is not included in the groupby clause
@@ -147,7 +195,7 @@ class Command(BaseCommand):
 
         # DRAWING HAPPENS HERE
         for g, n in draws.items():
-            participants.append(self.draw(base_qs.filter(gender=g), n))
+            participants.append(self.draw(pk_iterator(g), n))
 
         writer = csv.DictWriter(outfile, fieldnames=['numero', 'id', 'email', 'gender', 'college'])
         writer.writeheader()
@@ -155,7 +203,7 @@ class Command(BaseCommand):
         for numero, p in enumerate(chain.from_iterable(participants)):
             # add one so that sequence numbers start with 1
             writer.writerow({
-                'numero': create_code_from_int(numero+starting_number),
+                'numero': create_code_from_int(numero + starting_number),
                 'id': p.id,
                 'email': p.email,
                 'gender': p.gender,
