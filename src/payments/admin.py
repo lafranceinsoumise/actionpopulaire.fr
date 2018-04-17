282888c5666f9f735e313c0bc22f8937c05c9020
Guillaume Royer <perso@guilro.com>
2018-04-17 16:19:09 +0200

    fix payment admin form

--- src/payments/admin.py
+++ src/payments/admin.py
@@ -1,9 +1,23 @@
+from ajax_select.fields import AutoCompleteSelectField
+from django import forms
 from django.contrib import admin
 
 from api.admin import admin_site
 from payments import models
 
 
+class PaymentForm(forms.ModelForm):
+    person = AutoCompleteSelectField(
+        "people",
+        required=True,
+        label="Personne"
+    )
+
+    class Meta:
+        fields = '__all__'
+
+
 @admin.register(models.Payment, site=admin_site)
 class PaymentAdmin(admin.ModelAdmin):
+    form = PaymentForm
     list_display = ('person', 'email', 'first_name', 'last_name', 'price', 'status')
