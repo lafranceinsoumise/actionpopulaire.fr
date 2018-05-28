bb704b47c724ee6ab551b03d9213ebfd6613c660
Arthur Cheysson <arthur@cheysson.fr>
2018-05-28 18:26:56 +0200

    Remove dead code

--- agir/payments/admin.py
+++ agir/payments/admin.py
@@ -1,25 +1,11 @@
-from ajax_select.fields import AutoCompleteSelectField
-from django import forms
 from django.contrib import admin
 
 from agir.api.admin import admin_site
 from . import models
 
 
-class PaymentForm(forms.ModelForm):
-    person = AutoCompleteSelectField(
-        "people",
-        required=True,
-        label="Personne"
-    )
-
-    class Meta:
-        fields = '__all__'
-
-
 @admin.register(models.Payment, site=admin_site)
 class PaymentAdmin(admin.ModelAdmin):
-    form = PaymentForm
     list_display = ('type', 'person', 'email', 'first_name', 'last_name', 'price', 'status', 'created')
     readonly_fields = ('type', 'person', 'email', 'first_name', 'last_name', 'price', 'status', 'phone_number',
                        'location_address1', 'location_address2', 'location_zip', 'location_city',
