750862e8ac284d280644652cc62aa947a89dd0e2
Guillaume Royer <perso@guilro.com>
2018-06-29 18:28:51 +0200

    add admin for systempay transaction

---
+++ agir/system_pay/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+
+from agir.api.admin import admin_site
+from . import models
+
+
+@admin.register(models.SystemPayTransaction, site=admin_site)
+class SystemPayTransactionAdmin(admin.ModelAdmin):
+    list_display = ('payment', 'person', 'status', 'webhook_calls')
+    readonly_fields = ('payment', 'person', 'status', 'webhook_calls')
+    fields = readonly_fields
+    list_filter = ('status',)
+    search_fields = ('payment__email', 'payment__person__emails__address')
+
+    def person(self, obj):
+        return obj.person
