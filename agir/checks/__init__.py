d4719a3c6bfbe266f291ce6e79859fd9a6b91d35
Guillaume Royer <perso@guilro.com>
2018-07-02 12:06:25 +0200

    add retry info for check

--- agir/checks/__init__.py
+++ agir/checks/__init__.py
@@ -9,7 +9,14 @@ class CheckPaymentMode(AbstractPaymentMode):
     url_fragment = 'cheque'
     label =_('Paiement par chèque')
 
+    can_retry = True
+
     @cached_property
     def payment_view(self):
+        from . import views
+        return views.CheckView.as_view()
+
+    @cached_property
+    def retry_payment_view(self):
         from . import views
         return views.CheckView.as_view()
