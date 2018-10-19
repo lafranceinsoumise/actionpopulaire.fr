386b3672baa44faab2a3bc766302bd690b082928
Guillaume Royer <perso@guilro.com>
2018-10-19 18:35:04 +0200

    change drawing system

--- agir/people/management/commands/draw_people.py
+++ agir/people/management/commands/draw_people.py
@@ -1,8 +1,5 @@
-import sys
 import argparse
-import csv
-from itertools import chain
-from random import choices
+from random import sample
 
 from sympy import symbols
 from sympy.polys import Poly, rem
@@ -13,7 +10,7 @@ from django.db import connection
 from django.utils import timezone
 
 from agir.groups.models import SupportGroupSubtype
-from agir.people.models import Person
+from agir.people.models import Person, PersonTag
 
 X = symbols('x')
 
@@ -69,32 +66,6 @@ def request_executor(request, parameters):
     for row in rows:
         yield row[0]
 
-
-def create_code_from_int(c, length=6):
-    coeffs = []
-
-    while c:
-        coeffs.append(c % 10)
-        c = c // 10
-
-    coeffs = list(reversed(coeffs))
-
-    a = Poly.from_list(coeffs, X)
-    b = rem(a * X ** 3, G)
-
-    m = normalize_poly(a * X ** 3 - b)
-
-    return ''.join(CORR_CHARS[i] for i in m.all_coeffs()).zfill(length)
-
-
-def decode(c):
-    p = Poly.from_list(map(INVERSE_CORR.get, c), X)
-    if all(p.subs(X, f) % 13 == 0 for f in [2, 4, 8]):
-        return int(c[:-3])
-    else:
-        return None
-
-
 class Command(BaseCommand):
     date_format = "%d/%m/%Y"
 
@@ -108,39 +79,25 @@ class Command(BaseCommand):
         except ValueError:
             raise argparse.ArgumentTypeError(f'{d} is not a valid date')
 
-    def draw(self, it, draw_count):
-        return [Person.objects.get(pk=id) for id in choices(list(it), k=draw_count)]
+    def draw(self, it, draw_count, tag):
+        (tag, created) = PersonTag.objects.get_or_create(label=tag)
+        for id in sample(list(it), k=int(draw_count)):
+            person = Person.objects.get(pk=id)
+            person.tags.add(tag)
 
     def add_arguments(self, parser):
-        parser.add_argument(
-            '-o', '--output',
-            dest='outfile', type=argparse.FileType('w'), default=sys.stdout,
-            help='Csv file to which the list will be exported'
-        )
-
         parser.add_argument('reference_date', type=self.date)
-
         parser.add_argument('draw_count', type=int)
+        parser.add_argument('tag', action='store')
 
         parser.add_argument('-g', '--gender')
 
         parser.add_argument('-c', '--certified-on', dest='certified', action='store_true')
         parser.add_argument('-n', '--not-certified-on', dest='not_certified', action='store_true')
-        parser.add_argument('-p', '--previous-draw', dest='previous_draws', type=argparse.FileType('r'),
-                            action='append')
+        parser.add_argument('-p', '--previous-tags', dest='previous_tags', action='append')
 
-    def handle(self, draw_count, reference_date, outfile, certified, not_certified, previous_draws, gender, **kwargs):
-        ignore_ids = set()
-        starting_number = 1
-
-        for p in previous_draws:
-            r = csv.DictReader(p)
-            previously_drawn = list(r)
-
-            starting_number = max(starting_number, max(decode(p['numero']) for p in previously_drawn) + 1)
-            ignore_ids.update(p['id'] for p in previously_drawn)
-
-        ignore_ids = tuple(ignore_ids)
+    def handle(self, reference_date, draw_count, tag, gender, certified, not_certified, previous_tags, **kwargs):
+        exclude_ids = [p.id for p in Person.objects.filter(tags__label__in=previous_tags or [])]
 
         if certified and not_certified:
             raise CommandError('Set either --certified-only OR --not-certified-only')
@@ -152,14 +109,14 @@ class Command(BaseCommand):
         if not_certified:
             pk_iterator = lambda g: request_executor(NOT_CERTIFIED_QUERY, {
                 'date': reference_date,
-                'exclude_ids': ignore_ids,
+                'exclude_ids': exclude_ids,
                 'gender': g,
                 'subtype': certified_subtype.id
             })
         elif certified:
             pk_iterator = lambda g: request_executor(CERTIFIED_QUERY, {
                 'date': reference_date,
-                'exclude_ids': ignore_ids,
+                'exclude_ids': exclude_ids,
                 'gender': g,
                 'subtype': certified_subtype.id
             })
@@ -177,7 +134,7 @@ class Command(BaseCommand):
 
             total_count = sum(counts[g] for g in [Person.GENDER_FEMALE, Person.GENDER_MALE, Person.GENDER_OTHER])
             other_draw_count = round(draw_count * counts[Person.GENDER_OTHER] / total_count / 2) * 2
-            gendered_draw_count = (draw_count - other_draw_count) / 2
+            gendered_draw_count = round((draw_count - other_draw_count) / 2)
 
             if counts[Person.GENDER_MALE] < gendered_draw_count or counts[Person.GENDER_FEMALE] < gendered_draw_count:
                 raise CommandError("Not enough volunteers for drawing with parity")
@@ -191,21 +148,8 @@ class Command(BaseCommand):
         else:
             draws = {gender: draw_count}
 
-        participants = []
-
         # DRAWING HAPPENS HERE
         for g, n in draws.items():
-            participants.append(self.draw(pk_iterator(g), n))
+            self.draw(pk_iterator(g), n, tag + ' ' + g)
 
-        writer = csv.DictWriter(outfile, fieldnames=['numero', 'id', 'email', 'gender', 'college'])
-        writer.writeheader()
-
-        for numero, p in enumerate(chain.from_iterable(participants)):
-            # add one so that sequence numbers start with 1
-            writer.writerow({
-                'numero': create_code_from_int(numero + starting_number),
-                'id': p.id,
-                'email': p.email,
-                'gender': p.gender,
-                'college': college
-            })
+        print(f"Drew {gendered_draw_count} women, {gendered_draw_count} men and {other_draw_count} others.")
