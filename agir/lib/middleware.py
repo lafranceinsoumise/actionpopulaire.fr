4102c84770443ce315e2d1e727fef8f3340d575f
Arthur Cheysson <arthur@cheysson.fr>
2018-12-06 16:04:23 +0100

    Règle le bug lié à l'intégration de l'URL dans un header

--- agir/lib/middleware.py
+++ agir/lib/middleware.py
@@ -1,6 +1,7 @@
 from urllib.parse import urljoin
 
 from django.conf import settings
+from django.utils.http import urlquote
 
 
 class TurbolinksMiddleware:
@@ -10,6 +11,7 @@ class TurbolinksMiddleware:
     def __call__(self, request):
         response = self.get_response(request)
 
-        response["Turbolinks-Location"] = urljoin(settings.FRONT_DOMAIN, request.path)
+        location = urljoin(settings.FRONT_DOMAIN, urlquote(request.path))
+        response["Turbolinks-Location"] = location
 
         return response
