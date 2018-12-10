83afd16f5dd04e442e92d0ac07039c76e2c9b0db
Arthur Cheysson <arthur@cheysson.fr>
2018-12-10 20:03:59 +0100

    Améliore la qualité du code filtrage par départements et régions

--- agir/lib/data/__init__.py
+++ agir/lib/data/__init__.py
@@ -1,12 +1,11 @@
 import csv
 
-import re
-from functools import reduce
-
 import os
+import re
 from django.db.models import Q
+from functools import reduce
 from operator import or_
-
+from unidecode import unidecode
 
 with open(os.path.dirname(os.path.realpath(__file__)) + "/departements.csv") as file:
     departements = list(csv.DictReader(file))
@@ -15,9 +14,11 @@ with open(os.path.dirname(os.path.realpath(__file__)) + "/regions.csv") as file:
     regions = list(csv.DictReader(file))
 
 
-departements_choices = tuple((d["id"], d["nom"]) for d in departements)
+departements_choices = tuple((d["id"], f'{d["id"]} - {d["nom"]}') for d in departements)
 
-regions_choices = tuple((r["id"], r["nom"]) for r in regions)
+regions_choices = tuple(
+    (r["id"], r["nom"]) for r in sorted(regions, key=lambda d: unidecode(d["nom"]))
+)
 
 
 _CORSE_RE = re.compile("^[A|B]")
