e3ccdca717dfff9f86a32a3356dde589c2e391d1
Arthur Cheysson <arthur@cheysson.fr>
2018-12-14 14:06:16 +0100

    Réverte un fichier committé par erreur qui casse la création d'events

--- agir/lib/components/creationForms/createEventForm.js
+++ agir/lib/components/creationForms/createEventForm.js
@@ -16,74 +16,33 @@ import "./style.css";
 // defined by webpack
 const apiEndpoint = API_ENDPOINT; // eslint-disable-line no-undef
 
-const TYPE_REUNION_GROUPE = "G";
-const TYPE_REUNION_PUBLIQUE = "L";
-const TYPE_ACTION = "A";
-const TYPE_AUTRE = "O";
-
 const groupTypes = [
   {
-    id: TYPE_REUNION_GROUPE,
+    id: "G",
     label: "Une réunion de groupe",
     description:
       "Une réunion qui concerne principalement les membres du groupes, et non le public de façon générale. " +
       "Par exemple, la réunion hebdomadaire du groupe, une réunion de travail, ou l'audition d'une association"
   },
   {
-    id: TYPE_REUNION_PUBLIQUE,
+    id: "M",
     label: "Une réunion publique",
     description:
       "Une réunion ouverts à tous les publics, au-delà des membres du groupe d'action, mais qui aura lieu " +
       "dans un lieu privé. Par exemple, une réunion publique avec un orateur, une projection ou un concert."
   },
   {
-    id: TYPE_ACTION,
+    id: "A",
     label: "Une action publique",
     description:
       "Une action qui se déroulera dans un lieu public et qui aura comme objectif principal d'aller à la" +
       "rencontre ou d'atteindre des personnes extérieures à la FI"
   },
   {
-    id: TYPE_AUTRE,
+    id: "O",
     label: "Un autre type d'événement",
     description:
       "Tout autre type d'événement qui ne rentre pas dans les catégories ci-dessus."
-  }
-];
-
-const QUESTIONS = [
-  {
-    id: "interieur",
-    question: "L'événement aura-t-il lieu dans un lieu privé ?",
-    helpText:
-      "Répondez oui si l'événement a été organisé dans une salle, dans un bar ou dans un autre lieu privé.",
-    when: ({ type }) => [TYPE_REUNION_PUBLIQUE, TYPE_AUTRE].includes(type)
-  },
-  {
-    id: "installationTechnique",
-    question: "Cet événement nécessite-t-il une installation technique ?",
-    when: ({ type, interieur }) =>
-      [TYPE_REUNION_PUBLIQUE, TYPE_AUTRE].includes(type) && !interieur
-  },
-  {
-    id: "candidat",
-    question:
-      "Votre événement fait-il intervenir un candidat aux élections européennes ?",
-    when: ({ type }) => type !== TYPE_REUNION_GROUPE
-  },
-  {
-    id: "materielCampagne",
-    question:
-      "Utiliserez-vous du matériel siglé campagne européenne pendant votre événement ?",
-    when: ({ type }) => type !== TYPE_REUNION_GROUPE
-  },
-  {
-    id: "impressionPropresMoyens",
-    question:
-      "Imprimerez-vous ce matériel grâce à une imprimante personnelle ?",
-    helpText:
-      "Vous ne pouvez pas payer vous-mêmes une impression chez un imprimeur. Toutes les dépenses électorales " +
-      "doivent être réalisées par l'association de financement de la campagne européenne."
   }
 ];
 
@@ -118,10 +77,6 @@ class CreateEventForm extends React.Component {
             subtypes={this.state.subtypes}
           />
         )
-      },
-      {
-        name: "Que déclarer ?",
-        component: <QuestionStep />
       },
       {
         name: "Qui contacter ?",
@@ -187,9 +142,9 @@ function CheckBox({ label, active, onClick }) {
           onClick();
         }}
       >
-        {" "}
         <i className={"fa " + (active ? "fa-check-circle" : "fa-circle-o")} />
-        &nbsp;{label}
+        &nbsp;
+        {label}
       </a>
     </li>
   );
@@ -199,35 +154,6 @@ CheckBox.propTypes = {
   label: PropTypes.string,
   active: PropTypes.bool,
   onClick: PropTypes.func
-};
-
-const YesNoComponent = ({ name, value, setValue }) => (
-  <React.Fragment>
-    <label className="radio-inline">
-      <input
-        type="radio"
-        name={name}
-        onChange={() => setValue(true)}
-        checked={value === true}
-      />{" "}
-      Oui{" "}
-    </label>
-    <label className="radio-inline">
-      {" "}
-      <input
-        type="radio"
-        name={name}
-        onChange={() => setValue(false)}
-        checked={value === false}
-      />{" "}
-      Non{" "}
-    </label>
-  </React.Fragment>
-);
-YesNoComponent.propTypes = {
-  name: PropTypes.string,
-  value: PropTypes.bool,
-  setValue: PropTypes.func
 };
 
 class EventTypeStep extends FormStep {
@@ -253,31 +179,9 @@ class EventTypeStep extends FormStep {
     return !!this.props.fields.subtype;
   }
 
-  render() {
-    return (
-      <div className="row padtopmore">
-        <div className="col-sm-6">
-          <h2>Quel type d'événement voulez-vous créer ?</h2>
-          <p>
-            Chaque insoumis·e peut créer un événement sur la plateforme dès lors
-            qu’il respecte le cadre et la démarche de la France insoumise dans
-            un esprit d’ouverture, de bienveillance et de volonté de se projeter
-            dans l’action.{" "}
-          </p>
-          <p>
-            Afin de mieux identifier les événements que vous créez, et de
-            pouvoir mieux les recommander aux autres insoumis⋅es, indiquez le
-            type d'événement que vous organisez.{" "}
-          </p>
-          <p>
-            Vous souhaitez inviter un⋅e député⋅e, candidat⋅e, ou animateur⋅ice
-            de livret&nbsp;?{" "}
-            <a href="https://lafranceinsoumise.fr/groupes-appui/inviter-des-intervenants/">
-              Suivez le mode d'emploi.
-            </a>
-          </p>
-        </div>
-        return (
+  getRightColumn() {
+    if (this.state.isNotCampaign) {
+      return (
         <div className="col-sm-6 padbottom">
           <h3>Je veux créer...</h3>
           {groupTypes.map(type => (
@@ -296,105 +200,78 @@ class EventTypeStep extends FormStep {
             </div>
           ))}
         </div>
-        );
+      );
+    }
+
+    return (
+      <div className="col-sm-6 padbottom">
+        <div className="alert alert-danger">
+          Attention&nbsp;! Nous sommes désormais officiellement en campagne
+          électorale. Cela signifie que vous ne pouvez engager de frais sans la
+          validation du mandataire financier de la campagne.
+        </div>
+        <h3>Je veux créer...</h3>
+        <h5>...un événement de campagne avec frais</h5>
+        <p>
+          Un événement de campagne qui implique des moyens financiers. S'il
+          remplit certains critères, il peut être financé. Sinon, il doit être
+          financé par des dons. Dans tous les cas, les factures doivent être
+          réglées par le mandataire financier de la campagne.
+        </p>
+        <a
+          className="btn btn-default"
+          href="https://agir.lafranceinsoumise.fr/formulaires/E19-01/"
+        >
+          Je remplis le formulaire d'accord préalable
+        </a>
+        <h5>...un autre événement</h5>
+        <p>
+          Ce n'est pas un événement de campagne, ou c'est un événement de
+          campagne qui ne nécessite pas de moyens payants. S'il y a un concours
+          gratuit en nature (prêt de salle, de matériel...), vous devez
+          néanmoins remplir{" "}
+          <a href="https://lafranceinsoumise.fr/app/uploads/2018/11/europennes_dons_en_nature.pdf">
+            le formulaire dédié
+          </a>{" "}
+          à l'issue de l'événement.
+        </p>
+        <button
+          className="btn btn-default"
+          onClick={() => this.setState({ isNotCampaign: true })}
+        >
+          Je crée directement mon événement
+        </button>
       </div>
     );
-  }
-}
-
-class QuestionStep extends FormStep {
-  constructor(props) {
-    super(props);
   }
 
   render() {
     return (
       <div className="row padtopmore">
         <div className="col-sm-6">
-          <h2>Dites-nous en plus sur votre événement</h2>
+          <h2>Quel type d'événement voulez-vous créer ?</h2>
           <p>
-            Afin d'identifier clairement la nature de votre événement, merci de
-            répondre à ce quelques questions.
+            Chaque insoumis·e peut créer un événement sur la plateforme dès lors
+            qu’il respecte le cadre et la démarche de la France insoumise dans
+            un esprit d’ouverture, de bienveillance et de volonté de se projeter
+            dans l’action.
+          </p>
+          <p>
+            Afin de mieux identifier les événements que vous créez, et de
+            pouvoir mieux les recommander aux autres insoumis⋅es, indiquez le
+            type d'événement que vous organisez.
+          </p>
+          <p>
+            Vous souhaitez inviter un⋅e député⋅e, candidat⋅e, ou animateur⋅ice
+            de livret&nbsp;?{" "}
+            <a href="https://lafranceinsoumise.fr/groupes-appui/inviter-des-intervenants/">
+              Suivez le mode d'emploi.
+            </a>
           </p>
         </div>
+        {this.getRightColumn()}
       </div>
     );
-  }
-
-  getRightColumn() {
-    switch (subStep) {
-      case 1:
-        return (
-          <div className="col-sm-6 padbottom">
-            {campaign && funded && (
-              <div>
-                <div className="alert alert-danger">
-                  Attention&nbsp;! Aucune dépense de campagne ne peut être
-                  engagée sans la validation du mandataire financier de la
-                  campagne européenne. Pour cela, vous devez{" "}
-                  <strong>impérativement</strong> remplir le formulaire d'accord
-                  préalable et en obtenir la validation <em>avant</em> de créer
-                  votre événement.
-                </div>
-                <a className="btn btn-primary" href="/formulaires/E19-01/">
-                  {" "}
-                  Je remplis le formulaire d'accord préalable
-                </a>
-
-                <div>
-                  Ce n'est qu'une fois que vous aurez obtenu la validation de
-                  l'équipe de suivi des questions financières qu'il vous faudra
-                  créer votre événement.
-                </div>
-                <button
-                  className="btn btn-default"
-                  onClick={this.setState({ subStep: 2 })}
-                >
-                  J'ai la validation de l'équipe de suivi des questions
-                  financières, je crée mon événement
-                </button>
-              </div>
-            )}
-          </div>
-        );
-      default:
-        return (
-          <div className="col-sm-6 padbottom">
-            <div className="alert alert-danger">
-              Attention&nbsp;! De manière générale, vous ne pouvez pas engager
-              de frais sans la validation du mandataire financier de la campagne
-              européenne ou de la France insoumise. Pour vous guider dans la
-              création de votre événement, merci d'indiquer les éléments
-              suivants :
-            </div>
-            <div>
-              <h3>Mon événement...</h3>
-              <h5>...est en lien avec la campagne des européennes</h5>
-              <p>S'agit-il d'un événement de campagne ?</p>
-              <YesNoComponent
-                name="campaign"
-                value={campaign}
-                setValue={value => this.setState({ campaign: value })}
-              />
-
-              <h5>...nécessite un financement</h5>
-
-              <YesNoComponent
-                name="funded"
-                value={funded}
-                setValue={value => this.setState({ funded: value })}
-              />
-            </div>
-
-            <button
-              className="btn btn-primary"
-              onClick={() => this.validateQuestions()}
-            >
-              Valider
-            </button>
-          </div>
-        );
-    }
   }
 }
 
@@ -423,7 +300,7 @@ class OrganizerStep extends FormStep {
             Un événement peut être organisé à titre individuel par une personne.
             Mais comme vous êtes aussi gestionnaire d'un groupe d'action, il est
             aussi possible d'indiquer que cet événement est organisé par votre
-            groupe.{" "}
+            groupe.
           </p>
         </div>
         <div className="col-md-6">
@@ -431,12 +308,11 @@ class OrganizerStep extends FormStep {
           <div>
             <h4>...à titre individuel</h4>
             <CheckBoxList>
-              {" "}
               <CheckBox
                 active={!organizerGroup}
                 label="J'en suis l'organisateur"
                 onClick={this.setIndividual}
-              />{" "}
+              />
             </CheckBoxList>
           </div>
           <div>
@@ -463,7 +339,6 @@ class ValidateStep extends FormStep {
     super(props);
     this.post = this.post.bind(this);
     this.state = { processing: false };
-    this.eventNameRef = React.createRef();
   }
 
   async post(e) {
@@ -472,7 +347,7 @@ class ValidateStep extends FormStep {
 
     const { fields } = this.props;
     let data = qs.stringify({
-      name: this.eventNameRef.current.value,
+      name: this.eventName.value,
       contact_email: fields.email,
       contact_name: fields.name || null,
       contact_phone: fields.phone,
@@ -491,7 +366,7 @@ class ValidateStep extends FormStep {
 
     try {
       let res = await axios.post("form/", data);
-      window.location.href = res.data.url;
+      location.href = res.data.url;
     } catch (e) {
       this.setState({ error: e, processing: false });
     }
@@ -528,8 +403,9 @@ class ValidateStep extends FormStep {
             </li>
             <li>
               <strong>Lieu&nbsp;:</strong>
-              <br /> {fields.locationAddress1}
-              <br />{" "}
+              <br />
+              {fields.locationAddress1}
+              <br />
               {fields.locationAddress2 ? (
                 <span>
                   {fields.locationAddress2}
@@ -537,7 +413,7 @@ class ValidateStep extends FormStep {
                 </span>
               ) : (
                 ""
-              )}{" "}
+              )}
               {fields.locationZip}, {fields.locationCity}
             </li>
           </ul>
@@ -552,7 +428,7 @@ class ValidateStep extends FormStep {
             <div className="form-group">
               <input
                 className="form-control"
-                ref={this.eventNameRef}
+                ref={i => (this.eventName = i)}
                 type="text"
                 placeholder="Nom de l'événement"
                 required
@@ -568,7 +444,7 @@ class ValidateStep extends FormStep {
           </form>
           {this.state.error && (
             <div className="alert alert-warning">
-              Une erreur s'est produite. Merci de réessayer plus tard.{" "}
+              Une erreur s'est produite. Merci de réessayer plus tard.
             </div>
           )}
         </div>
