98601a9307dc8bbb2c22325e3178af986db05a0d
Jill Royer <perso@guilro.com>
2019-01-11 16:45:53 +0100

    ajoute le compte de destination dans systempayadmin

--- agir/system_pay/admin.py
+++ agir/system_pay/admin.py
@@ -6,11 +6,21 @@ from . import models
 
 @admin.register(models.SystemPayTransaction, site=admin_site)
 class SystemPayTransactionAdmin(admin.ModelAdmin):
-    list_display = ("id", "payment", "person", "status", "webhook_calls")
-    readonly_fields = ("id", "payment", "person", "status", "webhook_calls")
+    list_display = ("id", "payment", "destination", "person", "status", "webhook_calls")
+    readonly_fields = (
+        "id",
+        "payment",
+        "destination",
+        "person",
+        "status",
+        "webhook_calls",
+    )
     fields = readonly_fields
     list_filter = ("status",)
     search_fields = ("payment__email", "payment__person__emails__address__iexact")
+
+    def destination(self, obj):
+        return obj.payment.mode
 
     def person(self, obj):
         return obj.payment.person
