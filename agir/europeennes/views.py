7bd4fc6bb454059ef9b1274b46ea24a0001368c4
Arthur Cheysson <arthur@cheysson.fr>
2019-02-07 15:35:50 +0100

    Corrige texte et OpenGraph pour la page dons européennes

--- agir/europeennes/views.py
+++ agir/europeennes/views.py
@@ -5,9 +5,17 @@ from agir.donations.views import (
     PersonalInformationView as BasePersonalInformationView,
 )
 from agir.europeennes import AFCESystemPayPaymentMode
+from agir.front.view_mixins import SimpleOpengraphMixin
 
 
-class AskAmountView(BaseAskAmountView):
+class AskAmountView(SimpleOpengraphMixin, BaseAskAmountView):
+    meta_title = "Je donne à la campagne France insoumise pour les élections européennes le 26 mai"
+    meta_description = (
+        "Nos 79 candidats, menés par Manon Aubry, la tête de liste, sillonent déjà le pays. Ils ont"
+        " besoin de votre soutien pour pouvoir mener cette campagne. Votre contribution sera décisive !"
+    )
+    meta_type = "website"
+
     enable_allocations = False
     template_name = "europeennes/ask_amount.html"
     success_url = reverse_lazy("europeennes_donation_information")
