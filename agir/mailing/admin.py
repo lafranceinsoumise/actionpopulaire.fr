4b67e22c595a4fd379a893f4943d10353a9ae3bd
Jill Royer <perso@guilro.com>
2019-03-20 17:40:53 +0100

    plus de détail dans l'admin des segments

--- agir/mailing/admin.py
+++ agir/mailing/admin.py
@@ -10,3 +10,16 @@ from agir.mailing.models import Segment
 class SegmentAdmin(CenterOnFranceMixin, OSMGeoAdmin):
     autocomplete_fields = ("tags", "supportgroup_subtypes")
     readonly_fields = ("get_subscribers_count",)
+    ordering = ("name",)
+    list_display = (
+        "name",
+        "supportgroup_status",
+        "supportgroup_subtypes_list",
+        "tags_list",
+    )
+
+    def supportgroup_subtypes_list(self, instance):
+        return ", ".join(str(s) for s in instance.supportgroup_subtypes.all())
+
+    def tags_list(self, instance):
+        return ", ".join(str(t) for t in instance.tags.all())
