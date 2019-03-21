8bfc4573f00e9fd4a772395f713f3cfb210f377e
Arthur Cheysson <arthur@cheysson.fr>
2019-03-21 16:40:12 +0100

    S'assure que l'adresse d'un donateur résident fiscal est bien en France

--- agir/donations/forms.py
+++ agir/donations/forms.py
@@ -249,9 +249,10 @@ class DonorForm(MetaFieldsMixin, forms.ModelForm):
     def clean(self):
         cleaned_data = super().clean()
 
-        nationality, fiscal_resident = (
+        nationality, fiscal_resident, location_country = (
             cleaned_data.get("nationality"),
             cleaned_data.get("fiscal_resident"),
+            cleaned_data.get("location_country"),
         )
 
         if nationality != "FR" and not fiscal_resident:
@@ -262,6 +263,17 @@ class DonorForm(MetaFieldsMixin, forms.ModelForm):
                         "Les personnes non-françaises doivent être fiscalement domiciliées en France."
                     ),
                     code="not_fiscal_resident",
+                ),
+            )
+
+        if fiscal_resident and not location_country == "FR":
+            self.add_error(
+                "location_country",
+                forms.ValidationError(
+                    _(
+                        "Pour pouvoir donner si vous n'êtes pas français, vous devez être domicilié⋅e fiscalement en"
+                        " France et nous indiquer votre adresse fiscale en France."
+                    )
                 ),
             )
 
