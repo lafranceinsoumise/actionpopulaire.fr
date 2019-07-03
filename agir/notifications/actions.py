68bb74e490d26b1ba8e93255c8aa16d7e0a0b552
Arthur Cheysson <arthur@cheysson.fr>
2019-07-03 10:38:44 +0200

    refactoring(notifications): système de notifications personnelles
    Notification devient GlobalNotification pour marquer le fait qu'il ne
    s'agit que d'une sous-catégorie des notifications de façon générale.
    
    NotificationStatus prend le nom de Notification, et n'est plus seulement
    associé à une GlobalNotification, mais peut exister indépendament, ce
    qui permet d'ajouter des notifications individuelles suite à telle ou
    telle action.

--- agir/notifications/actions.py
+++ agir/notifications/actions.py
@@ -1,8 +1,31 @@
+from django.db.models import Exists, OuterRef
 from django.utils.functional import cached_property
-from glom import glom, T, Call
+from glom import glom, T, Call, Coalesce
 
 from agir.authentication.models import Role
-from agir.notifications.models import Notification
+from agir.notifications.models import Announcement, Notification
+
+
+def add_announcements(person):
+    announcements = (
+        Announcement.objects.active()
+        .annotate(
+            already_added=Exists(
+                Notification.objects.filter(
+                    person=person, announcement_id=OuterRef("id")
+                )
+            )
+        )
+        .exclude(already_added=True)
+        .select_related("segment")
+    )
+
+    for gn in announcements:
+        if (
+            not gn.segment
+            or gn.segment.get_subscribers_queryset().filter(person=person).exists()
+        ):
+            Notification.objects.create(person=person, announcement=gn)
 
 
 def get_notifications(request):
@@ -10,30 +33,22 @@ def get_notifications(request):
         return []
 
     person = request.user.person
+    add_announcements(person)
 
-    statuses = {
-        ns["notification_id"]: ns["status"]
-        for ns in person.notification_statuses.all().values("notification_id", "status")
-    }
-
+    # All fields are either
     spec = [
         {
             "id": "id",
-            "content": "content",
-            "link": "link",
-            "icon": "icon",
-            "status": Call(statuses.get, args=(T.id, "U")),
+            "content": Coalesce("content", "announcement.content", skip="", default=""),
+            "icon": Coalesce("icon", "announcement.icon", skip="", default=""),
+            "link": Coalesce("link", "announcement.link", skip="", default=""),
+            "status": "status",
         }
     ]
 
-    instances = [
-        n
-        for n in Notification.objects.active().select_related("segment")
-        if not n.segment
-        or n.segment.get_subscribers_queryset().filter(pk=person.pk).exists()
-    ]
+    notifications = person.notifications.all().select_related("announcement")[:5]
 
-    return glom(instances, spec)
+    return glom(notifications, spec)
 
 
 class NotificationRequestManager:
--- agir/notifications/views.py
+++ agir/notifications/views.py
@@ -5,31 +5,21 @@ from rest_framework.exceptions import PermissionDenied
 from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 
-from agir.authentication.models import Role
-from agir.notifications.models import Notification, NotificationStatus
+from agir.notifications.models import Notification
 
 
 class FollowNotificationView(DetailView):
     model = Notification
+    queryset = Notification.objects.select_related("announcement")
 
     def get(self, request, *args, **kwargs):
-        self.notification = self.get_object()
+        notification = self.get_object()
 
-        if request.user.is_authenticated and request.user.type == Role.PERSON_ROLE:
-            try:
-                notification = NotificationStatus.objects.get(
-                    notification=self.notification, person=request.user.person
-                )
-            except NotificationStatus.DoesNotExist:
-                notification = NotificationStatus(
-                    notification=self.notification, person=request.user.person
-                )
+        if notification.status != Notification.STATUS_CLICKED:
+            notification.status = Notification.STATUS_CLICKED
+            notification.save()
 
-            if notification.status != NotificationStatus.STATUS_CLICKED:
-                notification.status = NotificationStatus.STATUS_CLICKED
-                notification.save()
-
-        return HttpResponseRedirect(self.notification.link)
+        return HttpResponseRedirect(notification.link or notification.announcement.link)
 
 
 class NotificationsSerializer(serializers.Serializer):
@@ -47,16 +37,11 @@ class NotificationsSeenView(GenericAPIView):
         if not request.user.is_authenticated or not request.user.person:
             raise PermissionDenied(detail="Pas authentifié", code="unauthenticated")
 
-        person = request.user.person
-
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
 
-        for id in serializer.data["notifications"]:
-            NotificationStatus.objects.get_or_create(
-                person=person,
-                notification_id=id,
-                defaults={"status": NotificationStatus.STATUS_SEEN},
-            )
+        Notification.objects.filter(
+            status=Notification.STATUS_UNSEEN, id__in=serializer.data["notifications"]
+        ).update(status=Notification.STATUS_SEEN)
 
         return Response(data={"code": "ok"}, status=200)
