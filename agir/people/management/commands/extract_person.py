12ba6e9f6a2613520cdd97cec1696c9e1e205e32
Arthur Cheysson <arthur@cheysson.fr>
2019-10-16 17:14:02 +0200

    feat(payments): mise à jour commande extraction des données

--- agir/people/management/commands/extract_person.py
+++ agir/people/management/commands/extract_person.py
@@ -1,20 +1,40 @@
 import json
-from urllib.parse import urljoin
 
 from django.conf import settings
-
+from django.contrib.gis.db.models import GeometryField
 from django.core.management.base import BaseCommand, CommandError
 from django.core.serializers.json import DjangoJSONEncoder
-from django.urls import reverse
+from django_countries.fields import CountryField
+from glom import glom, T, Coalesce, Literal
+from phonenumber_field.modelfields import PhoneNumberField
+from stdimage.models import StdImageField
+
+from agir.authentication.models import Role
+from agir.events.models import EventImage
+from agir.lib.utils import front_url
+from agir.payments.models import Payment, Subscription
+from agir.people.models import Person, PersonFormSubmission, PersonTag
 
 
-from agir.people.models import Person
+def field_expression(f):
+    if isinstance(f, PhoneNumberField):
+        return (f.name, Coalesce("as_e164", default=""))
+    elif isinstance(f, GeometryField):
+        return (f.name, str)
+    elif isinstance(f, CountryField):
+        return (f.name, str)
+    elif isinstance(f, StdImageField):
+        return (f.name, lambda u: settings.FRONT_DOMAIN + settings.MEDIA_URL + u.url)
+    elif f.name == "password":
+        return Literal("(caché)")
+
+    return f.name
 
 
-def get_all_fields(obj):
+def get_all_fields(m):
     return {
-        str(f.verbose_name): getattr(obj, f.name)
-        for f in obj._meta.get_fields()
+        str(f.verbose_name): field_expression(f)
+        for f in m._meta.get_fields()
         if not f.is_relation
     }
 
@@ -29,65 +49,48 @@ class Command(BaseCommand):
         except Person.DoesNotExist:
             raise CommandError("L'email donné est inconnu.")
 
-        extract = {}
-        personal = extract["information personnelle"] = {
-            "emails": [e.address for e in p.emails.all()],
-            **get_all_fields(p),
+        spec_role = get_all_fields(Role)
+
+        spec_event = {
+            "Nom": "name",
+            "URL": ("id", lambda id: front_url("view_event", args=[id])),
         }
-        personal["pays"] = str(personal["pays"])  # not serializable
+        spec_membership = {
+            "Nom": "supportgroup.name",
+            "URL": ("supportgroup.id", lambda id: front_url("view_group", args=[id])),
+            "Animateur": "is_referent",
+            "Gestionnaire": "is_manager",
+        }
 
-        org_events = extract["événements organisés"] = []
-        attended_events = extract["participations aux événements"] = []
-        memberships = extract["participations à des groupes"] = []
-        payments = extract["paiements"] = []
-        event_images = extract["photos postées"] = []
-        form_subs = extract["soumissions de formulaires"] = []
-        tags = extract["tags"] = []
+        spec_payment = get_all_fields(Payment)
+        spec_subscription = get_all_fields(Subscription)
+        spec_event_images = get_all_fields(EventImage)
+        spec_form_submissions = get_all_fields(PersonFormSubmission)
+        spec_tags = get_all_fields(PersonTag)
 
-        for m in p.memberships.select_related("supportgroup"):
-            memberships.append(
-                {
-                    "Nom du groupe": m.supportgroup.name,
-                    "URL du groupe": urljoin(
-                        settings.FRONT_DOMAIN,
-                        reverse("view_group", args=(m.supportgroup_id,)),
-                    ),
-                    **get_all_fields(m),
-                }
-            )
+        spec_person = {
+            **get_all_fields(Person),
+            "pays": ("location_country", str),
+            "Rôle": ("role", spec_role),
+            "événements organisés": ("organized_events", T.all(), [spec_event]),
+            "participations aux événements": (
+                "rsvps",
+                T.all(),
+                [("event", spec_event)],
+            ),
+            "participations à des groupes": ("memberships", T.all(), [spec_membership]),
+            "paiements": ("payments", T.all(), [spec_payment]),
+            "souscription au don mensuel": Coalesce(
+                ("subscription", spec_subscription), default=None
+            ),
+            "images d'événements": ("event_images", T.all(), [spec_event_images]),
+            "réponses à des formulaires": (
+                "form_submissions",
+                T.all(),
+                [spec_form_submissions],
+            ),
+            "libellés": ("tags", T.all(), [spec_tags]),
+        }
 
-        for o in p.organizer_configs.select_related("event"):
-            org_events.append(
-                {
-                    "Nom de l'événement": o.event.name,
-                    "URL de l'événement": urljoin(
-                        settings.FRONT_DOMAIN, reverse("view_event", args=(o.event,))
-                    )
-                    ** get_all_fields(o),
-                }
-            )
-
-        for s in p.rsvps.select_related("event"):
-            attended_events.append(
-                {
-                    "Nom de l'événement": s.event.name,
-                    "URL de l'événement": urljoin(
-                        settings.FRONT_DOMAIN, reverse("view_event", args=(s.event,))
-                    )
-                    ** get_all_fields(s),
-                }
-            )
-
-        for p in p.payments.all():
-            payments.append(get_all_fields(p))
-
-        for i in p.event_images.all():
-            event_images.append(get_all_fields(i))
-
-        for s in p.form_submissions.all():
-            form_subs.append(get_all_fields(s))
-
-        for t in p.tags.all():
-            tags.append(get_all_fields(t))
-
-        json.dump(extract, self.stdout, cls=DjangoJSONEncoder)
+        self.stdout.ending = ""
+        json.dump(glom(p, spec_person), self.stdout, cls=DjangoJSONEncoder)
