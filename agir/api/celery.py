0354697b510a97b8fe28aab693da25d88d069eec
Arthur Cheysson <arthur@cheysson.fr>
2019-11-12 15:14:20 +0100

    feat(celery): ajoute des tâches pour auditer la mémoire du worker

--- agir/api/celery.py
+++ agir/api/celery.py
@@ -1,3 +1,5 @@
+import logging
+
 import os
 from celery import Celery
 
@@ -14,6 +16,36 @@ app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
 
 
+logger = logging.getLogger("agir.api.celery")
+
+
+_memory_tracker = None
+
+
+def get_memory_tracker():
+    global _memory_tracker
+    if _memory_tracker is None:
+        from pympler.tracker import SummaryTracker
+
+        _memory_tracker = SummaryTracker()
+    return _memory_tracker
+
+
 @app.task(bind=True)
 def debug_task(self):
     print("Request: {0!r}".format(self.request))
+
+
+@app.task
+def memory_summary():
+    from pympler import muppy, summary
+
+    all_objects = muppy.get_objects()
+    obj_summary = summary.summarize(all_objects)
+
+    logger.info("\n".join(summary.format_(obj_summary)))
+
+
+@app.task
+def memory_tracker():
+    logger.info("\n".join(get_memory_tracker().format_diff()))
