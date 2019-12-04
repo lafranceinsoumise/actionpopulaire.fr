519d1fbba924586e34fa5341c49305d1be0c21a1
Arthur Cheysson <arthur@cheysson.fr>
2019-12-04 15:23:48 +0100

    fix(municipales): ajoute les options CORS pour la recherche

--- agir/municipales/urls.py
+++ agir/municipales/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from agir.municipales.views import CommuneView, SearchView
 
 urlpatterns = (
-    path("communes/search/", SearchView.as_view(), name="search_commune"),
+    path("communes/chercher/", SearchView.as_view(), name="search_commune"),
     path(
         "communes/<str:code_departement>/<str:slug>/",
         CommuneView.as_view(),
