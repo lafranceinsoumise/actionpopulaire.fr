f852c0fb8c42be9fa3380b56de5d7bd2ba79ea97
Jill Royer <hello@jillroyer.me>
2019-12-04 17:51:35 +0100

    fix: phone_argument and labels

--- agir/telegram/management/commands/telegram_login.py
+++ agir/telegram/management/commands/telegram_login.py
@@ -1,21 +1,21 @@
 from django.core.management import BaseCommand
 from pyrogram import Client
 
-from agir.lib.management_utils import person_argument
+from agir.lib.management_utils import phone_argument
 from agir.lib.phone_numbers import is_mobile_number
 from agir.telegram.models import api_params, TelegramSession
 
 
 class Command(BaseCommand):
-    help = "Create a Telegram Session for a user"
+    help = "Create a Telegram Session with a phone number"
     requires_migrations_checks = True
 
     def add_arguments(self, parser):
-        parser.add_argument("phone_number", type=str)
+        parser.add_argument("phone_number", type=phone_argument)
 
     def handle(self, phone_number, **options):
         if not is_mobile_number(phone_number):
-            raise ValueError("La personne doit avoir un numéro de téléphone mobile.")
+            raise ValueError("Le numéro doit être un numéro de téléphone mobile.")
 
         try:
             TelegramSession.objects.get(phone_number=phone_number)
@@ -26,7 +26,9 @@ class Command(BaseCommand):
         client = Client(":memory:", phone_number=str(phone_number), **api_params)
         client.start()
         session_string = client.export_session_string()
-        TelegramSession.objects.create(phone_number, session_string=session_string)
+        TelegramSession.objects.create(
+            phone_number=phone_number, session_string=session_string
+        )
 
         print("OK !")
         client.stop()
