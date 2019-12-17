f4c1191d14f84b1b5dd46274cd51654145644a98
Jill Royer <hello@jillroyer.me>
2019-12-17 15:23:35 +0100

    fix(carte): certaines vues n'aimaient pas ajjacio

--- agir/carte/urls.py
+++ agir/carte/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path("liste_groupes/", views.GroupsView.as_view(), name="group_list"),
     path("evenements/", views.EventMapView.as_view(), name="events_map"),
     path(
-        "evenements_commune/<int:departement>/<slug:nom>/",
+        "evenements_commune/<str:departement>/<slug:nom>/",
         views.CommuneEventMapView.as_view(),
         name="commune_events_map",
     ),
@@ -19,7 +19,7 @@ urlpatterns = [
     ),
     path("groupes/", views.GroupMapView.as_view(), name="groups_map"),
     path(
-        "groupes_commune/<int:departement>/<slug:nom>/",
+        "groupes_commune/<str:departement>/<slug:nom>/",
         views.CommuneGroupMapView.as_view(),
         name="commune_groups_map",
     ),
