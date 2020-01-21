fceafdf403622307aa1d5af0f341b3c70cf105e1
Arthur Cheysson <arthur@cheysson.fr>
2020-01-21 15:24:41 +0100

    Test: corrige les tests

---
+++ agir/donations/components/donationForm/TypeWidget.test.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { render, fireEvent, cleanup } from "@testing-library/react";
+import TypeWidget from "@agir/donations/donationForm/TypeWidget";
+
+afterEach(cleanup);
+
+const typeChoices = [
+  { value: "S", label: "une seule fois" },
+  { value: "M", label: "tous les mois" }
+];
+
+test("TypeWidget style le bouton quand c'est sélectionné", () => {
+  const component = render(<TypeWidget typeChoices={typeChoices} type={"S"} />);
+  expect(component.getByLabelText("une seule fois")).toBeChecked();
+  expect(component.getByLabelText("tous les mois")).not.toBeChecked();
+});
+
+test("TypeWidget réagit quand on clique", () => {
+  let currentType = null;
+  const component = render(
+    <TypeWidget
+      typeChoices={typeChoices}
+      onTypeChange={t => (currentType = t)}
+    />
+  );
+
+  const input = component.getByLabelText("tous les mois");
+  fireEvent.click(input);
+  expect(currentType).toBe("M");
+});
