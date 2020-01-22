20fd4ca6b13bb22de4302ea868c5f308d4c760ae
Arthur Cheysson <arthur@cheysson.fr>
2020-01-22 15:57:44 +0100

    Feat(municipales): Amélioration graphiques

--- agir/lib/components/masonry.js
+++ agir/lib/components/masonry.js
@@ -4,8 +4,13 @@ import Masonry from "masonry-layout";
 
 const elem = document.querySelector(".masonry");
 new Masonry(elem, {
-  itemSelector: ".col-md-6",
+  itemSelector: ".gblock",
   percentPosition: true
 });
+
+const blocks = document.querySelectorAll(".gblock");
+for (let i = 0; i < blocks.length; i++) {
+  blocks[i].style.float = "none";
+}
 
 $('[data-toggle="tooltip"]').tooltip();
