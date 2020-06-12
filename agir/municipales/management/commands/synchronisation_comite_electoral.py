c533ee1b54e0bee41ec72d32e8f196bbc6196ab4
Arthur Cheysson <arthur@cheysson.fr>
2020-06-12 20:29:46 +0200

    Feat(municipales): pages pour le 2ème tour

--- agir/municipales/management/commands/synchronisation_comite_electoral.py
+++ agir/municipales/management/commands/synchronisation_comite_electoral.py
@@ -1,5 +1,7 @@
 import pandas as pd
+from data_france.models import Commune
 from django.core.management import BaseCommand
+from django.utils.text import slugify
 
 from agir.municipales.models import CommunePage
 
@@ -13,9 +15,7 @@ class Command(BaseCommand):
     def handle(self, *args, source, **options):
         df = self.get_reference_file(source)
 
-        all_communes = {
-            c.code: c for c in CommunePage.objects.filter(code__in=df.index)
-        }
+        all_communes = self.get_pages_communes(df.index)
 
         liste = df["Nom de la liste 2nd tour"]
         liste = liste.where(df["published"], "")
@@ -32,8 +32,33 @@ class Command(BaseCommand):
                 commune.published = published
 
                 commune.save(
-                    update_fields=["liste_tour_2", "tete_liste_2", "published",]
+                    update_fields=["liste_tour_2", "tete_liste_tour_2", "published",]
                 )
+
+        CommunePage.objects.exclude(code__in=df.index).update(published=False)
+
+    def get_pages_communes(self, codes):
+        pages_commune = {c.code: c for c in CommunePage.objects.filter(code__in=codes)}
+        communes_a_creer = set(codes).difference(set(pages_commune))
+
+        for c in communes_a_creer:
+            try:
+                commune = Commune.objects.get(code=c, type=Commune.TYPE_COMMUNE)
+            except Commune.DoesNotExist:
+                try:
+                    commune = Commune.objects.get(code=c)
+                except Commune.DoesNotExist:
+                    continue
+
+            pages_commune[c] = CommunePage.objects.create(
+                code=commune.code,
+                code_departement=commune.code_departement,
+                name=commune.nom_complet,
+                coordinates=commune.geometry,
+                slug=slugify(commune.nom_complet),
+            )
+
+        return pages_commune
 
     @staticmethod
     def get_reference_file(source):
