4c75842274e0bf3b8f8dd9e5b713f43c0e32eff2
Jill Royer <hello@jillroyer.me>
2020-07-16 17:17:14 +0200

    fix(auth): right name for the metrics

--- agir/authentication/metrics.py
+++ agir/authentication/metrics.py
@@ -1,5 +1,5 @@
 from prometheus_client import Counter
 
-logged_in = Counter("agir_people_logged_in", "Connexions réussies", ["backend"])
-logged_out = Counter("agir_people_logged_out", "Déconnexions")
-login_failed = Counter("agir_people_login_failed", "Connexions échouées", ["backend"])
+logged_in = Counter("agir_auth_logged_in", "Connexions réussies", ["backend"])
+logged_out = Counter("agir_auth_logged_out", "Déconnexions")
+login_failed = Counter("agir_auth_login_failed", "Connexions échouées", ["backend"])
