72c66521ad2a70f91cfeda0ddac1f72af48ca44c
Arthur Cheysson <arthur@cheysson.fr>
2020-07-23 16:51:15 +0200

    Fix: prise en compte changement d'api de webpack-merge

--- webpack.production.js
+++ webpack.production.js
@@ -7,14 +7,14 @@ const common = require("./webpack.common.js");
 const STATIC_URL = "/static";
 
 common.module.rules.find(
-  r => r.loader === "file-loader"
+  (r) => r.loader === "file-loader"
 ).options.publicPath = path.join(STATIC_URL, "components", "files");
 
-module.exports = merge(common, {
+module.exports = merge.merge(common, {
   mode: "production",
   plugins: [
     new webpack.DefinePlugin({
-      "process.env.NODE_ENV": JSON.stringify("production")
-    })
-  ]
+      "process.env.NODE_ENV": JSON.stringify("production"),
+    }),
+  ],
 });
