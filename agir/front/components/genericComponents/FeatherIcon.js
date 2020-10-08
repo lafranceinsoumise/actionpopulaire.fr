e0cbdcd9883476600f1c3d230b5953f95d03e35b
Arthur Cheysson <arthur@cheysson.fr>
2020-10-08 12:19:12 +0200

    fix: taille responsive des icônes

--- agir/front/components/genericComponents/FeatherIcon.js
+++ agir/front/components/genericComponents/FeatherIcon.js
@@ -1,40 +1,49 @@
 import React from "react";
 import PropTypes from "prop-types";
+import styled from "styled-components";
 import { icons } from "feather-icons";
 
-import style from "./style.scss";
+import mainStyle from "./style.scss";
 
 export const allIcons = Object.keys(icons);
 
-export const RawFeatherIcon = ({
-  name,
-  color,
-  width,
-  height,
-  strokeWidth,
-  strokeLinecap,
-  strokeLinejoin,
-}) => {
-  const attrs = {
-    name,
-    color,
-    width,
-    height,
-    "stroke-width": strokeWidth,
-    "stroke-linecap": strokeLinecap,
-    "stroke-linejoin": strokeLinejoin,
-  };
+export const RawFeatherIcon = styled.div.attrs(
+  ({ name, color, strokeWidth, strokeLinecap, strokeLinejoin, svgStyle }) => {
+    const attrs = {
+      name,
+      color,
+      "stroke-width": strokeWidth,
+      "stroke-linecap": strokeLinecap,
+      "stroke-linejoin": strokeLinejoin,
+    };
+    Object.keys(attrs).map((k) => attrs[k] === undefined && delete attrs[k]);
 
-  Object.keys(attrs).map((k) => attrs[k] === undefined && delete attrs[k]);
+    if (svgStyle !== undefined) {
+      attrs.style = Object.keys(svgStyle)
+        .map((key) => {
+          const cssKey = key.replace(/[A-Z]/g, (m) => `-${m.toLowerCase()}`);
+          return `${cssKey}:${svgStyle[key]}`;
+        })
+        .join(";");
+    }
 
-  return (
-    <div
-      dangerouslySetInnerHTML={{
-        __html: icons[name].toSvg(attrs),
-      }}
-    />
-  );
-};
+    return {
+      dangerouslySetInnerHTML: { __html: icons[name].toSvg(attrs) },
+    };
+  }
+)`
+  display: inline-flex;
+  align-items: center;
+
+  svg {
+    position: relative;
+    top: ${(props) => props.vOffset || 0};
+
+    width: ${(props) => props.width};
+    height: ${(props) => props.height};
+  }
+`;
+
 RawFeatherIcon.propTypes = {
   name: PropTypes.oneOf(allIcons),
   color: PropTypes.string,
@@ -43,30 +52,48 @@ RawFeatherIcon.propTypes = {
   strokeWidth: PropTypes.number,
   strokeLinecap: PropTypes.number,
   strokeLinejoin: PropTypes.number,
+  style: PropTypes.objectOf(PropTypes.string),
 };
 
-const FeatherIcon = ({ name, type }) => {
-  const dimension = type === "normal" ? 24 : 16;
-  const color = type === "normal" ? style.brandBlack : style.gray;
+/*
+La taille de police de base est de 16 pixels.
+Les grands icônes doivent faire 24 pixels (1.5rem)
+Les petits icônes doivent faire 16 pixels (1rem)
+ */
+
+const FeatherIcon = ({ name, type, color }) => {
+  // stroke dimensions
   const strokeWidth = type === "normal" ? 2 : 1.33;
+
+  // positionning dimensions
+  const remDimension = type === "normal" ? 1.5 : 1;
+  const topValue = type === "normal" ? 0.3 : 0.175;
+
+  // color
+  color = color || (type === "normal" ? mainStyle.brandBlack : mainStyle.gray);
 
   return (
     <RawFeatherIcon
       name={name}
-      width={dimension}
-      height={dimension}
+      width={`${remDimension}rem`}
+      height={`${remDimension}rem`}
       color={color}
       strokeWidth={strokeWidth}
+      vOffset={`${topValue}rem`}
     />
   );
 };
 FeatherIcon.propTypes = {
   name: PropTypes.oneOf(allIcons),
   type: PropTypes.oneOf(["normal", "small"]),
+  color: PropTypes.string,
 };
 
 FeatherIcon.defaultProps = {
   type: "normal",
 };
+
+FeatherIcon.RawFeatherIcon = RawFeatherIcon;
+FeatherIcon.allIcons = allIcons;
 
 export default FeatherIcon;
--- agir/front/components/genericComponents/FeatherIcon.stories.js
+++ agir/front/components/genericComponents/FeatherIcon.stories.js
@@ -1,15 +1,26 @@
 import React from "react";
-import FeatherIcon, { allIcons } from "./FeatherIcon";
+import FeatherIcon, { allIcons, RawFeatherIcon } from "./FeatherIcon";
 
 export default {
   component: FeatherIcon,
+  subcomponents: { RawFeatherIcon },
   title: "FeatherIcon",
   argTypes: {
     name: {
+      name: "Nom de l'icône",
       control: {
         type: "select",
         options: allIcons,
       },
+    },
+    color: {
+      name: "Couleur de l'icône",
+      control: {
+        type: "color",
+      },
+    },
+    type: {
+      name: "Type d'icône",
     },
   },
 };
@@ -26,3 +37,25 @@ Small.args = {
   ...Default.args,
   type: "small",
 };
+
+export const WithText = ({ text, fontSize, ...args }) => (
+  <div style={{ fontSize: `${fontSize}px` }}>
+    <FeatherIcon {...args} /> <span>{text}</span>
+  </div>
+);
+WithText.args = {
+  ...Default.args,
+  text: "Texte",
+  fontSize: 16,
+};
+
+WithText.argTypes = {
+  text: {
+    type: "string",
+    name: "Texte",
+  },
+  fontSize: {
+    name: "Taille de la police du texte",
+  },
+};
+WithText.storyName = "Avec du texte";
