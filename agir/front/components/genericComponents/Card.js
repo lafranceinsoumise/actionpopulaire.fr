a69cbe027181fb81995e653b178eae8f42749247
Jill Royer <hello@jillroyer.me>
2020-10-08 11:17:12 +0200

    feat: basic Card

---
+++ agir/front/components/genericComponents/Card.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const Card = styled.section`
+  box-shadow: 0px 0px 3px rgba(0, 0, 0, 0.1), 0px 3px 2px rgba(0, 0, 0, 0.05);
+  background: #ffffff;
+  padding: 24px;
+`;
+
+export default Card;
---
+++ agir/front/components/genericComponents/Card.stories.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Card from "./Card";
+
+export default {
+  component: Card,
+  title: "Card",
+};
+
+const Template = (args) => (
+  <div style={{ maxWidth: "400px", padding: "20px" }}>
+    <Card {...args} />
+  </div>
+);
+
+export const SimpleText = Template.bind({});
+SimpleText.args = {
+  children:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod " +
+    "tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim " +
+    "veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea " +
+    "commodo consequat. Duis aute irure dolor in reprehenderit in voluptate " +
+    "velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat " +
+    "cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+};
