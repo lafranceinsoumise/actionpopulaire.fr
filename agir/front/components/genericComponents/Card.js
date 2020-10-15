d531cb6e9c5d861eb864c554e26e608181f3df49
Jill Royer <hello@jillroyer.me>
2020-10-15 16:46:04 +0200

    feat: border radius on card

--- agir/front/components/genericComponents/Card.js
+++ agir/front/components/genericComponents/Card.js
@@ -4,6 +4,7 @@ const Card = styled.section`
   box-shadow: 0px 0px 3px rgba(0, 0, 0, 0.1), 0px 3px 2px rgba(0, 0, 0, 0.05);
   background: #ffffff;
   padding: 24px;
+  border-radius: 8px;
 `;
 
 export default Card;
