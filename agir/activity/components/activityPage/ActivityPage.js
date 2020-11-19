6df48f8eabd0a24a5617e4290bfca0c720680b0b
Giuseppe De Ponte <giuseppedeponte@gmail.com>
2020-11-19 18:05:18 +0100

    fix: activity list page layout + styles

--- agir/activity/components/activityPage/ActivityPage.js
+++ agir/activity/components/activityPage/ActivityPage.js
@@ -1,11 +1,25 @@
 import React from "react";
 
-import Layout from "@agir/front/dashboardComponents/Layout";
+import Layout, { LayoutTitle } from "@agir/front/dashboardComponents/Layout";
 import ActivityList from "@agir/activity/activityPage/ActivityList";
 
+import styled from "styled-components";
+import style from "@agir/front/genericComponents/_variables.scss";
+
+const Page = styled.article`
+  margin: 0;
+
+  @media (max-width: ${style.collapse}px) {
+    margin: 25px 0;
+  }
+`;
+
 const ActivityPage = (props) => (
-  <Layout active="activity" title="Notifications">
-    <ActivityList {...props} />
+  <Layout active="activity">
+    <Page>
+      <LayoutTitle>Notifications</LayoutTitle>
+      <ActivityList {...props} />
+    </Page>
   </Layout>
 );
 
