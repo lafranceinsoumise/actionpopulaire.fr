7d4d6b946387a98c3313a02f6f4a9116ec5ecb16
Giuseppe De Ponte <giuseppedeponte@gmail.com>
2020-11-19 12:29:47 +0100

    feat: fix desktop app menu on the page

--- agir/front/components/dashboardComponents/Layout.js
+++ agir/front/components/dashboardComponents/Layout.js
@@ -21,14 +21,56 @@ const LayoutTitle = styled.h1`
   }
 `;
 
+const MainColumn = styled(Column)``;
+
+const MainContainer = styled(Container)`
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: 100vh;
+  width: 100%;
+  overflow: hidden;
+  padding-top: 72px;
+  padding-bottom: 72px;
+
+  & > ${Row} > ${Column} {
+    max-height: calc(100vh - 72px);
+    overflow-x: hidden;
+    overflow-y: auto;
+    padding-top: 72px;
+
+    @media (max-width: ${style.collapse}px) {
+      padding-top: 0;
+    }
+  }
+
+  & > ${Row} > ${MainColumn} {
+    height: calc(100vh - 72px);
+
+    @media (max-width: ${style.collapse}px) {
+      padding-top: 24px;
+      padding-bottom: 100px;
+      height: calc(100vh - 144px);
+    }
+
+    & > section {
+      @media (min-width: ${style.collapse}px) {
+        max-width: 711px;
+      }
+    }
+  }
+`;
+
 const Layout = (props) => (
   <GrayBackground>
-    <Container style={{ paddingTop: "64px" }}>
+    <MainContainer>
       <Row gutter={72}>
         <Column>
           <Navigation {...props} />
         </Column>
-        <Column grow style={{ minHeight: "100vh" }}>
+        <MainColumn grow>
           <section>
             {props.title ? (
               <header>
@@ -37,9 +79,9 @@ const Layout = (props) => (
             ) : null}
             {props.children}
           </section>
-        </Column>
+        </MainColumn>
       </Row>
-    </Container>
+    </MainContainer>
   </GrayBackground>
 );
 
