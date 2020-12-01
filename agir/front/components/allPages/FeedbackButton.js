b17a45f0072646eb4b84aec98e976effc117cf96
Giuseppe De Ponte <giuseppedeponte@gmail.com>
2020-12-01 12:18:01 +0100

    feat: add FeedbackButton component

---
+++ agir/front/components/allPages/FeedbackButton.js
@@ -0,0 +1,137 @@
+import PropTypes from "prop-types";
+import React from "react";
+import { useTransition, animated } from "react-spring";
+import styled from "styled-components";
+
+import style from "@agir/front/genericComponents/_variables.scss";
+
+import { useGlobalContext } from "@agir/front/genericComponents/GlobalContext";
+
+import background from "./feedback-form-button.svg";
+import closeButton from "./close-btn.svg";
+
+const slideInTransition = {
+  from: { opacity: 0, bottom: "-15rem" },
+  enter: { opacity: 1, bottom: "1rem" },
+  leave: { opacity: 0, bottom: "-15rem" },
+};
+
+const fadeInTransition = {
+  from: { opacity: 0 },
+  enter: { opacity: 1 },
+  leave: { opacity: 0 },
+  delay: 200,
+};
+
+const Tooltip = styled(animated.p)`
+  position: absolute;
+  top: -10px;
+  right: 0;
+  transform: translate(-26px, -100%);
+  right: 0;
+  width: 175px;
+  background-color: ${style.black1000};
+  color: white;
+  padding: 1rem;
+  display: flex;
+  flex-flow: column nowrap;
+  font-size: 13px;
+  line-height: 1.3;
+
+  strong {
+    font-size: 14px;
+  }
+
+  button {
+    position: absolute;
+    top: 0;
+    right: 0;
+    transform: translate(50%, -50%);
+    width: 24px;
+    height: 24px;
+    background-color: transparent;
+    border: none;
+    background-image: url(${closeButton});
+    background-repeat: no-repeat;
+    background-position: center center;
+    background-size: cover;
+    cursor: pointer;
+  }
+
+  :after {
+    content: "";
+    position: absolute;
+    top: 100%;
+    right: 0;
+    margin-left: -4px;
+    border-width: 4px;
+    border-style: solid;
+    border-color: black black transparent transparent;
+  }
+`;
+
+const Button = styled.a`
+  display: block;
+  width: 100%;
+  height: 100%;
+  border-radius: 100%;
+  background-color: transparent;
+  background-image: url(${background});
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center center;
+`;
+
+const Wrapper = styled(animated.div)`
+  position: fixed;
+  bottom: 1rem;
+  right: 1rem;
+  width: 53px;
+  height: 53px;
+  z-index: ${style.zindexFeedbackButton};
+`;
+
+export const FeedbackButton = (props) => {
+  const { isActive, href } = props;
+  const [hasTooltip, setHasTooltip] = React.useState(false);
+  const handleCloseTooltip = React.useCallback(() => {
+    setHasTooltip(false);
+  }, []);
+  const handleOpenTooltip = React.useCallback(() => {
+    setHasTooltip(true);
+  }, []);
+
+  const tooltipTransition = useTransition(hasTooltip, null, fadeInTransition);
+  const wrapperTransition = useTransition(isActive, null, {
+    ...slideInTransition,
+    onRest: handleOpenTooltip,
+  });
+
+  return wrapperTransition.map(({ item, key, props }) =>
+    item ? (
+      <Wrapper key={key} style={props}>
+        {tooltipTransition.map(({ item, key, props }) =>
+          item ? (
+            <Tooltip key={key} style={props}>
+              <strong>Aidez-nous !</strong>
+              <span>Donnez votre avis sur le nouveau site →</span>
+              <button aria-label="Cacher" onClick={handleCloseTooltip} />
+            </Tooltip>
+          ) : null
+        )}
+        <Button href={href} aria-label="Je donne mon avis" />
+      </Wrapper>
+    ) : null
+  );
+};
+FeedbackButton.propTypes = {
+  isActive: PropTypes.bool,
+  href: PropTypes.string.isRequired,
+};
+const ConnectedFeedbackButton = (props) => {
+  const { routes } = useGlobalContext();
+  const href = routes && routes.feedbackForm;
+
+  return <FeedbackButton {...props} href={href} isActive />;
+};
+export default ConnectedFeedbackButton;
---
+++ agir/front/components/allPages/FeedbackButton.stories.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { FeedbackButton } from "./FeedbackButton";
+
+export default {
+  component: FeedbackButton,
+  title: "Layout/FeedbackButton",
+};
+
+const Template = (args) => <FeedbackButton {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  isActive: true,
+  href: "#feedback-form",
+};
