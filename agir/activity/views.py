c85f1aeb23748f3f1053ca89aff59537c538e4db
Arthur Cheysson <arthur@cheysson.fr>
2020-12-09 17:42:45 +0100

    feat: vue pour mettre à jour le statut des activités

--- agir/activity/views.py
+++ agir/activity/views.py
@@ -1,10 +1,15 @@
 from django.http import HttpResponseRedirect
 from django.views.generic import DetailView
-from rest_framework.generics import RetrieveUpdateAPIView
+from rest_framework import status
+from rest_framework.generics import RetrieveUpdateAPIView, GenericAPIView
+from rest_framework.response import Response
 
 from agir.activity.models import Activity, Announcement
-from agir.activity.serializers import ActivitySerializer
-from agir.lib.rest_framework_permissions import GlobalOrObjectPermissions
+from agir.activity.serializers import ActivitySerializer, ActivityStatusUpdateRequest
+from agir.lib.rest_framework_permissions import (
+    GlobalOrObjectPermissions,
+    IsPersonPermission,
+)
 
 
 class ActivityAPIView(RetrieveUpdateAPIView):
@@ -20,3 +25,19 @@ class AnnouncementLinkView(DetailView):
     def get(self, request, *args, **kwargs):
         announcement = self.get_object()
         return HttpResponseRedirect(announcement.link)
+
+
+class ActivityStatusUpdateView(GenericAPIView):
+    serializer_class = ActivityStatusUpdateRequest
+    permission_classes = (IsPersonPermission,)
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        Activity.objects.filter(
+            recipient=request.user.person,  # pour ne laisser la possibilité de modifier que ses propres activités
+            id__in=serializer.validated_data["ids"],
+        ).update(status=serializer.validated_data["status"])
+
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
