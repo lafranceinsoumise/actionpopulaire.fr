2f6a5c417251e34443be7b9d77c60908d95bf030
Giuseppe De Ponte <giuseppedeponte@gmail.com>
2020-12-14 17:13:20 +0100

    fix: remove urijs and migrate uri utils to URL API

--- agir/lib/components/utils/url.test.js
+++ agir/lib/components/utils/url.test.js
@@ -1,38 +1,38 @@
-import { addQueryStringParams } from "./uri.js";
+import { addQueryStringParams } from "./url.js";
 
-describe("URI utils", function () {
+describe("URL utils", function () {
   describe("addQueryStringParams", function () {
-    it("should return an empty string if argument uri is falsy", function () {
+    it("should return an empty string if argument url is falsy", function () {
       const args = [null, {}];
       const expected = "";
       const result = addQueryStringParams(...args);
       expect(result).toEqual(expected);
     });
-    it("should return argument uri if argument params is falsy", function () {
+    it("should return argument url if argument params is falsy", function () {
       const args = ["https://actionpopulaire.fr/", null];
       const expected = args[0];
       const result = addQueryStringParams(...args);
       expect(result).toEqual(expected);
     });
-    it("should return argument uri if argument params type is not 'object'", function () {
+    it("should return argument url if argument params type is not 'object'", function () {
       const args = ["https://actionpopulaire.fr/", "not an object"];
       const expected = args[0];
       const result = addQueryStringParams(...args);
       expect(result).toEqual(expected);
     });
-    it("should return argument uri if argument params type has no properties", function () {
+    it("should return argument url if argument params type has no properties", function () {
       const args = ["https://actionpopulaire.fr/", {}];
       const expected = args[0];
       const result = addQueryStringParams(...args);
       expect(result).toEqual(expected);
     });
-    it("should add query string params to argument uri if none are already present", function () {
+    it("should add query string params to argument url if none are already present", function () {
       const args = ["https://actionpopulaire.fr/", { a: "a" }];
       const expected = "https://actionpopulaire.fr/?a=a";
       const result = addQueryStringParams(...args);
       expect(result).toEqual(expected);
     });
-    it("should add query string params to argument uri if some are present", function () {
+    it("should add query string params to argument url if some are present", function () {
       const args = ["https://actionpopulaire.fr/?a=a", { b: "b" }];
       const expected = "https://actionpopulaire.fr/?a=a&b=b";
       const result = addQueryStringParams(...args);
