4151d360c72789ea3e88481bf2d9289a828bc09c
Giuseppe De Ponte <giuseppedeponte@gmail.com>
2020-12-14 14:17:10 +0100

    feat: add `uri` helper function module
    - Add uri module
    - Create an `addQueryStringParams` function to create/update an URI search string
    - Install `urijs` module

---
+++ agir/lib/components/utils/uri.test.js
@@ -0,0 +1,48 @@
+import { addQueryStringParams } from "./uri.js";
+
+describe("URI utils", function () {
+  describe("addQueryStringParams", function () {
+    it("should return an empty string if argument uri is falsy", function () {
+      const args = [null, {}];
+      const expected = "";
+      const result = addQueryStringParams(...args);
+      expect(result).toEqual(expected);
+    });
+    it("should return argument uri if argument params is falsy", function () {
+      const args = ["https://actionpopulaire.fr/", null];
+      const expected = args[0];
+      const result = addQueryStringParams(...args);
+      expect(result).toEqual(expected);
+    });
+    it("should return argument uri if argument params type is not 'object'", function () {
+      const args = ["https://actionpopulaire.fr/", "not an object"];
+      const expected = args[0];
+      const result = addQueryStringParams(...args);
+      expect(result).toEqual(expected);
+    });
+    it("should return argument uri if argument params type has no properties", function () {
+      const args = ["https://actionpopulaire.fr/", {}];
+      const expected = args[0];
+      const result = addQueryStringParams(...args);
+      expect(result).toEqual(expected);
+    });
+    it("should add query string params to argument uri if none are already present", function () {
+      const args = ["https://actionpopulaire.fr/", { a: "a" }];
+      const expected = "https://actionpopulaire.fr/?a=a";
+      const result = addQueryStringParams(...args);
+      expect(result).toEqual(expected);
+    });
+    it("should add query string params to argument uri if some are present", function () {
+      const args = ["https://actionpopulaire.fr/?a=a", { b: "b" }];
+      const expected = "https://actionpopulaire.fr/?a=a&b=b";
+      const result = addQueryStringParams(...args);
+      expect(result).toEqual(expected);
+    });
+    it("should prefer arguent params values if they conflicts with argument url already present params", function () {
+      const args = ["https://actionpopulaire.fr/?a=a", { a: "b" }];
+      const expected = "https://actionpopulaire.fr/?a=b";
+      const result = addQueryStringParams(...args);
+      expect(result).toEqual(expected);
+    });
+  });
+});
