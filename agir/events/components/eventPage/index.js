4f255d293e4e3bf635f061b49078f0fd746a403c
Jill Royer <hello@jillroyer.me>
2021-01-09 19:15:33 +0100

    chore: remove useless entry points (-6Mo)

--- agir/events/components/eventPage/index.js
+++
@@ -1,3 +0,0 @@
-import { renderWithContext } from "@agir/lib/utils/react";
-
-renderWithContext(import("./EventPage").then((module) => module.default));
--- agir/groups/components/groupsPage/index.js
+++
@@ -1,3 +0,0 @@
-import { renderWithContext } from "@agir/lib/utils/react";
-
-renderWithContext(import("./GroupsPage").then((module) => module.default));
