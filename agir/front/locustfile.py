ee573d82aefe0d02375f698f9886965dc9c62a22
Arthur Cheysson <arthur@cheysson.fr>
2021-01-11 11:41:12 +0100

    feat: première locustfile qui charge les pages principales

---
+++ agir/front/locustfile.py
@@ -0,0 +1,21 @@
+from locust import TaskSet, task
+
+
+class FrontTaskSet(TaskSet):
+    @task
+    def interrompre(self):
+        self.interrupt()
+
+    @task(2)
+    def dashboard(self):
+        self.client.get("/")
+        self.client.get("/api/evenements/rsvped")
+        self.client.get("/api/evenements/suggestions")
+        self.client.get("/api/session")
+
+    @task(2)
+    def groupes(self):
+        self.client.get("/mes-groupes/")
+        self.client.get("/api/groupes/")
+        self.client.get("/api/session/")
+        self.client.get("/carte/groupes")
