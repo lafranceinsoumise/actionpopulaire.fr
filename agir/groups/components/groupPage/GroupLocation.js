9c5450eb18774156d111e298ee675ed88af93ef4
Giuseppe De Ponte <giuseppedeponte@gmail.com>
2021-01-12 11:02:59 +0100

    feat: create GroupLocation component

---
+++ agir/groups/components/groupPage/GroupLocation.js
@@ -0,0 +1,141 @@
+import PropTypes from "prop-types";
+import React from "react";
+import styled from "styled-components";
+
+import style from "@agir/front/genericComponents/_variables.scss";
+
+import Card from "@agir/front/genericComponents/Card";
+import Map from "@agir/carte/common/Map";
+import FeatherIcon from "@agir/front/genericComponents/FeatherIcon";
+
+const StyledMap = styled.div``;
+const StyledAddress = styled.div``;
+const StyledCard = styled(Card)`
+  padding-top: 1.5rem;
+  padding-bottom: 1.5rem;
+
+  & > * {
+    margin-left: 0.5rem;
+    margin-right: 0.5rem;
+  }
+
+  h4 {
+    font-size: 1rem;
+    font-weight: 600;
+    margin-top: 0.5rem;
+    margin-bottom: 1rem;
+  }
+
+  ${StyledMap} {
+    height: 308px;
+
+    @media (max-width: ${style.collapse}px) {
+      height: 155px;
+    }
+  }
+
+  ${StyledAddress} {
+    margin-top: 1rem;
+    margin-bottom: 0.5rem;
+    display: flex;
+    flex-flow: row nowrap;
+    align-items: flex-start;
+
+    p {
+      margin-left: 0.5rem;
+      display: flex;
+      flex-flow: column nowrap;
+      font-size: 0.875rem;
+      color: ${style.black500};
+      line-height: 1.5;
+      font-weight: 400;
+
+      span {
+        &:first-child {
+          font-weight: 500;
+          color: ${style.black1000};
+        }
+      }
+
+      &:last-child {
+        margin-left: auto;
+      }
+
+      a {
+        font-weight: 500;
+        text-decoration: none;
+        color: ${style.primary500};
+      }
+    }
+  }
+`;
+
+const GroupLocation = (props) => {
+  const { location, iconConfiguration } = props;
+
+  if (!location || Object.values(location).filter(Boolean).length === 0) {
+    return null;
+  }
+
+  const {
+    name,
+    address1,
+    address2,
+    city,
+    zip,
+    state,
+    country,
+    coordinates,
+  } = location;
+
+  return (
+    <StyledCard>
+      <h4>Accès</h4>
+      {Array.isArray(coordinates) ? (
+        <StyledMap>
+          <Map center={coordinates} iconConfiguration={iconConfiguration} />
+        </StyledMap>
+      ) : null}
+      <StyledAddress>
+        <FeatherIcon name="map-pin" small inline />
+        <p>
+          {name && <span>{name}</span>}
+          {address1 && <span>{address1}</span>}
+          {address2 && <span>{address2}</span>}
+          {(zip || city) && (
+            <span>{[zip, city].filter(Boolean).join(" ")}</span>
+          )}
+          {(state || country) && (
+            <span>{[state, country].filter(Boolean).join(", ")}</span>
+          )}
+        </p>
+        <p>
+          {Array.isArray(coordinates) ? (
+            <a
+              href={`https://www.google.com/maps/dir/?api=1&destination=${
+                coordinates[1] + "," + coordinates[0]
+              }`}
+            >
+              Itinéraire
+            </a>
+          ) : null}
+        </p>
+      </StyledAddress>
+    </StyledCard>
+  );
+};
+
+GroupLocation.propTypes = {
+  iconConfiguration: PropTypes.object,
+  location: PropTypes.shape({
+    name: PropTypes.string,
+    address1: PropTypes.string,
+    address2: PropTypes.string,
+    city: PropTypes.string,
+    zip: PropTypes.string,
+    state: PropTypes.string,
+    country: PropTypes.string,
+    coordinates: PropTypes.arrayOf(PropTypes.number),
+  }).isRequired,
+};
+export default GroupLocation;
---
+++ agir/groups/components/groupPage/GroupLocation.stories.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+import GroupLocation from "./GroupLocation";
+
+export default {
+  component: GroupLocation,
+  title: "Group/GroupLocation",
+};
+
+const Template = (args) => {
+  return (
+    <GroupLocation
+      {...args}
+      location={{
+        name: args.locationName,
+        address1: args.locationAddress1,
+        address2: args.locationAddress2,
+        city: args.locationCity,
+        zip: args.locationZip,
+        state: args.locationState,
+        country: args.locationCountry,
+        coordinates:
+          args.latitude && args.longitude
+            ? [args.latitude, args.longitude]
+            : null,
+      }}
+    />
+  );
+};
+
+export const Default = Template.bind({});
+Default.args = {
+  name: "Comités d'appui et de travail pour une Vienne Insoumise",
+  locationName: "Le café de la gare",
+  locationAddress1: "1 Place de la République",
+  locationAddress2: "Au fond à gauche",
+  locationCity: "Poitiers",
+  locationZip: "86000",
+  locationState: "Île-de-France",
+  locationCountry: "France",
+  latitude: -97.14704,
+  longitude: 49.8844,
+  iconConfiguration: { color: "#49b37d", iconName: "book" },
+};
