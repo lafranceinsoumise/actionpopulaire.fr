35ac0ac2d8b05fec0be50412c10bb3bd142a1710
Giuseppe De Ponte <giuseppedeponte@gmail.com>
2021-01-13 18:04:48 +0100

    feat: create GroupPage components

---
+++ agir/groups/components/groupPage/GroupPage/GroupPage.js
@@ -0,0 +1,33 @@
+import PropTypes from "prop-types";
+import React from "react";
+
+import { ResponsiveLayout } from "@agir/front/genericComponents/grid";
+import { PageFadeIn } from "@agir/front/genericComponents/PageFadeIn";
+
+import DesktopGroupPage, { DesktopGroupPageSkeleton } from "./DesktopGroupPage";
+import MobileGroupPage, { MobileGroupPageSkeleton } from "./MobileGroupPage";
+
+export const GroupPage = (props) => {
+  const { isLoading } = props;
+  return (
+    <PageFadeIn
+      ready={!isLoading}
+      wait={
+        <ResponsiveLayout
+          DesktopLayout={DesktopGroupPageSkeleton}
+          MobileLayout={MobileGroupPageSkeleton}
+        />
+      }
+    >
+      <ResponsiveLayout
+        {...props}
+        MobileLayout={MobileGroupPage}
+        DesktopLayout={DesktopGroupPage}
+      />
+    </PageFadeIn>
+  );
+};
+GroupPage.propTypes = {
+  isLoading: PropTypes.bool,
+};
+export default GroupPage;
