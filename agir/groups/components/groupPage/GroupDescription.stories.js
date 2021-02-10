89e7aad8d8e55c8ea56cbbf60d301d9857fa206f
Giuseppe De Ponte <giuseppedeponte@gmail.com>
2021-02-10 17:46:10 +0100

    feat: style GroupDescription links

--- agir/groups/components/groupPage/GroupDescription.stories.js
+++ agir/groups/components/groupPage/GroupDescription.stories.js
@@ -15,7 +15,7 @@ export const Default = Template.bind({});
 Default.args = {
   description: `
   <p>
-    Chocolate cake gummies fruitcake. Lemon drops chocolate bar jelly beans danish candy bonbon. Cotton candy dessert bonbon topping macaroon. <strong>Marzipan croissant apple pie powder halvah brownie bear claw.</strong> Carrot cake jelly-o tootsie roll cake jelly beans. Croissant gingerbread icing pudding ice cream candy canes. Candy canes wafer caramels gingerbread.
+    <a href="#">Chocolate cake gummies</a> fruitcake. Lemon drops chocolate bar jelly beans danish candy bonbon. Cotton candy dessert bonbon topping macaroon. <strong>Marzipan croissant apple pie powder halvah brownie bear claw.</strong> Carrot cake jelly-o tootsie roll cake jelly beans. Croissant gingerbread icing pudding ice cream candy canes. Candy canes wafer caramels gingerbread.
   </p>
   <p>
     Ice cream biscuit brownie gingerbread liquorice soufflé wafer soufflé. <a href="#">Fruitcake cake lollipop</a>. Gummies sesame snaps chocolate bar soufflé jelly-o candy marzipan sweet jujubes. Lemon drops cheesecake wafer chocolate bar dessert pie sesame snaps fruitcake. Brownie gummies marshmallow chocolate halvah macaroon.
