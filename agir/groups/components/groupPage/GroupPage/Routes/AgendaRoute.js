db2e722efa037f6773ff98b8de62ba00e52b70a1
Giuseppe De Ponte <giuseppedeponte@gmail.com>
2021-02-10 18:13:14 +0100

    feat: remove responsive layout for group page Agenda tab

--- agir/groups/components/groupPage/GroupPage/Routes/AgendaRoute.js
+++ agir/groups/components/groupPage/GroupPage/Routes/AgendaRoute.js
@@ -1,18 +1,13 @@
 import PropTypes from "prop-types";
 import React from "react";
 
-import { ResponsiveLayout } from "@agir/front/genericComponents/grid";
-import ShareCard from "@agir/front/genericComponents/ShareCard";
-
 import GroupEventList from "@agir/groups/groupPage/GroupEventList";
-import GroupLocation from "@agir/groups/groupPage/GroupLocation";
-import GroupDescription from "@agir/groups/groupPage/GroupDescription";
 import {
   MemberEmptyEvents,
   ManagerEmptyEvents,
 } from "@agir/groups/groupPage/EmptyContent";
 
-const MobileAgendaRoute = ({
+const AgendaRoute = ({
   group,
   allEvents,
   upcomingEvents,
@@ -41,7 +36,7 @@ const MobileAgendaRoute = ({
     ) : null}
   </>
 );
-MobileAgendaRoute.propTypes = {
+AgendaRoute.propTypes = {
   group: PropTypes.object,
   allEvents: PropTypes.arrayOf(PropTypes.object),
   upcomingEvents: PropTypes.arrayOf(PropTypes.object),
@@ -50,57 +45,5 @@ MobileAgendaRoute.propTypes = {
   loadMorePastEvents: PropTypes.func,
   hasTabs: PropTypes.bool,
 };
-
-const DesktopAgendaRoute = ({
-  group,
-  allEvents,
-  upcomingEvents,
-  pastEvents,
-  loadMorePastEvents,
-  isLoadingPastEvents,
-  hasTabs,
-}) =>
-  Array.isArray(upcomingEvents) &&
-  Array.isArray(pastEvents) &&
-  allEvents.length === 0 ? (
-    <>
-      {hasTabs && group.isManager ? <ManagerEmptyEvents /> : null}
-      {hasTabs && !group.isManager && group.isMember ? (
-        <MemberEmptyEvents />
-      ) : null}
-      <GroupDescription {...group} maxHeight="auto" />
-      <ShareCard title="Inviter vos ami·es à rejoindre le groupe" />
-      <GroupLocation {...group} />
-    </>
-  ) : (
-    <>
-      <GroupEventList title="Événements à venir" events={upcomingEvents} />
-      <GroupEventList
-        title="Événements passés"
-        events={pastEvents}
-        loadMore={loadMorePastEvents}
-        isLoading={isLoadingPastEvents}
-      />
-      <GroupLocation {...group} />
-      <ShareCard title="Partager le lien du groupe" />
-    </>
-  );
-DesktopAgendaRoute.propTypes = {
-  group: PropTypes.object,
-  allEvents: PropTypes.arrayOf(PropTypes.object),
-  upcomingEvents: PropTypes.arrayOf(PropTypes.object),
-  pastEvents: PropTypes.arrayOf(PropTypes.object),
-  isLoadingPastEvents: PropTypes.bool,
-  loadMorePastEvents: PropTypes.func,
-  hasTabs: PropTypes.bool,
-};
-
-const AgendaRoute = (props) => (
-  <ResponsiveLayout
-    MobileLayout={MobileAgendaRoute}
-    DesktopLayout={DesktopAgendaRoute}
-    {...props}
-  />
-);
 
 export default AgendaRoute;
