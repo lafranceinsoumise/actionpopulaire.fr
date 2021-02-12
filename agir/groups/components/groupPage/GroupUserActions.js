afe6f2555cde7a54c09fca719ae195764f30a79e
Giuseppe De Ponte <giuseppedeponte@gmail.com>
2021-02-12 10:38:08 +0100

    fix: restore GroupUserActions Member version on small screens

--- agir/groups/components/groupPage/GroupUserActions.js
+++ agir/groups/components/groupPage/GroupUserActions.js
@@ -10,7 +10,85 @@ import FeatherIcon, {
   RawFeatherIcon,
 } from "@agir/front/genericComponents/FeatherIcon";
 import Popin from "@agir/front/genericComponents/Popin";
+import BottomSheet from "@agir/front/genericComponents/BottomSheet";
+import { ResponsiveLayout } from "@agir/front/genericComponents/grid";
 
+const StyledList = styled.ul`
+  width: 100%;
+  cursor: pointer;
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: stretch;
+  list-style: none;
+  color: ${style.primary500};
+  padding: 0;
+
+  @media (max-width: ${style.collapse}px) {
+    margin: 0;
+    padding: 1.5rem;
+  }
+
+  a {
+    display: flex;
+    align-items: center;
+    border: none;
+    padding: 0;
+    margin: 0;
+    text-decoration: none;
+    background: inherit;
+    cursor: pointer;
+    text-align: center;
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    font-size: 0.875rem;
+    line-height: 20px;
+    font-weight: 400;
+    color: ${style.black1000};
+    margin-bottom: 0.5rem;
+
+    &:last-child {
+      margin-bottom: 0;
+    }
+
+    &:hover,
+    &:focus {
+      text-decoration: underline;
+      border: none;
+      outline: none;
+    }
+
+    &[disabled],
+    &[disabled]:hover,
+    &[disabled]:focus {
+      opacity: 0.75;
+      text-decoration: none;
+      cursor: default;
+    }
+
+    @media (max-width: ${style.collapse}px) {
+      margin-bottom: 1.5rem;
+      text-decoration: none;
+    }
+
+    & > *:first-child {
+      margin-right: 0.5rem;
+      width: 1rem;
+      height: 1rem;
+
+      @media (max-width: ${style.collapse}px) {
+        margin-right: 1rem;-
+        width: 1.5rem;
+        height: 1.5rem;
+      }
+
+      svg {
+        width: inherit;
+        height: inherit;
+        stroke-width: 2;
+      }
+    }
+  }
+`;
 const StyledPanel = styled.div`
   width: 100%;
   background-color: ${style.primary100};
@@ -102,198 +180,221 @@ const StyledContent = styled.div`
       margin-bottom: 0;
     }
   }
-
-  ul {
-    list-style: none;
-    padding: 0;
-    margin: 0;
-
-    li {
-      font-size: 0.813rem;
-      line-height: 1.3;
-      display: flex;
-      align-items: baseline;
-
-      a {
-        color: ${style.black1000};
-        margin-left: 0.5rem;
-      }
-    }
-
-    li + li {
-      margin-top: 0.5rem;
-    }
-  }
 `;
 
-const GroupLinks = (props) => {
-  const { isMember, isManager, is2022 = false, routes } = props;
-
-  const [isPopinOpen, setIsPopinOpen] = useState(false);
-  const openPopin = useCallback(() => setIsPopinOpen(true), []);
-  const closePopin = useCallback(() => setIsPopinOpen(false), []);
-
-  if (isManager) {
-    return (
-      <StyledContent>
-        <StyledPanel>
-          <h6>Gestion du groupe</h6>
-          {routes.createEvent && (
-            <Button
-              as="a"
-              href={routes.createEvent}
-              color="primary"
-              icon="plus"
-              small
-              inline
-            >
-              Créer un événement {is2022 ? "de l'équipe" : "du groupe"}
-            </Button>
-          )}
-          <ul>
-            {routes.members && (
-              <li>
-                <RawFeatherIcon
-                  small
-                  inline
-                  color={style.primary500}
-                  name="users"
-                />
-                <a href={routes.members}>Membres</a>
-              </li>
-            )}
-            {routes.settings && (
-              <li>
-                <RawFeatherIcon
-                  inline
-                  small
-                  name="file-text"
-                  color={style.primary500}
-                />
-                <a href={routes.settings}>Informations</a>
-              </li>
-            )}
-            {routes.members && (
-              <li>
-                <RawFeatherIcon
-                  small
-                  inline
-                  color={style.primary500}
-                  name="lock"
-                />
-                <a href={routes.members}>Animateur·ices et gestionnaires</a>
-              </li>
-            )}
-            {routes.financement && (
-              <li>
-                <RawFeatherIcon
-                  small
-                  inline
-                  color={style.primary500}
-                  name="folder"
-                />
-                <a href={routes.financement}>Remboursement et dépense</a>
-              </li>
-            )}
-            {routes.certification && (
-              <li>
-                <RawFeatherIcon
-                  small
-                  inline
-                  color={style.primary500}
-                  name="check-circle"
-                />
-                <a href={routes.certification}>Certification</a>
-              </li>
-            )}
-            {routes.financement && (
-              <li>
-                <RawFeatherIcon
-                  small
-                  inline
-                  color={style.primary500}
-                  name="loader"
-                />
-                <a href={routes.financement}>Financement</a>
-              </li>
-            )}
-            {routes.invitation && (
-              <li>
-                <RawFeatherIcon
-                  small
-                  inline
-                  color={style.primary500}
-                  name="users"
-                />
-                <a href={routes.invitation}>Inviter</a>
-              </li>
-            )}
-            {routes.admin && (
-              <li>
-                <RawFeatherIcon
-                  inline
-                  small
-                  name="settings"
-                  color={style.primary500}
-                />
-                <a href={routes.admin}>Administration</a>
-              </li>
-            )}
-          </ul>
-        </StyledPanel>
-        <Button
-          as="a"
-          href={routes.createEvent}
-          color="primary"
-          icon="plus"
-          small
-        >
-          Créer un événement {is2022 ? "de l'équipe" : "du groupe"}
-        </Button>
-        <Button as="a" href={routes.settings} icon="settings" small>
-          Gestion {is2022 ? "de l'équipe" : "du groupe"}
-        </Button>
-      </StyledContent>
-    );
-  }
-
-  if (isMember && routes.quit) {
-    return (
-      <StyledContent hideOnMobile>
-        <div
-          style={{
-            width: "100%",
-            position: "relative",
-            margin: "0 0 1rem",
-          }}
-        >
-          <Button onClick={openPopin} color="default" icon="check">
-            Vous êtes membre {is2022 ? "de l'équipe" : "du groupe"}
-          </Button>
-          <Popin isOpen={isPopinOpen} onDismiss={closePopin} position="bottom">
-            <ul>
-              <li>
-                <FeatherIcon
-                  small
-                  inline
-                  color={style.primary500}
-                  name="users"
-                />
-                &emsp;
-                <a href={routes.quit}>
-                  Quitter {is2022 ? "l'équipe" : "le groupe"}
-                </a>
-              </li>
-            </ul>
-          </Popin>
-        </div>
-      </StyledContent>
-    );
-  }
+const ManagerActions = (props) => {
+  const { is2022 = false, routes } = props;
 
   return (
     <StyledContent>
-      <CSRFProtectedForm method="post" action={routes.join || ""}>
+      <StyledPanel>
+        <h6>Gestion du groupe</h6>
+        {routes.createEvent && (
+          <Button
+            as="a"
+            href={routes.createEvent}
+            color="primary"
+            icon="plus"
+            small
+            inline
+          >
+            Créer un événement {is2022 ? "de l'équipe" : "du groupe"}
+          </Button>
+        )}
+        <ul>
+          {routes.members && (
+            <li>
+              <RawFeatherIcon
+                small
+                inline
+                color={style.primary500}
+                name="users"
+              />
+              <a href={routes.members}>Membres</a>
+            </li>
+          )}
+          {routes.settings && (
+            <li>
+              <RawFeatherIcon
+                inline
+                small
+                name="file-text"
+                color={style.primary500}
+              />
+              <a href={routes.settings}>Informations</a>
+            </li>
+          )}
+          {routes.members && (
+            <li>
+              <RawFeatherIcon
+                small
+                inline
+                color={style.primary500}
+                name="lock"
+              />
+              <a href={routes.members}>Animateur·ices et gestionnaires</a>
+            </li>
+          )}
+          {routes.financement && (
+            <li>
+              <RawFeatherIcon
+                small
+                inline
+                color={style.primary500}
+                name="folder"
+              />
+              <a href={routes.financement}>Remboursement et dépense</a>
+            </li>
+          )}
+          {routes.certification && (
+            <li>
+              <RawFeatherIcon
+                small
+                inline
+                color={style.primary500}
+                name="check-circle"
+              />
+              <a href={routes.certification}>Certification</a>
+            </li>
+          )}
+          {routes.financement && (
+            <li>
+              <RawFeatherIcon
+                small
+                inline
+                color={style.primary500}
+                name="loader"
+              />
+              <a href={routes.financement}>Financement</a>
+            </li>
+          )}
+          {routes.invitation && (
+            <li>
+              <RawFeatherIcon
+                small
+                inline
+                color={style.primary500}
+                name="users"
+              />
+              <a href={routes.invitation}>Inviter</a>
+            </li>
+          )}
+          {routes.admin && (
+            <li>
+              <RawFeatherIcon
+                inline
+                small
+                name="settings"
+                color={style.primary500}
+              />
+              <a href={routes.admin}>Administration</a>
+            </li>
+          )}
+        </ul>
+      </StyledPanel>
+      <Button
+        as="a"
+        href={routes.createEvent}
+        color="primary"
+        icon="plus"
+        small
+      >
+        Créer un événement {is2022 ? "de l'équipe" : "du groupe"}
+      </Button>
+      <Button as="a" href={routes.settings} icon="settings" small>
+        Gestion {is2022 ? "de l'équipe" : "du groupe"}
+      </Button>
+    </StyledContent>
+  );
+};
+
+const MobileMemberActions = (props) => {
+  const { is2022 = false, routes, isMenuOpen, openMenu, closeMenu } = props;
+
+  return (
+    <div
+      style={{
+        width: "100%",
+        position: "relative",
+        margin: "0 0 1rem",
+        textAlign: "center",
+      }}
+    >
+      <Button onClick={openMenu} color="default" icon="check" small>
+        Vous êtes membre {is2022 ? "de l'équipe" : "du groupe"}
+      </Button>
+      <BottomSheet isOpen={isMenuOpen} onDismiss={closeMenu} position="bottom">
+        <StyledList>
+          <li>
+            <a href={routes.quit}>
+              <FeatherIcon small inline color={style.primary500} name="users" />
+              Quitter {is2022 ? "l'équipe" : "le groupe"}
+            </a>
+          </li>
+        </StyledList>
+      </BottomSheet>
+    </div>
+  );
+};
+const DesktopMemberActions = (props) => {
+  const { is2022 = false, routes, isMenuOpen, openMenu, closeMenu } = props;
+
+  return (
+    <div
+      style={{
+        width: "100%",
+        position: "relative",
+        margin: "0 0 1rem",
+      }}
+    >
+      <Button onClick={openMenu} color="default" icon="check">
+        Vous êtes membre {is2022 ? "de l'équipe" : "du groupe"}
+      </Button>
+      <Popin isOpen={isMenuOpen} onDismiss={closeMenu} position="bottom">
+        <StyledList>
+          <li>
+            <a href={routes.quit}>
+              <FeatherIcon small inline color={style.primary500} name="users" />
+              Quitter {is2022 ? "l'équipe" : "le groupe"}
+            </a>
+          </li>
+        </StyledList>
+      </Popin>
+    </div>
+  );
+};
+DesktopMemberActions.propTypes = MobileMemberActions.propTypes = {
+  is2022: PropTypes.bool,
+  routes: PropTypes.object,
+  isMenuOpen: PropTypes.bool,
+  openMenu: PropTypes.func,
+  closeMenu: PropTypes.func,
+};
+const MemberActions = (props) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const openMenu = useCallback(() => setIsMenuOpen(true), []);
+  const closeMenu = useCallback(() => setIsMenuOpen(false), []);
+
+  return (
+    <StyledContent>
+      <ResponsiveLayout
+        {...props}
+        MobileLayout={MobileMemberActions}
+        DesktopLayout={DesktopMemberActions}
+        isMenuOpen={isMenuOpen}
+        openMenu={openMenu}
+        closeMenu={closeMenu}
+      />
+    </StyledContent>
+  );
+};
+
+const NonMemberActions = (props) => {
+  const { is2022 = false, routes } = props;
+
+  return (
+    <StyledContent>
+      <CSRFProtectedForm method="post" action={(routes && routes.join) || ""}>
         <input type="hidden" name="action" value="join" />
         <Button type="submit" color="success">
           Rejoindre {is2022 ? "l'équipe" : "le groupe"}
@@ -304,10 +405,20 @@ const GroupLinks = (props) => {
   );
 };
 
-GroupLinks.propTypes = {
+const GroupUserActions = (props) => {
+  if (props.isManager) {
+    return <ManagerActions {...props} />;
+  }
+  if (props.isMember) {
+    return <MemberActions {...props} />;
+  }
+  return <NonMemberActions {...props} />;
+};
+
+ManagerActions.propTypes = MemberActions.propTypes = NonMemberActions.propTypes = GroupUserActions.propTypes = {
   isMember: PropTypes.bool,
   isManager: PropTypes.bool,
   is2022: PropTypes.bool,
   routes: PropTypes.object,
 };
-export default GroupLinks;
+export default GroupUserActions;
