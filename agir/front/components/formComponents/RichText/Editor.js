e78203883419520738e3a5aff486619b2d70bc8d
Giuseppe De Ponte <giuseppedeponte@gmail.com>
2021-03-03 11:44:51 +0100

    feat: create RichTextField component

---
+++ agir/front/components/formComponents/RichText/Editor.js
@@ -0,0 +1,200 @@
+import PropTypes from "prop-types";
+import React, { useCallback, useState } from "react";
+import ReactQuill from "react-quill";
+import styled from "styled-components";
+
+import style from "@agir/front/genericComponents/_variables.scss";
+
+import "react-quill/dist/quill.snow.css";
+
+const StyledEditor = styled(ReactQuill)`
+  z-index: 0;
+  width: 100%;
+  min-height: 80px;
+  padding: 0;
+  font-size: 1rem;
+  border: 1px solid;
+  border-color: ${({ $invalid, $focused, readOnly }) => {
+    if ($invalid) {
+      return style.redNSP;
+    }
+    if (readOnly) {
+      return style.black100;
+    }
+    if ($focused) {
+      return style.black1000;
+    }
+    return style.black100;
+  }};
+
+  @media (max-width: ${style.collapse}px) {
+    font-size: 0.875rem;
+  }
+
+  && > .ql-container {
+    border: none;
+    padding: 1rem;
+
+    .ql-blank::before {
+      font-style: normal;
+      top: 0;
+      left: 0;
+      padding: 1rem;
+    }
+
+    .ql-editor {
+      padding: 0;
+      font-family: ${style.fontFamilyBase};
+      font-size: 0.875rem;
+      line-height: ${style.lineHeightBase};
+      color: ${style.textColor};
+      h1,
+      h2,
+      h3,
+      h4,
+      h5,
+      h6 {
+        font-weight: 600;
+        line-height: 1.2;
+        color: #00232c;
+        margin-top: 0.5em;
+        margin-bottom: 0.7em;
+      }
+      h1 {
+        font-size: 2.563em;
+      }
+      h2 {
+        font-size: 2.125em;
+      }
+      h3 {
+        font-size: 1.125em;
+      }
+      h4 {
+        font-size: 1.125em;
+      }
+      h5 {
+        font-size: 1em;
+      }
+      h6 {
+        font-size: 0.875em;
+      }
+      p {
+        margin: 0 0 0.688em;
+        a,
+        a:hover,
+        a:focus {
+          color: inherit;
+          text-decoration: underline;
+        }
+      }
+      a {
+        color: ${style.linkColor};
+        text-decoration: none;
+        font-weight: 600;
+
+        &:hover,
+        &:focus {
+          color: ${style.linkHoverColor};
+          text-decoration: ${style.linkHoverDecoration};
+        }
+
+        &:focus {
+          outline: 5px auto -webkit-focus-ring-color;
+          outline-offset: -2px;
+        }
+      }
+      strong {
+        font-weight: 600;
+      }
+    }
+  }
+
+  && > .ql-toolbar {
+    border: none;
+    border-bottom: 1px solid ${style.black100};
+
+    @media (max-width: ${style.collapse}px) {
+      display: ${({ $focused }) => ($focused ? "block" : "none")};
+      position: fixed;
+      bottom: 0;
+      left: 0;
+      right: 0;
+      z-index: 1;
+      border: none;
+      background-color: white;
+      box-shadow: 0px -3px 3px rgba(0, 35, 44, 0.1),
+        0px 2px 0px rgba(0, 35, 44, 0.08);
+    }
+
+    .ql-null {
+      display: none;
+    }
+
+    button {
+      color: ${style.black500};
+
+      .ql-fill {
+        fill: ${style.black500};
+      }
+      .ql-stroke {
+        stroke: ${style.black500};
+      }
+
+      &.ql-active,
+      &:hover,
+      &:focus {
+        color: ${style.black1000};
+        .ql-fill {
+          fill: ${style.black1000};
+        }
+        .ql-stroke {
+          stroke: ${style.black1000};
+        }
+      }
+    }
+  }
+`;
+
+const MODULES = {
+  toolbar: [
+    "null",
+    { header: 1 },
+    { header: 2 },
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "link",
+  ],
+};
+
+const Editor = (props) => {
+  const { value, onChange, disabled, hasError, ...rest } = props;
+  const [isFocused, setIsFocused] = useState(false);
+  const handleFocus = useCallback(() => setIsFocused(true), []);
+  const handleBlur = useCallback(() => setIsFocused(false), []);
+
+  return (
+    <StyledEditor
+      {...rest}
+      $valid={!hasError}
+      $invalid={hasError}
+      $empty={!value}
+      $focused={isFocused}
+      value={value}
+      onChange={onChange}
+      readOnly={!disabled}
+      theme="snow"
+      modules={MODULES}
+      onFocus={handleFocus}
+      onBlur={handleBlur}
+    />
+  );
+};
+Editor.propTypes = {
+  value: PropTypes.string,
+  onChange: PropTypes.func.isRequired,
+  disabled: PropTypes.bool,
+  hasError: PropTypes.bool,
+};
+export default Editor;
---
+++ agir/front/components/formComponents/RichText/RichTextField.js
@@ -0,0 +1,98 @@
+import PropTypes from "prop-types";
+import React from "react";
+import styled from "styled-components";
+
+import style from "@agir/front/genericComponents/_variables.scss";
+
+import Editor from "./Editor";
+
+const StyledLabel = styled.span``;
+const StyledHelpText = styled.span``;
+const StyledInput = styled.div``;
+const StyledError = styled.span``;
+
+const StyledField = styled.label`
+  display: grid;
+  grid-template-columns: 1fr auto;
+  grid-template-rows: auto auto auto auto;
+  grid-gap: 0 0.75rem;
+  align-items: stretch;
+  font-size: 0.813rem;
+  font-weight: 400;
+  line-height: 1;
+
+  & > * {
+    margin: 4px 0;
+  }
+
+  ${StyledLabel} {
+    grid-row: 1;
+    grid-column: 1/3;
+    font-weight: 600;
+  }
+  ${StyledHelpText} {
+    grid-row: 2;
+    grid-column: 1/3;
+  }
+
+  ${StyledInput} {
+    grid-row: 3;
+    grid-column: 1/3;
+  }
+
+  ${StyledError} {
+    display: ${({ $invalid }) => ($invalid ? "flex" : "none")};
+    grid-row: 4;
+    grid-column: 1/3;
+    color: ${style.redNSP};
+  }
+`;
+
+const RichTextField = (props) => {
+  const {
+    id,
+    value,
+    onChange,
+    error,
+    label,
+    helpText,
+    disabled,
+    placeholder,
+  } = props;
+
+  return (
+    <StyledField
+      htmlFor={id}
+      $valid={!error}
+      $invalid={!!error}
+      $empty={!!value}
+    >
+      {label && <StyledLabel>{label}</StyledLabel>}
+      {helpText && <StyledHelpText>{helpText}</StyledHelpText>}
+      <StyledInput>
+        <Editor
+          id={id}
+          disabled={disabled}
+          onChange={onChange}
+          value={value}
+          hasError={!!error}
+          placeholder={placeholder}
+        />
+      </StyledInput>
+      <StyledError>{error}</StyledError>
+    </StyledField>
+  );
+};
+
+RichTextField.propTypes = {
+  value: PropTypes.any,
+  onChange: PropTypes.func.isRequired,
+  id: PropTypes.string,
+  label: PropTypes.string,
+  helpText: PropTypes.string,
+  error: PropTypes.string,
+  disabled: PropTypes.bool,
+  placeholder: PropTypes.string,
+};
+
+export default RichTextField;
