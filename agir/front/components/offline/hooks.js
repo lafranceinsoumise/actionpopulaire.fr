8b0c965f4717c4f8ecc3ea86d10953aa52db1c0c
Giuseppe De Ponte <giuseppedeponte@gmail.com>
2021-03-05 14:14:55 +0100

    fix: do not show connectivity warning on page unload

--- agir/front/components/offline/hooks.js
+++ agir/front/components/offline/hooks.js
@@ -1,9 +1,29 @@
+import { useCallback, useEffect, useRef, useState } from "react";
 import useSWR from "swr";
 
 export const useIsOffline = () => {
   const { data, error, isValidating } = useSWR("/api/session");
+  const [isOffline, setIsOffline] = useState(false);
+  const timeout = useRef(null);
+  const clear = useCallback(() => {
+    if (timeout.current) {
+      clearTimeout(timeout.current);
+      timeout.current = null;
+    }
+  }, [timeout]);
+  useEffect(() => {
+    const isOffline = !data && !error && isValidating ? null : !!error;
 
-  if (!data && !error && isValidating) return null;
+    if (isOffline !== false) {
+      setIsOffline(isOffline);
+    } else {
+      timeout.current = setTimeout(() => {
+        setIsOffline(isOffline);
+      }, 1000);
+    }
 
-  return !!error;
+    return clear;
+  }, [clear, data, error, isValidating]);
+
+  return isOffline;
 };
