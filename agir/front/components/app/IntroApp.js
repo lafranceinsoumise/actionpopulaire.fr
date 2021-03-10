634def63752f7150d6e7ef11366a6f3085c9be2f
MaximeG <mgoaziou@gmail.com>
2021-03-10 15:13:39 +0100

    feat introApp: fix opti & add last view

--- agir/front/components/app/IntroApp.js
+++ agir/front/components/app/IntroApp.js
@@ -1,10 +1,9 @@
-import { useState } from "react";
-import React from "react";
+import React, { useState, useCallback } from "react";
 import Button from "@agir/front/genericComponents/Button";
 import styled from "styled-components";
 import style from "@agir/front/genericComponents/_variables.scss";
-
 import img1 from "../genericComponents/images/introApp1.jpg";
+import logo from "../genericComponents/images/logoActionPopulaire.png";
 
 const Mark = styled.span`
   width: 24px;
@@ -28,79 +27,158 @@ const Block = styled.div`
   height: 100vh;
 `;
 
+const InlineBlock = styled.span`
+  display: inline-block;
+`;
+
+const items = [
+  {
+    name: "Rencontrez",
+    description: "d'autres membres et agissez ensemble !",
+    image: img1,
+  },
+  {
+    name: "Agissez concrètement",
+    description: "formez-vous et commandez du matériel, tracts et affiches !",
+    image: img1,
+  },
+  {
+    name: "Organisez et rejoignez",
+    description: "une équipe de soutien autour de vous !",
+    image: img1,
+  },
+];
+
 const IntroApp = () => {
   const [index, setIndex] = useState(0);
-  const [showConnexion, setShowConnexion] = useState(false);
 
-  const items = [
-    {
-      name: "Rencontrez",
-      description: "d'autres membres et agissez ensemble !",
-      image: img1,
-    },
-    {
-      name: "Agissez concrètement",
-      description: "formez-vous et commandez du matériel, tracts et affiches !",
-      image: img1,
-    },
-    {
-      name: "Organisez et rejoignez",
-      description: "une équipe de soutien autour de vous !",
-      image: img1,
-    },
-  ];
+  const handleClick = useCallback(() => {
+    setIndex((index) => index + 1);
+  }, []);
 
-  const handleClick = () => {
-    if (index >= items.length - 1) {
-      console.log("Redirect into app !");
-      return;
-    }
-    setIndex(index + 1);
-  };
+  const handleClickBack = useCallback(() => {
+    setIndex((index) => index - 1);
+  }, []);
+
+  const showConnexion = index >= items.length;
 
   return (
     <>
       {!showConnexion && (
         <Block>
-          <img src={items[index].image} alt="" style={{ maxWidth: "100%" }} />
+          <div style={{ padding: "1.4rem" }}>
+            <img src={items[index].image} alt="" style={{ maxWidth: "100%" }} />
 
-          <p
-            style={{
-              color: style.primary500,
-              fontWeight: 700,
-              fontSize: "1.75rem",
-            }}
-          >
-            {items[index].name}
-          </p>
+            <p
+              style={{
+                color: style.primary500,
+                fontWeight: 700,
+                fontSize: "1.75rem",
+              }}
+            >
+              {items[index].name}
+            </p>
 
-          <p style={{ fontSize: "1.375rem", marginTop: "0.375rem" }}>
-            {items[index].description}
-          </p>
+            <p style={{ fontSize: "1.375rem", marginTop: "0.375rem" }}>
+              {items[index].description}
+            </p>
 
-          <Button
-            color="secondary"
-            onClick={() => handleClick()}
-            style={{
-              marginTop: "2.5rem",
-              maxWidth: "100%",
-              width: "330px",
-              justifyContent: "center",
-            }}
-          >
-            Continuer
-          </Button>
+            <Button
+              color="secondary"
+              onClick={handleClick}
+              style={{
+                marginTop: "2.5rem",
+                maxWidth: "100%",
+                width: "330px",
+                justifyContent: "center",
+              }}
+            >
+              Continuer
+            </Button>
 
-          <div style={{ marginTop: "2.5rem" }}></div>
+            <div style={{ marginTop: "2.5rem" }}></div>
 
-          <div style={{ postition: "absolute", bottom: "3rem" }}>
-            <Mark $active={0 === index}></Mark>
-            <Mark $active={1 === index}></Mark>
-            <Mark $active={2 === index}></Mark>
+            <div style={{ postition: "absolute", bottom: "3rem" }}>
+              <Mark $active={0 === index}></Mark>
+              <Mark $active={1 === index}></Mark>
+              <Mark $active={2 === index}></Mark>
+            </div>
           </div>
         </Block>
       )}
-      {showConnexion && <div>CONNEXION !</div>}
+      {showConnexion && (
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justify: "center",
+            alignItems: "center",
+          }}
+        >
+          <div
+            style={{
+              backgroundColor: style.secondary500,
+              padding: "1.4rem",
+              // height: "618px",
+              display: "flex",
+              justifyContent: "center",
+              flexDirection: "column",
+              alignItems: "center",
+              position: "relative",
+              width: "100%",
+              textAlign: "center",
+            }}
+          >
+            <img src={logo} alt="" style={{ maxWidth: "400px" }} />
+            <p style={{ fontSize: "1.375rem" }}>
+              Agissez concrètement dans votre quartier et faites gagner Jean-Luc
+              Mélenchon
+              <InlineBlock>en 2022 !</InlineBlock>
+            </p>
+            {/* <div style={{overflow:"none", backgroundColor: "transparent", postition: "absolute", 
+                bottom: "0px", left: "0px"}}>
+                <div style={{position:"absolute", bottom:"0", width: "200%", height: "100px", backgroundColor: "white",
+                  border: "1px solid red", transform: "rotate(-10deg)" }}>
+                </div>
+              </div> */}
+          </div>
+
+          <div
+            style={{
+              marginBottom: "40px",
+              display: "flex",
+              flexAlign: "center",
+              flexDirection: "column",
+            }}
+          >
+            <Button
+              color="primary"
+              onClick={handleClickBack}
+              style={{
+                marginTop: "2.5rem",
+                maxWidth: "100%",
+                width: "330px",
+                justifyContent: "center",
+              }}
+            >
+              Je crée mon compte
+            </Button>
+            <Button
+              color="secondary"
+              onClick={handleClickBack}
+              style={{
+                marginTop: "0.5rem",
+                marginLeft: "0px",
+                maxWidth: "100%",
+                width: "330px",
+                justifyContent: "center",
+              }}
+            >
+              Je dispose déjà d'un compte
+            </Button>
+          </div>
+        </div>
+      )}
     </>
   );
 };
