b2974cb47768f9e159f06ccbd1fb7f5a37746984
Giuseppe De Ponte <giuseppedeponte@gmail.com>
2021-03-12 15:06:28 +0100

    feat: update authentication api helpers

--- agir/front/components/authentication/api.js
+++ agir/front/components/authentication/api.js
@@ -4,18 +4,22 @@ export const ENDPOINT = {
   login: "/api/connexion/",
   checkCode: "/api/connexion/code/",
   logout: "/api/deconnexion",
+  signUp: "api/people/subscription/",
+  getProfile: "/api/profil/",
+  getProfileOptions: "/api/profil/",
+  updateProfile: "/api/profil/",
 };
 
 export const login = async (email) => {
   const result = {
-    data: null,
+    success: false,
     error: null,
   };
   const url = ENDPOINT.login;
   const body = { email };
   try {
     const response = await axios.post(url, body);
-    result.data = response.data;
+    result.success = response.status === 200;
   } catch (e) {
     result.error = (e.response && e.response.data) || e.message;
   }
@@ -25,14 +29,14 @@ export const login = async (email) => {
 
 export const checkCode = async (code) => {
   const result = {
-    data: null,
+    success: false,
     error: null,
   };
   const url = ENDPOINT.checkCode;
   const body = { code };
   try {
     const response = await axios.post(url, body);
-    result.data = response.data;
+    result.success = response.status === 200;
   } catch (e) {
     result.error = (e.response && e.response.data) || e.message;
   }
@@ -42,12 +46,76 @@ export const checkCode = async (code) => {
 
 export const logout = async () => {
   const result = {
-    data: null,
+    success: false,
     error: null,
   };
   const url = ENDPOINT.checkCode;
   try {
     const response = await axios.get(url);
+    result.success = response.status === 200;
+  } catch (e) {
+    result.error = (e.response && e.response.data) || e.message;
+  }
+
+  return result;
+};
+
+export const signUp = async (data) => {
+  const result = {
+    data: null,
+    error: null,
+  };
+  const url = ENDPOINT.signUp;
+  try {
+    const response = await axios.post(url, data);
+    result.data = response.data;
+  } catch (e) {
+    result.error = (e.response && e.response.data) || e.message;
+  }
+
+  return result;
+};
+
+export const getProfile = async () => {
+  const result = {
+    data: null,
+    error: null,
+  };
+  const url = ENDPOINT.getProfile;
+  try {
+    const response = await axios.get(url);
+    result.data = response.data;
+  } catch (e) {
+    result.error = (e.response && e.response.data) || e.message;
+  }
+
+  return result;
+};
+
+export const getProfileOptions = async () => {
+  const result = {
+    data: null,
+    error: null,
+  };
+  const url = ENDPOINT.getProfileOptions;
+  try {
+    const response = await axios.options(url);
+    result.data = response.data;
+  } catch (e) {
+    result.error = (e.response && e.response.data) || e.message;
+  }
+
+  return result;
+};
+
+export const updateProfile = async (data) => {
+  const result = {
+    data: null,
+    error: null,
+  };
+  const url = ENDPOINT.updateProfile;
+  try {
+    const response = await axios.patch(url, data);
     result.data = response.data;
   } catch (e) {
     result.error = (e.response && e.response.data) || e.message;
