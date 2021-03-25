954708018bd7cf2845cc35e0bcfe1feadb40a587
MaximeG <mgoaziou@gmail.com>
2021-03-25 17:46:27 +0100

    merge intro 3

--- agir/front/components/app/IntroApp.js
+++ agir/front/components/app/IntroApp.js
@@ -165,14 +165,13 @@ const items = [
     name: "Agissez concrètement",
     description: (
       <>
-        participez aux action, diffusez notre programme et commandez&nbsp;
-        <InlineBlock>du matériel</InlineBlock>
+        participez aux action, diffusez notre programme et commandez&nbsp;<InlineBlock>du matériel</InlineBlock>
       </>
     ),
     image: img2,
   },
   {
-    name: "Organisez et rejoignez",
+    name: "Rejoignez ou créer",
     description: (
       <>
         une équipe de soutien&nbsp;<InlineBlock>autour de vous !</InlineBlock>
