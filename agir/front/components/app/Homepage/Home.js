7b18041f7ff20703efc6b04190427c8eb41529c2
Giuseppe De Ponte <giuseppedeponte@gmail.com>
2021-03-26 17:41:43 +0100

    fix: agenda & home paged padding top

--- agir/front/components/app/Homepage/Home.js
+++ agir/front/components/app/Homepage/Home.js
@@ -1,4 +1,7 @@
 import React from "react";
+import styled from "styled-components";
+
+import style from "@agir/front/genericComponents/_variables.scss";
 
 import IntroApp from "@agir/front/app/IntroApp";
 import Footer from "@agir/front/dashboardComponents/Footer";
@@ -10,6 +13,14 @@ import { useMobileApp } from "@agir/front/app/hooks";
 import MobileHome from "./MobileHome";
 import DesktopHome from "./DesktopHome";
 
+const StyledHome = styled.div`
+  padding-top: 72px;
+
+  @media (max-width: ${style.collapse}px) {
+    padding-top: 56px;
+  }
+`;
+
 const Home = (props) => {
   const { isMobileApp } = useMobileApp();
 
@@ -20,12 +31,14 @@ const Home = (props) => {
   return (
     <>
       <TopBar />
-      <ResponsiveLayout
-        MobileLayout={MobileHome}
-        DesktopLayout={DesktopHome}
-        {...props}
-      />
-      <Footer hideBanner />
+      <StyledHome>
+        <ResponsiveLayout
+          MobileLayout={MobileHome}
+          DesktopLayout={DesktopHome}
+          {...props}
+        />
+        <Footer hideBanner />
+      </StyledHome>
     </>
   );
 };
