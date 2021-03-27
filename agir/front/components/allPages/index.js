a7d442c38ec21806aa079798b34601f4be6ed8d7
Jill Royer <hello@jillroyer.me>
2021-03-27 22:29:44 +0100

    feat: remove unused turbolinks

--- agir/front/components/allPages/index.js
+++ agir/front/components/allPages/index.js
@@ -1,4 +1,3 @@
-import "./style.css";
 import "../genericComponents/style.scss";
 
 import "./renderTopBar";
--- agir/lib/middleware.py
+++ agir/lib/middleware.py
@@ -5,19 +5,6 @@ from django.conf import settings
 from django.utils.http import urlquote
 
 
-class TurbolinksMiddleware:
-    def __init__(self, get_response):
-        self.get_response = get_response
-
-    def __call__(self, request):
-        response = self.get_response(request)
-
-        location = urljoin(settings.FRONT_DOMAIN, urlquote(request.path))
-        response["Turbolinks-Location"] = location
-
-        return response
-
-
 class NoVaryCookieMiddleWare:
     cc_delim_re = re.compile(r"\s*,\s*")
 
