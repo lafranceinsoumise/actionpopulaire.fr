953d7a5a84919ea772ecc4e2bf40a3ce84ea6e69
Giuseppe De Ponte <giuseppedeponte@gmail.com>
2021-03-29 16:36:41 +0200

    fix: clean up unused authentication views/forms/templates

--- agir/authentication/views/auth_views.py
+++ agir/authentication/views/auth_views.py
@@ -1,30 +1,16 @@
 from django.conf import settings
 from django.contrib import messages
-from django.contrib.auth import login, REDIRECT_FIELD_NAME
-from django.http import HttpResponseRedirect
-from django.urls import reverse, reverse_lazy
-from django.utils import timezone
-from django.utils.http import is_safe_url, urlquote
-from django.utils.safestring import mark_safe
-from django.views.generic import FormView, RedirectView
+from django.contrib.auth import REDIRECT_FIELD_NAME
+from django.urls import reverse_lazy
+from django.utils.http import is_safe_url
+from django.views.generic import RedirectView
 from oauth2_provider.views import AuthorizationView
 
-from agir.authentication.forms import EmailForm, CodeForm
-from agir.authentication.utils import (
-    is_soft_logged,
-    is_hard_logged,
-    get_bookmarked_emails,
-    bookmark_email,
-)
 from agir.authentication.view_mixins import HardLoginRequiredMixin
-from agir.lib.utils import get_client_ip
-from agir.people.models import PersonEmail
 
 
 __all__ = [
     "RedirectToMixin",
-    "LoginView",
-    "CheckCodeView",
     "Oauth2AuthorizationView",
     "SocialLoginError",
 ]
@@ -60,123 +46,6 @@ class RedirectToMixin:
             require_https=self.request.is_secure(),
         )
         return redirect_to if url_is_safe else ""
-
-
-class LoginView(RedirectToMixin, FormView):
-    form_class = EmailForm
-    template_name = "authentication/login.html"
-
-    def dispatch(self, request, *args, **kwargs):
-        self.bookmarked_emails = get_bookmarked_emails(self.request)
-        return super().dispatch(request, *args, **kwargs)
-
-    def get(self, request, *args, **kwargs):
-        auto = not request.GET.get("no_auto")
-        if is_soft_logged(request) and auto:
-            form = self.form_class(data={"email": request.user.person.email})
-
-            if form.is_valid():
-                return self.form_valid(form)
-
-        return super().get(request, *args, **kwargs)
-
-    def get_form_kwargs(self):
-        return {
-            "has_bookmarked_emails": len(self.bookmarked_emails) > 0,
-            **super().get_form_kwargs(),
-        }
-
-    def form_valid(self, form):
-        if not form.send_email(request_ip=get_client_ip(self.request)):
-            return self.form_invalid(form)
-
-        local_expiration_time = timezone.localtime(form.expiration).strftime("%H:%M")
-
-        if form.person is None:
-            messages.add_message(
-                self.request,
-                messages.DEBUG,
-                f"Pas de compte correspondant à l'adresse {form.cleaned_data['email']}.",
-            )
-        else:
-            messages.add_message(
-                self.request,
-                messages.DEBUG,
-                mark_safe(
-                    f'Le code de connexion est <span class="logincode">{form.short_code}</span> (expiration à {local_expiration_time}).'
-                ),
-            )
-
-        redirect_to = self.get_redirect_url()
-
-        self.request.session["login_email"] = form.cleaned_data["email"]
-        success_url = reverse("check_short_code")
-
-        if redirect_to:
-            success_url += f"?{self.redirect_field_name}={urlquote(redirect_to)}"
-
-        return bookmark_email(
-            form.cleaned_data["email"], self.request, HttpResponseRedirect(success_url)
-        )
-
-    def get_context_data(self, **kwargs):
-        return super().get_context_data(
-            bookmarked_emails=self.bookmarked_emails,
-            is_hard_logged=is_hard_logged(self.request),
-            **kwargs,
-        )
-
-
-class CheckCodeView(RedirectToMixin, FormView):
-    form_class = CodeForm
-    template_name = "authentication/check_short_code.html"
-    redirect_field_name = REDIRECT_FIELD_NAME
-
-    def dispatch(self, request, *args, **kwargs):
-        self.login_email = request.session.get("login_email")
-        if not self.login_email:
-            return self.redirect_to_login_page()
-
-        return super().dispatch(request, *args, **kwargs)
-
-    def redirect_to_login_page(self):
-        return HttpResponseRedirect(reverse("short_code_login"))
-
-    def get_success_url(self):
-        return self.get_redirect_url() or "/"
-
-    def get_form_kwargs(self):
-        kwargs = super().get_form_kwargs()
-        kwargs["email"] = self.login_email
-        return kwargs
-
-    def form_valid(self, form):
-        login(self.request, form.role)
-
-        if form.role.person.primary_email.bounced:
-            try:
-                validated_email_instance = form.role.person.emails.get_by_natural_key(
-                    self.login_email
-                )
-            except PersonEmail.DoesNotExist:
-                # en cas de connexion pile au moment de la suppression d'une adresse email...
-                pass
-            else:
-                if validated_email_instance.bounced:
-                    validated_email_instance.bounced = False
-                    validated_email_instance.bounced_date = None
-                    validated_email_instance.save()
-
-                if validated_email_instance != form.role.person.primary_email:
-                    form.role.person.set_primary_email(validated_email_instance)
-
-        return super().form_valid(form)
-
-    def get_context_data(self, **kwargs):
-        kwargs.update(
-            {"is_soft_logged": is_soft_logged(self.request), "email": self.login_email}
-        )
-        return super().get_context_data(**kwargs)
 
 
 class Oauth2AuthorizationView(HardLoginRequiredMixin, AuthorizationView):
