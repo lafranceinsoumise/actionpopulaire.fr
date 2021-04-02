b2d37690a68022e3cc31a2eac74a76ca9bda4a4d
MaximeG <mgoaziou@gmail.com>
2021-04-02 12:07:57 +0200

    fix: run prettier black on model event

--- agir/events/migrations/0004_event_online_url.py
+++ agir/events/migrations/0004_event_online_url.py
@@ -6,13 +6,15 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('events', '0003_has_priority_field_on_event_subtype'),
+        ("events", "0003_has_priority_field_on_event_subtype"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='event',
-            name='online_url',
-            field=models.URLField(blank=True, default='', verbose_name='Url de visio-conférence'),
+            model_name="event",
+            name="online_url",
+            field=models.URLField(
+                blank=True, default="", verbose_name="Url de visio-conférence"
+            ),
         ),
     ]
