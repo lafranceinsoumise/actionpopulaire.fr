0a0d4fe211ba189905b7bdffab319d07d51ca895
Jill Royer <hello@jillroyer.me>
2021-04-02 11:23:32 +0200

    feat: simple model - subscriptions are by user

--- agir/notifications/models.py
+++ agir/notifications/models.py
@@ -1,5 +1,3 @@
-from push_notifications import models as push_models
-from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
@@ -7,26 +5,24 @@ from agir.activity.models import Activity
 from agir.lib.models import TimeStampedModel, UUIDIdentified
 
 
-class PushSubscription(UUIDIdentified, TimeStampedModel):
-    type = models.CharField("Type", max_length=50, choices=Activity.TYPE_CHOICES)
+class Subscription(UUIDIdentified, TimeStampedModel):
+    SUBSCRIPTION_EMAIL = "email"
+    SUBSCRIPTION_PUSH = "push"
+    SUBSCRIPTION_CHOICES = (
+        (SUBSCRIPTION_EMAIL, "Email"),
+        (SUBSCRIPTION_PUSH, "Push"),
+    )
 
+    person = models.ForeignKey("people.Person", on_delete=models.CASCADE)
+    type = models.CharField("Type", max_length=5, choices=SUBSCRIPTION_CHOICES)
+    activity_type = models.CharField(
+        "Type", max_length=50, choices=Activity.TYPE_CHOICES
+    )
     # type of the object reference in this field
     # is implicit by the type of the subscription
+    # can be event, group, whatever...
     related_object_id = models.UUIDField(blank=True)
-
-    content_type = models.ForeignKey(
-        ContentType, on_delete=models.CASCADE, verbose_name="Type de périphérique"
-    )
-    object_id = models.IntegerField()
-    device = GenericForeignKey()
 
     class Meta:
         # only one subscription by device and by type and by object
-        unique_together = [["content_type", "object_id", "type", "related_object_id"]]
-
-
-class WebPushDevice(push_models.WebPushDevice):
-    settings = GenericRelation(PushSubscription, related_query_name="subscription")
-
-    class Meta:
-        proxy = True
+        unique_together = [["person", "type", "activity_type", "related_object_id"]]
