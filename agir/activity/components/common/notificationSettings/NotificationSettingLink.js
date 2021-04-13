f8c313c15c340c3c77f5bd13c3c88df65b9d36b7
Giuseppe De Ponte <giuseppedeponte@gmail.com>
2021-04-13 11:10:45 +0200

    fix: hide notification setting top bar link outside mobile apps

--- agir/activity/components/common/notificationSettings/NotificationSettingLink.js
+++ agir/activity/components/common/notificationSettings/NotificationSettingLink.js
@@ -1,5 +1,5 @@
 import PropTypes from "prop-types";
-import React, { useMemo } from "react";
+import React, { useEffect, useMemo } from "react";
 import styled from "styled-components";
 
 import style from "@agir/front/genericComponents/_variables.scss";
@@ -8,6 +8,10 @@ import Button from "@agir/front/genericComponents/Button";
 
 import { routeConfig } from "@agir/front/app/routes.config";
 import { useMobileApp } from "@agir/front/app/hooks";
+
+import { useDispatch } from "@agir/front/globalContext/GlobalContext";
+
+import { setTopBarRightLink } from "@agir/front/globalContext/actions";
 
 const StyledLink = styled(Button)`
   margin-left: auto;
@@ -32,6 +36,13 @@ const NotificationSettingLink = (props) => {
   const { root } = props;
   const route = useNotificationSettingLink(root);
   const { isMobileApp } = useMobileApp();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!isMobileApp) {
+      dispatch(setTopBarRightLink(null));
+    }
+  }, [isMobileApp, dispatch]);
 
   if (!isMobileApp) {
     return null;
