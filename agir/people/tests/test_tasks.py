33681c1b11bf03569e8b392e3aa67a85f99fe159
Benjamin L <benjamin.lemaire59850@gmail.com>
2021-04-19 12:15:36 +0200

    inactive user tests works now

--- agir/people/tests/test_tasks.py
+++ agir/people/tests/test_tasks.py
@@ -7,8 +7,7 @@ from agir.people import tasks
 
 class PeopleTasksTestCase(TestCase):
     def setUp(self):
-        self.person = Person.objects.create_insoumise("me@me.org")
-        self.person.ensure_role_exists()
+        self.person = Person.objects.create_insoumise("me@me.org", create_role=True)
 
     def test_welcome_mail(self):
         tasks.send_welcome_mail(self.person.pk, type="LFI")
@@ -23,13 +22,19 @@ class PeopleTasksTestCase(TestCase):
         self.assertEqual(mail.outbox[0].recipients(), [self.person.email])
 
     def test_user_no_role_dont_receive_mail(self):
-        self.person.role = None
-        tasks.send_welcome_mail(self.person.pk, type="LFI")
+        no_role_person = Person.objects.create_insoumise("norole@me.org")
+        no_role_person.role = None
+        self.person.save()
+        tasks.send_welcome_mail(no_role_person.pk, type="LFI")
 
         self.assertEqual(len(mail.outbox), 0)
 
     def test_inactive_user_dont_receive_mail(self):
-        self.person.role.is_active = False
-        tasks.send_welcome_mail(self.person.pk, type="LFI")
+        person = Person.objects.create_insoumise(
+            "inactiverole@me.org", create_role=True
+        )
+        person.role.is_active = False
+        person.role.save()
+        tasks.send_welcome_mail(person.pk, type="LFI")
 
         self.assertEqual(len(mail.outbox), 0)
