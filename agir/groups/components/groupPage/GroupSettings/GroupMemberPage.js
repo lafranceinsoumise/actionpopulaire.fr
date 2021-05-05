9b17c01aecb7056134977ace29569ac64d19484d
Giuseppe De Ponte <giuseppedeponte@gmail.com>
2021-05-05 11:43:35 +0200

    fix: add loading skeleton to group member setting panel

--- agir/groups/components/groupPage/GroupSettings/GroupMemberPage.js
+++ agir/groups/components/groupPage/GroupSettings/GroupMemberPage.js
@@ -1,11 +1,15 @@
 import PropTypes from "prop-types";
 import React from "react";
+import styled from "styled-components";
 import useSWR from "swr";
+
+import style from "@agir/front/genericComponents/_variables.scss";
 
 import Link from "@agir/front/app/Link";
 import GroupMember from "./GroupMember";
 import ShareLink from "@agir/front/genericComponents/ShareLink.js";
 import Spacer from "@agir/front/genericComponents/Spacer.js";
+import PageFadeIn from "@agir/front/genericComponents/PageFadeIn.js";
 import HeaderPanel from "./HeaderPanel";
 
 import { StyledTitle } from "./styledComponents.js";
@@ -13,41 +17,72 @@ import { StyledTitle } from "./styledComponents.js";
 import { useGroup } from "@agir/groups/groupPage/hooks/group.js";
 import { getGroupPageEndpoint } from "@agir/groups/groupPage/api.js";
 
+const StyledSkeleton = styled.div`
+  & > * {
+    background-color: ${style.black50};
+    margin: 0;
+    width: 100%;
+  }
+
+  & > :first-child {
+    height: 37px;
+    margin: 0.5rem 0;
+  }
+
+  & > :nth-child(2) {
+    height: 56px;
+    margin-bottom: 2rem;
+  }
+
+  & > :last-child {
+    height: 177px;
+  }
+`;
+
+const MembersSkeleton = (
+  <StyledSkeleton>
+    <div />
+    <div />
+    <div />
+  </StyledSkeleton>
+);
+
 const GroupMemberPage = (props) => {
   const { onBack, illustration, groupPk } = props;
   const group = useGroup(groupPk);
   const { data: members } = useSWR(
-    getGroupPageEndpoint("getMembers", { groupPk }),
-    { initialData: [] }
+    getGroupPageEndpoint("getMembers", { groupPk })
   );
 
   return (
     <>
       <HeaderPanel onBack={onBack} illustration={illustration} />
-      <StyledTitle>{group?.facts?.memberCount} Membres</StyledTitle>
-
-      <ShareLink
-        label="Copier les mails des membres"
-        color="secondary"
-        url={members.map(({ email }) => email).join(", ")}
-      />
-
-      <Spacer size="2rem" />
-
-      {members.map((e, id) => (
-        <>
-          <GroupMember
-            key={id}
-            name={e?.displayName}
-            email={e?.email}
-            image={e?.image}
-            membershipType={e?.membershipType}
-            assets={e?.assets}
-          />
-          <Spacer size="1rem" />
-        </>
-      ))}
-
+      <PageFadeIn ready={Array.isArray(members)} wait={MembersSkeleton}>
+        <StyledTitle>{group?.facts?.memberCount} Membres</StyledTitle>
+        <ShareLink
+          label="Copier les mails des membres"
+          color="secondary"
+          url={
+            Array.isArray(members) &&
+            members.map(({ email }) => email).join(", ")
+          }
+        />
+        <Spacer size="2rem" />
+        {Array.isArray(members) &&
+          members.map((e, id) => (
+            <>
+              <GroupMember
+                key={id}
+                name={e?.displayName}
+                email={e?.email}
+                image={e?.image}
+                membershipType={e?.membershipType}
+                assets={e?.assets}
+              />
+              <Spacer size="1rem" />
+            </>
+          ))}
+      </PageFadeIn>
       <Spacer size="2rem" />
 
       <ShareLink
