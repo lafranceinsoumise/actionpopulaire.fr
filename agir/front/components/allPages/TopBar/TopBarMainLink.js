3fb3c2f783fbc3ebfeb4c5a5f5ecaf9350e90c63
Benjamin L <benjamin.lemaire59850@gmail.com>
2021-05-07 14:59:47 +0200

    titles appears now on every pages

--- agir/front/components/allPages/TopBar/TopBarMainLink.js
+++ agir/front/components/allPages/TopBar/TopBarMainLink.js
@@ -1,17 +1,25 @@
 import React, { useMemo } from "react";
-import styled from "styled-components";
 
 import Logo from "./Logo";
 import MenuLink from "./MenuLink";
 import { routeConfig } from "../../app/routes.config";
+import { useLocation } from "react-router-dom";
 
 export const TopBarMainLink = (props) => {
   const { isMobileApp, path } = props;
 
   const currentRoute = useMemo(() => {
-    return Object.values(routeConfig).find((route) => route.match(path));
+    let route = Object.values(routeConfig).find((route) => route.match(path));
+
+    // for some pages routeConfig doesn't work, we use useLocation hook instead
+    if (typeof route !== "undefined") return route;
+    else
+      return Object.values(routeConfig).find((route) =>
+        route.match(useLocation().pathname)
+      );
   }, [path]);
 
+  //console.log(Object.values(routeConfig).find((route) => route.match(path)));
   if (!isMobileApp || !currentRoute) {
     return null;
   }
