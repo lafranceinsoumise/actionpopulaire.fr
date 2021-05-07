f7b3d37658fdb94de634ed92ab912fc489e09451
Giuseppe De Ponte <giuseppedeponte@gmail.com>
2021-05-07 12:20:35 +0200

    fix: add loader to group setting finance donation amount

--- agir/groups/components/groupPage/GroupSettings/GroupFinancePage.js
+++ agir/groups/components/groupPage/GroupSettings/GroupFinancePage.js
@@ -1,17 +1,19 @@
 import PropTypes from "prop-types";
-import React, { useState, useEffect, useCallback } from "react";
+import React from "react";
 import styled from "styled-components";
+import useSWR from "swr";
 
 import style from "@agir/front/genericComponents/_variables.scss";
 
 import Button from "@agir/front/genericComponents/Button";
+import PageFadeIn from "@agir/front/genericComponents/PageFadeIn";
 import ShareLink from "@agir/front/genericComponents/ShareLink.js";
 import Spacer from "@agir/front/genericComponents/Spacer.js";
 import HeaderPanel from "./HeaderPanel";
 
 import { StyledTitle } from "./styledComponents.js";
 
-import { getFinance } from "@agir/groups/groupPage/api.js";
+import { getGroupPageEndpoint } from "@agir/groups/groupPage/api.js";
 import { useGroup } from "@agir/groups/groupPage/hooks/group.js";
 import { useGroupWord } from "@agir/groups/utils/group";
 
@@ -30,50 +32,42 @@ const Buttons = styled.div`
   }
 `;
 
+const DonationSkeleton = styled.p`
+  height: 36px;
+  margin-bottom: 10px;
+  width: 50%;
+  background-color: ${style.black50};
+`;
+
 const GroupFinancePage = (props) => {
   const { onBack, illustration, groupPk } = props;
 
-  const [donation, setDonation] = useState(0);
-
   const group = useGroup(groupPk);
   const withGroupWord = useGroupWord(group);
-
-  const getFinanceAPI = useCallback(async (groupPk) => {
-    const res = await getFinance(groupPk);
-    setDonation(res.data.donation);
-  }, []);
-
-  useEffect(() => {
-    groupPk && getFinanceAPI(groupPk);
-  }, [groupPk, getFinanceAPI]);
-
+  const { data } = useSWR(getGroupPageEndpoint("getFinance", { groupPk }));
   return (
     <>
       <HeaderPanel onBack={onBack} illustration={illustration} />
-
       <StyledTitle>
         {withGroupWord`Dons alloués aux personnes de mon groupe`}
       </StyledTitle>
-
-      <Spacer size="1rem" />
-
-      <span style={{ fontSize: "2rem" }}>{donation} €</span>
-
-      <Spacer size="1rem" />
-
-      <div style={{ color: style.black700 }}>
-        {!donation && (
-          <>
+      <PageFadeIn ready={!!data} wait={<DonationSkeleton />}>
+        <p style={{ fontSize: "2rem", margin: 0 }}>{data?.donation} €</p>
+        <Spacer size=".5rem" />
+        {data?.donation === 0 && (
+          <p style={{ color: style.black700 }}>
             Personne n'a encore alloué de dons à vos actions.
-            <br />
-          </>
+          </p>
         )}
+      </PageFadeIn>
+      <p style={{ color: style.black700 }}>
         Vous pouvez allouer des dons à vos actions sur la{" "}
         <a href="/dons/">page de dons</a>.
-        <Spacer size="0.5rem" />
+      </p>
+      <p style={{ color: style.black700 }}>
         Vous pouvez déjà créer une demande, mais vous ne pourrez la faire
         valider que lorsque votre allocation sera suffisante.
-      </div>
+      </p>
 
       <Spacer size="1rem" />
       <Buttons>
@@ -93,9 +87,9 @@ const GroupFinancePage = (props) => {
         {withGroupWord`Solliciter des dons pour mon groupe`}
       </StyledTitle>
 
-      <span style={{ color: style.black700 }}>
+      <p style={{ color: style.black700 }}>
         {withGroupWord`Partagez ce lien pour solliciter des dons pour votre groupe :`}
-      </span>
+      </p>
 
       <ShareLink
         color="primary"
