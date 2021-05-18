632bada02bbfb0037fffb7a14497546975eccd0e
maxGoaz <mgoaziou@gmail.com>
2021-05-18 10:53:22 +0200

    fix: import PrefixQuery

--- agir/activity/admin.py
+++ agir/activity/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.utils.html import format_html
 
 from agir.activity.models import Activity, Announcement
+from agir.lib.search import PrefixSearchQuery
 
 
 @admin.register(Activity)
