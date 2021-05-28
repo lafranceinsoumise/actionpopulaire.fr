8ce23b9ee71c853e91480cee5260be5d0bd676b9
Jill Royer <hello@jillroyer.me>
2021-05-28 12:27:22 +0200

    feat: add django storage for media storage

--- agir/api/settings.py
+++ agir/api/settings.py
@@ -416,6 +416,16 @@ MEDIA_URL = "/media/"
 
 MEDIA_ROOT = os.environ.get("MEDIA_ROOT", "media")
 
+if not DEBUG:
+    DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+    AWS_DEFAULT_ACL = "public-read"
+    AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+    AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME", "agir")
+    AWS_S3_REGION_NAME = os.environ.get("AWS_S3_REGION_NAME")
+    AWS_S3_ENDPOINT_URL = os.environ.get("AWS_S3_ENDPOINT_URL")
+    AWS_QUERYSTRING_AUTH = False
+
 # Authentication
 
 AUTH_USER_MODEL = "authentication.Role"
