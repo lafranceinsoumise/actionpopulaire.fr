671326e92f65c22f87f44b64440694f76802209d
maxGoaz <mgoaziou@gmail.com>
2021-05-31 10:52:53 +0200

    fix: import defaultConfig from SWRConfig

--- agir/front/components/app/App.js
+++ agir/front/components/app/App.js
@@ -2,7 +2,7 @@ import { Helmet } from "react-helmet";
 import React from "react";
 
 import { GlobalContextProvider } from "@agir/front/globalContext/GlobalContext";
-import { SWRConfig, defaultConfig } from "swr";
+import { SWRConfig } from "swr";
 import axios from "@agir/lib/utils/axios";
 
 import PushModal from "@agir/front/allPages/PushModal/PushModal";
@@ -16,7 +16,7 @@ const fetcher = async (url) => {
 
 const errorRetry = (error) => {
   if ([403, 404].includes(error.status)) return;
-  defaultConfig.onErrorRetry();
+  SWRConfig.default.onErrorRetry();
 };
 
 export default function App() {
