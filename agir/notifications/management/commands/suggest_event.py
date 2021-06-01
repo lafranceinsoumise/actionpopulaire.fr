4ac2f2fcc54e52dd0252a7e0b5af9b8dd7694a79
Jill Royer <hello@jillroyer.me>
2021-06-01 15:35:37 +0200

    feat: allow suggest_event to be filtered by segment

--- agir/notifications/management/commands/suggest_event.py
+++ agir/notifications/management/commands/suggest_event.py
@@ -3,6 +3,7 @@ from django.core.management.base import BaseCommand
 from django.utils import timezone
 
 from agir.events.actions.notifications import new_event_suggestion_notification
+from agir.lib.management_utils import segment_argument
 from agir.people.models import Person
 from agir.events.models import Event
 
@@ -12,8 +13,16 @@ class Command(BaseCommand):
 
     help = "suggest an event to each user"
 
-    def handle(self, limit=SUGGEST_LIMIT_METERS, *args, **options):
-        for person in Person.objects.exclude(coordinates=None).filter(
+    def add_arguments(self, parser):
+        parser.add_argument("-S", "--segment", type=segment_argument)
+
+    def handle(self, segment, limit=SUGGEST_LIMIT_METERS, *args, **options):
+
+        if segment:
+            base_queryset = segment.get_subscribers_queryset()
+        else:
+            base_queryset = Person.all()
+        for person in base_queryset.exclude(coordinates=None).filter(
             role__is_active=True
         ):
             base_queryset = (
