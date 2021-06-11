ab0b71e7d536750015070003051a94d76814bbad
Giuseppe De Ponte <giuseppedeponte@gmail.com>
2021-06-11 14:10:26 +0200

    fix: loading/non-loading MessagePage layout

--- agir/msgs/components/MessagePage/MessagePage.js
+++ agir/msgs/components/MessagePage/MessagePage.js
@@ -22,17 +22,23 @@ import Skeleton from "@agir/front/genericComponents/Skeleton";
 import MessageThreadList from "./MessageThreadList";
 import EmptyMessagePage from "./EmptyMessagePage";
 
-const StyledPage = styled(PageFadeIn)`
+const StyledPageFadeIn = styled(PageFadeIn)``;
+const StyledPage = styled.div`
   margin: 0 auto;
   width: 100%;
   max-width: 1320px;
   height: calc(100vh - 84px);
-  padding: 2.625rem 1.5rem;
+  padding: 2.625rem 1.5rem 1.5rem;
 
   @media (max-width: ${style.collapse}px) {
     padding: 0;
     height: auto;
     max-width: 100%;
+  }
+
+  & > ${StyledPageFadeIn} {
+    height: 100%;
+    width: 100%;
   }
 `;
 
@@ -77,52 +83,54 @@ const MessagePage = ({ messagePk }) => {
         <title>Messages - Action Populaire</title>
       </Helmet>
       <NotificationSettings />
-      <StyledPage
-        ready={user && typeof messages !== "undefined"}
-        wait={<Skeleton />}
-      >
-        {!!writeNewMessage && (
-          <MessageModal
-            shouldShow={shouldShowMessageModal}
-            onClose={dismissMessageAction}
-            user={user}
-            groups={messageRecipients}
-            onSelectGroup={getSelectedGroupEvents}
-            events={selectedGroupEvents}
-            isLoading={isLoading}
-            message={messageAction === "edit" ? currentMessage : null}
-            onSend={saveMessage}
-          />
-        )}
-        {currentMessage && (
-          <MessageActionModal
-            action={shouldShowMessageActionModal ? messageAction : undefined}
-            shouldShow={shouldShowMessageActionModal}
-            onClose={dismissMessageAction}
-            onDelete={onDelete}
-            onReport={onReport}
-            isLoading={isLoading}
-          />
-        )}
-        {Array.isArray(messages) && messages.length > 0 ? (
-          <MessageThreadList
-            isLoading={isLoading}
-            messages={messages}
-            selectedMessagePk={messagePk}
-            selectedMessage={currentMessage}
-            onSelect={onSelectMessage}
-            onEdit={editMessage}
-            onDelete={confirmDelete}
-            onReport={confirmReport}
-            onDeleteComment={confirmDeleteComment}
-            onReportComment={confirmReportComment}
-            user={user}
-            writeNewMessage={writeNewMessage}
-            onComment={writeNewComment}
-          />
-        ) : (
-          <EmptyMessagePage />
-        )}
+      <StyledPage>
+        <StyledPageFadeIn
+          ready={user && typeof messages !== "undefined"}
+          wait={<Skeleton />}
+        >
+          {!!writeNewMessage && (
+            <MessageModal
+              shouldShow={shouldShowMessageModal}
+              onClose={dismissMessageAction}
+              user={user}
+              groups={messageRecipients}
+              onSelectGroup={getSelectedGroupEvents}
+              events={selectedGroupEvents}
+              isLoading={isLoading}
+              message={messageAction === "edit" ? currentMessage : null}
+              onSend={saveMessage}
+            />
+          )}
+          {currentMessage && (
+            <MessageActionModal
+              action={shouldShowMessageActionModal ? messageAction : undefined}
+              shouldShow={shouldShowMessageActionModal}
+              onClose={dismissMessageAction}
+              onDelete={onDelete}
+              onReport={onReport}
+              isLoading={isLoading}
+            />
+          )}
+          {Array.isArray(messages) && messages.length > 0 ? (
+            <MessageThreadList
+              isLoading={isLoading}
+              messages={messages}
+              selectedMessagePk={messagePk}
+              selectedMessage={currentMessage}
+              onSelect={onSelectMessage}
+              onEdit={editMessage}
+              onDelete={confirmDelete}
+              onReport={confirmReport}
+              onDeleteComment={confirmDeleteComment}
+              onReportComment={confirmReportComment}
+              user={user}
+              writeNewMessage={writeNewMessage}
+              onComment={writeNewComment}
+            />
+          ) : (
+            <EmptyMessagePage />
+          )}
+        </StyledPageFadeIn>
       </StyledPage>
       <Hide over>
         <Navigation active="messages" />
