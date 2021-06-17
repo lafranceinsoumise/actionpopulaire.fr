03c48440c49adbde9cc58e7cd62d5ca63297698a
maxGoaz <mgoaziou@gmail.com>
2021-06-17 18:18:44 +0200

    refactor

--- agir/people/migrations/0005_add_subscriptions.py
+++ agir/people/migrations/0005_add_subscriptions.py
@@ -1,11 +1,74 @@
 from django.db import migrations
 
-from agir.notifications.models import Subscription as SubscriptionTypes
-from agir.activity.models import Activity
-
-
 SUBSCRIPTION_EMAIL = "email"
 SUBSCRIPTION_PUSH = "push"
+
+TYPE_REFERRAL = "referral-accepted"
+
+# PERSON/EVENT TYPES
+TYPE_NEW_ATTENDEE = "new-attendee"
+TYPE_EVENT_UPDATE = "event-update"
+TYPE_CANCELLED_EVENT = "cancelled-event"
+TYPE_WAITING_LOCATION_EVENT = "waiting-location-event"
+TYPE_WAITING_LOCATION_GROUP = "waiting-location-group"
+TYPE_EVENT_SUGGESTION = "event-suggestion"
+TYPE_ANNOUNCEMENT = "announcement"
+
+# GROUP TYPES
+TYPE_NEW_REPORT = "new-report"
+TYPE_NEW_EVENT_MYGROUPS = "new-event-mygroups"
+TYPE_GROUP_INVITATION = "group-invitation"
+TYPE_NEW_MEMBER = "new-member"
+TYPE_GROUP_MEMBERSHIP_LIMIT_REMINDER = "group-membership-limit-reminder"
+TYPE_GROUP_INFO_UPDATE = "group-info-update"
+TYPE_NEW_MESSAGE = "new-message"
+TYPE_NEW_COMMENT = "new-comment"
+TYPE_GROUP_CREATION_CONFIRMATION = "group-creation-confirmation"
+TYPE_ACCEPTED_INVITATION_MEMBER = "accepted-invitation-member"
+TYPE_TRANSFERRED_GROUP_MEMBER = "transferred-group-member"
+TYPE_NEW_MEMBERS_THROUGH_TRANSFER = "new-members-through-transfer"
+
+# OTHER
+TYPE_GROUP_COORGANIZATION_INFO = "group-coorganization-info"
+TYPE_GROUP_COORGANIZATION_ACCEPTED = "group-coorganization-accepted"
+TYPE_GROUP_COORGANIZATION_INVITE = "group-coorganization-invite"
+TYPE_WAITING_PAYMENT = "waiting-payment"
+
+# MANDATORY TYPES
+MANDATORY_EMAIL_TYPES = (
+    TYPE_WAITING_PAYMENT,
+    # EVENT
+    TYPE_CANCELLED_EVENT,
+    # GROUP
+    TYPE_TRANSFERRED_GROUP_MEMBER,
+    TYPE_GROUP_INVITATION,
+    TYPE_GROUP_MEMBERSHIP_LIMIT_REMINDER,
+)
+
+# DEFAULT PERSON/EVENT TYPES
+DEFAULT_PERSON_EMAIL_TYPES = [
+    TYPE_EVENT_SUGGESTION,
+    TYPE_EVENT_UPDATE,
+    TYPE_NEW_ATTENDEE,
+    TYPE_WAITING_LOCATION_EVENT,
+]
+
+# DEFAULT GROUP TYPES
+DEFAULT_GROUP_EMAIL_TYPES = [
+    TYPE_NEW_EVENT_MYGROUPS,
+    TYPE_GROUP_COORGANIZATION_INFO,
+    TYPE_GROUP_INFO_UPDATE,
+    TYPE_NEW_MESSAGE,
+    TYPE_NEW_COMMENT,
+    TYPE_NEW_REPORT,
+    TYPE_NEW_MEMBER,
+    TYPE_ACCEPTED_INVITATION_MEMBER,
+    TYPE_NEW_MEMBERS_THROUGH_TRANSFER,
+    TYPE_WAITING_LOCATION_GROUP,
+    TYPE_GROUP_COORGANIZATION_INVITE,
+    TYPE_GROUP_CREATION_CONFIRMATION,
+    TYPE_GROUP_COORGANIZATION_ACCEPTED,
+]
 
 
 def migrate_default_subscriptions_switch_global_notifications_enabled(
@@ -15,9 +78,7 @@ def migrate_default_subscriptions_switch_global_notifications_enabled(
     Person = apps.get_model("people", "Person")
 
     # Delete mandatory subscriptions for push and email (here MANDATORY_EMAIL_TYPES = MANDATORY_PUSH_TYPES)
-    Subscription.objects.filter(
-        activity_type__in=SubscriptionTypes.MANDATORY_EMAIL_TYPES
-    ).delete()
+    Subscription.objects.filter(activity_type__in=MANDATORY_EMAIL_TYPES).delete()
 
     # If 'event_notifications' or 'group_notifications' are set, add or remove their subscriptions :
 
@@ -26,26 +87,14 @@ def migrate_default_subscriptions_switch_global_notifications_enabled(
         [
             Subscription(person=p, type=SUBSCRIPTION_EMAIL, activity_type=t,)
             for p in Person.objects.all().filter(event_notifications=True)
-            for t in [
-                # EVENTS
-                Activity.TYPE_EVENT_UPDATE,
-                Activity.TYPE_EVENT_SUGGESTION,
-                Activity.TYPE_NEW_ATTENDEE,
-                Activity.TYPE_WAITING_LOCATION_EVENT,
-            ]
+            for t in DEFAULT_PERSON_EMAIL_TYPES
         ]
     )
     Subscription.objects.bulk_create(
         [
             Subscription(person=p, type=SUBSCRIPTION_PUSH, activity_type=t,)
             for p in Person.objects.all().filter(event_notifications=True)
-            for t in [
-                # EVENTS
-                Activity.TYPE_EVENT_UPDATE,
-                Activity.TYPE_EVENT_SUGGESTION,
-                Activity.TYPE_NEW_ATTENDEE,
-                Activity.TYPE_WAITING_LOCATION_EVENT,
-            ]
+            for t in DEFAULT_PERSON_EMAIL_TYPES
         ]
     )
 
@@ -54,7 +103,7 @@ def migrate_default_subscriptions_switch_global_notifications_enabled(
         if (
             s.person.group_notifications is False
             or s.membership.group_notifications is False
-        ) and s.activity_type in SubscriptionTypes.DEFAULT_GROUP_EMAIL_TYPES:
+        ) and s.activity_type in DEFAULT_GROUP_EMAIL_TYPES:
             s.delete()
 
 
