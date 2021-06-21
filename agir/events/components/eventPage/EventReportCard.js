cf5d694909f123db7fdf23a0bdcd4a95a60d541a
Benjamin L <benjamin.lemaire59850@gmail.com>
2021-06-21 17:57:42 +0200

    create report card component for event page

---
+++ agir/events/components/eventPage/EventReportCard.js
@@ -0,0 +1,50 @@
+import { DateTime } from "luxon";
+import React from "react";
+import styled from "styled-components";
+import Collapsible from "../../../front/components/genericComponents/Collapsible";
+import Button from "../../../front/components/genericComponents/Button";
+import Card from "../../../front/components/genericComponents/Card";
+
+const StyledButton = styled(Button)`
+  height: 48px;
+  font-size: 16px;
+  font-weight: 500;
+  border-radius: 0.5rem;
+  margin-top: 16px;
+`;
+
+const StyledCard = styled(Card)`
+  p {
+    margin: 0;
+  }
+`;
+
+const EventReportCard = ({ compteRendu, isOrganizer, endTime, routes }) => {
+  const isPast = endTime < DateTime.local();
+
+  return (
+    <>
+      {isPast && (compteRendu || isOrganizer) ? (
+        <StyledCard>
+          <b>Compte-rendu</b>
+          {compteRendu ? (
+            <Collapsible
+              dangerouslySetInnerHTML={{ __html: compteRendu }}
+              style={{ margin: "1em 0 3em" }}
+              fadingOverflow
+            />
+          ) : (
+            <p>Il n'y a pas encore de compte-rendu de cet événement.</p>
+          )}
+          {isOrganizer && endTime < DateTime.local() && (
+            <StyledButton as="a" href={routes.compteRendu}>
+              {compteRendu ? "Modifier le" : "Ajouter un"} compte-rendu
+            </StyledButton>
+          )}
+        </StyledCard>
+      ) : null}
+    </>
+  );
+};
+
+export default EventReportCard;
