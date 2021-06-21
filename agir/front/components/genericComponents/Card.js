4abc1d0262a20693af87618d64e59f4678bd5c33
Benjamin L <benjamin.lemaire59850@gmail.com>
2021-06-21 15:44:51 +0200

    add border radius to card component

--- agir/front/components/genericComponents/Card.js
+++ agir/front/components/genericComponents/Card.js
@@ -21,6 +21,7 @@ const cardTypes = {
 };
 
 const Card = styled.div`
+  margin-bottom: 24px;
   background: ${({ type }) =>
     type && cardTypes[type] && cardTypes[type].background
       ? cardTypes[type].background
@@ -34,10 +35,6 @@ const Card = styled.div`
       ? cardTypes[type].backgroundColor
       : cardTypes.default.background};
   padding: 1.5rem;
-  border-radius: ${({ type }) =>
-    type && cardTypes[type] && cardTypes[type].borderRadius
-      ? cardTypes[type].borderRadius
-      : cardTypes.default.borderRadius};
   font-weight: 500;
   cursor: ${({ onClick }) => (onClick ? "pointer" : "default")};
   border: 1px solid;
@@ -53,10 +50,14 @@ const Card = styled.div`
         : ""}
   }
 
+  box-shadow: ${style.cardShadow};
+  border-radius: 8px;
+  overflow: hidden;
+  border-bottom: 1px solid ${style.black50};
+
   @media (max-width: ${style.collapse}px) {
     border: none;
     box-shadow: ${style.elaborateShadow};
-    border-radius: 0;
     padding: 1rem;
   }
 
