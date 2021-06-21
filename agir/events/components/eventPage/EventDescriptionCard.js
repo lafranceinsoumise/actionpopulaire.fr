0800732fa820ce69f7e596c46fb893d96529a1c4
Benjamin L <benjamin.lemaire59850@gmail.com>
2021-06-21 16:10:23 +0200

    create new component in event page for display event description

---
+++ agir/events/components/eventPage/EventDescriptionCard.js
@@ -0,0 +1,144 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import styled from "styled-components";
+
+import { DateTime } from "luxon";
+import Button from "@agir/front/genericComponents/Button";
+import Collapsible from "@agir/front/genericComponents/Collapsible.js";
+import Card from "../../../front/components/genericComponents/Card";
+import Spacer from "../../../front/components/genericComponents/Spacer";
+
+const DescriptionSection = styled.div`
+  margin: 0;
+
+  &:empty {
+    margin: 0;
+  }
+
+  & + & {
+    margin-top: 1rem;
+  }
+
+  & > * {
+    margin-top: 0;
+    margin-bottom: 1rem;
+
+    &:empty,
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+
+  h2 {
+    font-size: 1.25rem;
+    margin-bottom: 16px;
+  }
+
+  img {
+    margin: 10px 0px;
+  }
+`;
+
+const StyledButton = styled(Button)`
+  height: 48px;
+  font-size: 16px;
+  font-weight: 500;
+  border-radius: 0.5rem;
+  margin-top: 16px;
+`;
+
+const EventDescription = ({
+  illustration,
+  description,
+  isOrganizer,
+  endTime,
+  routes,
+}) => {
+  if (!illustration?.banner) {
+    console.log("bravo");
+  }
+  return (
+    <>
+      {description ? (
+        <Card>
+          <DescriptionSection>
+            <h2>L'événement</h2>
+            {illustration?.banner && (
+              <DescriptionSection>
+                <img
+                  src={illustration.banner}
+                  alt="Image d'illustration de l'événement postée par l'utilisateur"
+                  style={{
+                    maxWidth: "100%",
+                    height: "auto",
+                    maxHeight: "500px",
+                    borderRadius: "8px",
+                  }}
+                />
+              </DescriptionSection>
+            )}
+
+            <Collapsible
+              dangerouslySetInnerHTML={{ __html: description }}
+              fadingOverflow
+            />
+            <Spacer />
+            <StyledButton as="a" href={routes.edit}>
+              Modifier la description
+            </StyledButton>
+
+            <StyledButton as="a" href={routes.edit}>
+              Ajouter une image d'illustration
+            </StyledButton>
+          </DescriptionSection>
+        </Card>
+      ) : null}
+
+      {!description && isOrganizer && endTime > DateTime.local() && (
+        <Card>
+          <DescriptionSection>
+            <h2>Ajoutez une description !</h2>
+            <p>
+              Donner tous les informations nécessaires aux participants de votre
+              événement. Comment accéder au lieu, quel est le programme, les
+              liens pour être tenu au courant... Et ajoutez une image !
+            </p>
+            <div>
+              <StyledButton as="a" href={routes.edit}>
+                Ajouter une description
+              </StyledButton>
+
+              {!illustration?.banner && (
+                <StyledButton as="a" href={routes.edit}>
+                  Ajouter une image d'illustration
+                </StyledButton>
+              )}
+            </div>
+          </DescriptionSection>
+        </Card>
+      )}
+    </>
+  );
+};
+
+EventDescription.propTypes = {
+  compteRendu: PropTypes.string,
+  compteRenduPhotos: PropTypes.arrayOf(
+    PropTypes.shape({
+      image: PropTypes.string,
+      thumbnail: PropTypes.string,
+    })
+  ),
+  illustration: PropTypes.shape({
+    banner: PropTypes.string,
+    thumbnail: PropTypes.string,
+  }),
+  description: PropTypes.string,
+  isOrganizer: PropTypes.bool,
+  endTime: PropTypes.instanceOf(DateTime),
+  routes: PropTypes.object,
+  rsvp: PropTypes.string,
+};
+
+export default EventDescription;
