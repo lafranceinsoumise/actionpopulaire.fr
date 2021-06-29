b8a670d2b5aeb9798bee2fbcf7c9022ee63a9d1e
maxGoaz <mgoaziou@gmail.com>
2021-06-29 14:51:04 +0200

    refactor: less lines to get authors

--- agir/groups/tasks.py
+++ agir/groups/tasks.py
@@ -500,14 +500,12 @@ def send_message_notification_email(message_pk):
 def send_comment_notification_email(comment_pk):
     comment = SupportGroupMessageComment.objects.get(pk=comment_pk)
 
-    authors = [comment.message.author.id]
-    for comment in comment.message.comments.all():
-        if comment.author.id not in authors:
-            authors.append(comment.author.id)
+    comment_authors = list(comment.message.comments.values_list("author_id", flat=True))
+    comment_authors = set(comment_authors + [comment.message.author_id])
 
     recipients = Person.objects.exclude(id=comment.author.id).filter(
         notification_subscriptions__membership__supportgroup=comment.message.supportgroup,
-        notification_subscriptions__person_id__in=authors,
+        notification_subscriptions__person__in=comment_authors,
         notification_subscriptions__type=Subscription.SUBSCRIPTION_EMAIL,
         notification_subscriptions__activity_type=Activity.TYPE_NEW_COMMENT,
     )
