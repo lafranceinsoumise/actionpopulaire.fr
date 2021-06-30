2169deda44852c7b0f95218e1b533520f1c75ca8
maxGoaz <mgoaziou@gmail.com>
2021-06-30 14:57:24 +0200

    fix: margin right navbar

--- agir/front/components/allPages/TopBar/TopBar.js
+++ agir/front/components/allPages/TopBar/TopBar.js
@@ -92,7 +92,7 @@ const HorizontalFlex = styled.div`
     min-width: 0;
     flex-shrink: 0;
     max-width: 100%;
-    margin-right: 1em;
+    margin-right: 1rem;
   }
 
   & > *:last-child {
@@ -205,7 +205,7 @@ export const TopBar = (props) => {
                 </>
               ) : (
                 <>
-                  <Hide under>
+                  <Hide under style={{ marginRight: 0 }}>
                     <div style={{ display: "flex" }}>
                       <MenuLink route="events">
                         <TopbarLink $active={routeConfig.events.match(path)}>
