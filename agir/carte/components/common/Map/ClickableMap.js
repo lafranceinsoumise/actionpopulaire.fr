9319d5d0f24f6e6bd2bcefa90c3845bc2727de84
Benjamin L <benjamin.lemaire59850@gmail.com>
2021-07-05 10:41:14 +0200

    fix geolocations queries for android and ios

--- agir/carte/components/common/Map/ClickableMap.js
+++ agir/carte/components/common/Map/ClickableMap.js
@@ -24,8 +24,8 @@ const ClickableMap = (props) => {
   let latitude = location.coordinates.coordinates.toString().split(",")[1];
   let longitude = location.coordinates.coordinates.toString().split(",")[0];
 
-  const appleHref = `https://maps.apple.com/?ll=${latitude},${longitude}&ui=maps`;
-  const androidHref = `geo:${latitude},${longitude}`;
+  const appleHref = `https://maps.apple.com/?ll=${latitude},${longitude}&q=${encodeURI(location.address)}&ui=maps`;
+  const androidHref = `geo:0,0?q=${latitude},${longitude}`;
 
   let href = `https://www.google.fr/maps/search/${location.address}`;
   if (isAndroid) {
