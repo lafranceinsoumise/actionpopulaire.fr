3f4c201cdd7330c5c31ec84429ef4178ab541e48
Jill Royer <hello@jillroyer.me>
2021-07-05 10:47:32 +0200

    feat: update weekly_stats command

--- agir/lib/stats.py
+++ agir/lib/stats.py
@@ -29,9 +29,9 @@ def get_general_stats(start, end):
     )
 
     return {
-        "soutiens_NSP": nouveaux_soutiens.count(),
-        "soutiens_NSP_insoumis": nouveaux_soutiens.filter(is_insoumise=True).count(),
-        "soutiens_NSP_non_insoumis": nouveaux_soutiens.filter(
+        "soutiens_2022": nouveaux_soutiens.count(),
+        "soutiens_2022_insoumis": nouveaux_soutiens.filter(is_insoumise=True).count(),
+        "soutiens_2022_non_insoumis": nouveaux_soutiens.filter(
             is_insoumise=False
         ).count(),
         "news_LFI": ouvert_news.filter(is_insoumise=True).distinct().count(),
@@ -40,32 +40,22 @@ def get_general_stats(start, end):
         ).count()
         / (sent_events.filter(subscriber__is_insoumise=True).count() or 1)
         * 100,
-        "news_NSP": ouvert_news.filter(is_insoumise=False).distinct().count(),
-        "taux_news_NSP": sent_events.filter(
+        "news_2022": ouvert_news.filter(is_insoumise=False).distinct().count(),
+        "taux_news_2022": sent_events.filter(
             subscriber__is_insoumise=False, open_count__gt=0,
         ).count()
         / (sent_events.filter(subscriber__is_insoumise=False,).count() or 1)
         * 100,
         "ap_users": ap_users.count(),
         "ap_users_LFI": ap_users.filter(is_insoumise=True).count(),
-        "ap_users_NSP": ap_users.filter(is_insoumise=False, is_2022=True).count(),
+        "ap_users_2022": ap_users.filter(is_insoumise=False, is_2022=True).count(),
         "ap_events": Event.objects.filter(
             visibility=Event.VISIBILITY_PUBLIC, start_time__range=(start, end)
         ).count(),
-        "ap_events_LFI": Event.objects.filter(
-            visibility=Event.VISIBILITY_PUBLIC,
-            for_users=Event.FOR_USERS_INSOUMIS,
-            start_time__range=(start, end),
-        ).count(),
-        "ap_events_NSP": Event.objects.filter(
-            visibility=Event.VISIBILITY_PUBLIC,
-            for_users=Event.FOR_USERS_2022,
-            start_time__range=(start, end),
-        ).count(),
-        "ga_LFI": SupportGroup.objects.active()
+        "ga": SupportGroup.objects.active()
         .filter(type=SupportGroup.TYPE_LOCAL_GROUP, created__range=(start, end))
         .count(),
-        "membres_ga_LFI": Person.objects.filter(
+        "membres_ga": Person.objects.filter(
             memberships__supportgroup__type=SupportGroup.TYPE_LOCAL_GROUP,
             memberships__supportgroup__published=True,
         )
@@ -85,39 +75,37 @@ def get_events_by_subtype(start, end):
 
 def get_instant_stats():
     return {
-        "soutiens_NSP": Person.objects.filter(is_2022=True).count(),
-        "soutiens_NSP_insoumis": Person.objects.filter(
+        "soutiens_2022": Person.objects.filter(is_2022=True).count(),
+        "soutiens_2022_insoumis": Person.objects.filter(
             is_2022=True, is_insoumise=True
         ).count(),
-        "soutiens_NSP_non_insoumis": Person.objects.filter(
+        "soutiens_2022_non_insoumis": Person.objects.filter(
             is_2022=True, is_insoumise=False
         ).count(),
-        "ga_LFI": SupportGroup.objects.filter(
+        "ga": SupportGroup.objects.filter(
             type=SupportGroup.TYPE_LOCAL_GROUP, published=True
         ).count(),
-        "ga_LFI_certifies": SupportGroup.objects.certified()
-        .filter(published=True)
-        .count(),
-        "membres_ga_LFI": Person.objects.filter(
+        "ga_certifies": SupportGroup.objects.certified().filter(published=True).count(),
+        "membres_ga": Person.objects.filter(
             supportgroups__type=SupportGroup.TYPE_LOCAL_GROUP,
             supportgroups__published=True,
         )
         .distinct()
         .count(),
-        "membres_ga_LFI_certifies": Person.objects.filter(
+        "membres_ga_certifies": Person.objects.filter(
             supportgroups__type=SupportGroup.TYPE_LOCAL_GROUP,
             supportgroups__subtypes__label__in=settings.CERTIFIED_GROUP_SUBTYPES,
             supportgroups__published=True,
         )
         .distinct()
         .count(),
-        "insoumis_non_NSP": Person.objects.filter(
+        "insoumis_non_2022": Person.objects.filter(
             is_insoumise=True,
             is_2022=False,
             emails___bounced=False,
             newsletters__contains=[Person.NEWSLETTER_LFI],
         ).count(),
-        "insoumis_non_NSP_newsletter": Person.objects.filter(
+        "insoumis_non_2022_newsletter": Person.objects.filter(
             is_insoumise=True,
             is_2022=False,
             emails___bounced=False,
@@ -127,7 +115,7 @@ def get_instant_stats():
         )
         .distinct()
         .count(),
-        "insoumis_non_NSP_phone": Person.objects.filter(
+        "insoumis_non_2022_phone": Person.objects.filter(
             is_insoumise=True,
             is_2022=False,
             newsletters__contains=[Person.NEWSLETTER_LFI],
