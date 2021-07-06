3b992091d0c4b063f98e384c26bdc5eb267f302a
maxGoaz <mgoaziou@gmail.com>
2021-07-06 11:53:43 +0200

    feat: refactor and add components for website items, add images

--- agir/front/components/toolsPage/index.js
+++ agir/front/components/toolsPage/index.js
@@ -9,6 +9,13 @@ import Link from "@agir/front/app/Link";
 import Button from "@agir/front/genericComponents/Button";
 import { RawFeatherIcon } from "@agir/front/genericComponents/FeatherIcon";
 
+import img_JLM_2022 from "@agir/front/genericComponents/logos/JLM_2022.jpg";
+import img_AvenirEnCommun from "@agir/front/genericComponents/logos/AvenirEnCommun.jpg";
+import img_Linsoumission from "@agir/front/genericComponents/logos/Linsoumission.jpg";
+import img_Comparateur from "@agir/front/genericComponents/logos/Comparateur.jpg";
+
+import nonReactRoutes from "@agir/front/globalContext/nonReactRoutes.config";
+
 const Container = styled.div`
   padding: 25px 85px;
 
@@ -21,6 +28,7 @@ const BlockTitle = styled.div`
   display: flex;
   justify-content: space-between;
   margin-bottom: 23px;
+  margin-top: 23px;
 
   > div:first-child {
     display: flex;
@@ -49,20 +57,24 @@ const Subtitle = styled.div`
 
 const StyledButton = styled(Button)`
   height: 40px;
-  font-size: 16px;
+  font-size: 1rem;
   font-weight: 500;
   border-radius: 0.5rem;
+
+  ${RawFeatherIcon} {
+    margin-left: 4px;
+  }
 `;
 
 const ItemActionContainer = styled.div`
   box-shadow: 0px 0px 2px rgb(0 0 0 / 50%), 0px 3px 3px rgb(0 35 44 / 10%);
-  border-radius: 8px;
+  border-radius: 0.5rem;
   overflow: hidden;
   width: 340px;
   display: inline-flex;
   flex-direction: column;
-  margin-right: 24px;
-  margin-bottom: 24px;
+  margin-right: 1.5rem;
+  margin-bottom: 1rem;
   color: ${style.black1000};
 
   > div:first-child {
@@ -91,16 +103,117 @@ const ListItemAction = styled.div`
   max-width: 100%;
   overflow-y: hidden;
   overflow-x: auto;
+
+  div:first-child {
+    margin-left: 2px;
+  }
 `;
 
-const ItemAction = ({ image, title }) => {
+const ItemWebsiteContainer = styled.div`
+  display: inline-flex;
+  height: 65px;
+  max-width: 388px;
+  width: 100%;
+  border-radius: 0.5rem;
+  box-shadow: 0px 0px 2px rgb(0 0 0 / 50%), 0px 3px 3px rgb(0 35 44 / 10%);
+  margin-right: 1.5rem;
+  margin-bottom: 1rem;
+  overflow: hidden;
+  color: ${style.black1000};
+
+  > div:first-child {
+    width: 113px;
+    ${({ img }) => `
+      background-image: url(${img});
+      background-position: center;
+      background-size: cover;
+    `}
+  }
+
+  > div:last-child {
+    flex-grow: 1;
+    justify-content: center;
+    align-items: center;
+    display: flex;
+  }
+`;
+
+const ItemAction = ({ image, title, href }) => {
   return (
-    <ItemActionContainer img={image}>
-      <div />
-      <div dangerouslySetInnerHTML={{ __html: title }}></div>
-    </ItemActionContainer>
+    <Link href={href}>
+      <ItemActionContainer img={image}>
+        <div />
+        <div dangerouslySetInnerHTML={{ __html: title }}></div>
+      </ItemActionContainer>
+    </Link>
   );
 };
+
+const LinkInfoAction = () => (
+  <StyledButton
+    small
+    as="Link"
+    color="secondary"
+    href="https://materiel.lafranceinsoumise.fr/"
+    target="_blank"
+  >
+    Accéder au fiches pratiques
+    <RawFeatherIcon
+      name="arrow-up-right"
+      color={style.black1000}
+      width="1.25rem"
+    />
+  </StyledButton>
+);
+
+const LinkMaterial = () => (
+  <StyledButton
+    small
+    as="Link"
+    color="secondary"
+    href="https://materiel.lafranceinsoumise.fr/"
+    target="_blank"
+  >
+    Accéder au site matériel
+    <RawFeatherIcon
+      name="arrow-up-right"
+      color={style.black1000}
+      width="1.25rem"
+    />
+  </StyledButton>
+);
+
+const ItemWebsite = ({ img, href, title }) => (
+  <Link href={href}>
+    <ItemWebsiteContainer img={img}>
+      <div />
+      <div>{title}</div>
+    </ItemWebsiteContainer>
+  </Link>
+);
+
+const WEBSITES = [
+  {
+    title: "Mélenchon 2022",
+    img: img_JLM_2022,
+    href: nonReactRoutes.jlm2022,
+  },
+  {
+    title: "L'avenir en commun",
+    img: img_AvenirEnCommun,
+    href: nonReactRoutes.programme,
+  },
+  {
+    title: "L'insoumission",
+    img: img_Linsoumission,
+    href: nonReactRoutes.linsoumission,
+  },
+  {
+    title: "Comparateur de programme",
+    img: img_Comparateur,
+    href: nonReactRoutes.comparateur,
+  },
+];
 
 const ToolsPage = () => {
   const [categories, setCategories] = useState([]);
@@ -164,24 +277,19 @@ const ToolsPage = () => {
         </div>
 
         <Hide under>
-          <StyledButton
-            small
-            as="Link"
-            color="secondary"
-            href="https://materiel.lafranceinsoumise.fr/"
-            target="_blank"
-          >
-            Accéder au site matériel
-            <RawFeatherIcon
-              name="arrow-up-right"
-              color={style.black1000}
-              width="1.25rem"
-            />
-          </StyledButton>
+          <LinkMaterial />
         </Hide>
       </BlockTitle>
 
       <BlockContent></BlockContent>
+
+      <Hide over>
+        <LinkMaterial />
+      </Hide>
+
+      <Hide over>
+        <hr />
+      </Hide>
 
       <BlockTitle>
         <div>
@@ -190,20 +298,7 @@ const ToolsPage = () => {
         </div>
 
         <Hide under>
-          <StyledButton
-            small
-            as="Link"
-            color="secondary"
-            href="https://materiel.lafranceinsoumise.fr/"
-            target="_blank"
-          >
-            Accéder au fiches pratiques
-            <RawFeatherIcon
-              name="arrow-up-right"
-              color={style.black1000}
-              width="1.25rem"
-            />
-          </StyledButton>
+          <LinkInfoAction />
         </Hide>
       </BlockTitle>
 
@@ -214,14 +309,24 @@ const ToolsPage = () => {
 
             <ListItemAction>
               {pages[category.id]?.map((page) => (
-                <Link href={page.link}>
-                  <ItemAction image={page.img} title={page.title} />
-                </Link>
+                <ItemAction
+                  image={page.img}
+                  title={page.title}
+                  href={page.link}
+                />
               ))}
             </ListItemAction>
           </>
         ))}
       </BlockContent>
+
+      <Hide over>
+        <LinkInfoAction />
+      </Hide>
+
+      <Hide over>
+        <hr />
+      </Hide>
 
       <BlockTitle>
         <div>
@@ -230,7 +335,15 @@ const ToolsPage = () => {
         </div>
       </BlockTitle>
 
-      <BlockContent></BlockContent>
+      <BlockContent>
+        {WEBSITES.map((w) => (
+          <ItemWebsite img={w.img} href={w.href} title={w.title} />
+        ))}
+      </BlockContent>
+
+      <Hide over>
+        <hr />
+      </Hide>
 
       <BlockTitle>
         <div>
