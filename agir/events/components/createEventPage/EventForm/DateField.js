04fd0ee14a57ae3d68ffb3a3eb3b01b11c7dd571
Giuseppe De Ponte <giuseppedeponte@gmail.com>
2021-07-07 13:46:18 +0200

    fix: event timezone display

--- agir/events/components/createEventPage/EventForm/DateField.js
+++ agir/events/components/createEventPage/EventForm/DateField.js
@@ -52,7 +52,7 @@ const TimezoneToggle = styled.p`
 
 const Field = styled.div`
   display: inline-grid;
-  grid-template-columns: auto 160px 160px;
+  grid-template-columns: auto 160px 270px;
   grid-auto-rows: auto auto;
   grid-gap: 0.5rem;
 
