d64853d9361c4e9d500864079e49478e0eb8ead0
Giuseppe De Ponte <giuseppedeponte@gmail.com>
2021-07-09 11:51:40 +0200

    feat: create RequiredDocumentCard component

---
+++ agir/events/components/eventRequiredDocuments/RequiredDocumentCard.js
@@ -0,0 +1,127 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+import Button from "@agir/front/genericComponents/Button";
+import { RawFeatherIcon } from "@agir/front/genericComponents/FeatherIcon";
+import Spacer from "@agir/front/genericComponents/Spacer";
+
+import { EVENT_DOCUMENT_TYPES } from "./config";
+
+const StyledCard = styled.div`
+  overflow: hidden;
+  border-radius: ${(props) => props.theme.borderRadius};
+  box-shadow: ${(props) => props.theme.cardShadow};
+  font-size: 0.875rem;
+  width: 100%;
+  padding: 0;
+  margin: 0;
+
+  @media (min-width: ${(props) => props.theme.collapse}px) {
+    max-width: 680px;
+    display: flex;
+    align-items: stretch;
+  }
+
+  &::before {
+    display: block;
+    content: "";
+    height: 108px;
+    background-image: url(${(props) => props.$img});
+    background-repeat: no-repeat;
+    background-position: center center;
+    background-size: cover;
+
+    @media (min-width: ${(props) => props.theme.collapse}px) {
+      flex: 0 0 152px;
+      height: auto;
+    }
+  }
+
+  & > div {
+    padding: 1rem;
+
+    @media (min-width: ${(props) => props.theme.collapse}px) {
+      flex: 1 1 auto;
+      padding: 1.5rem;
+    }
+
+    & > * {
+      margin: 0;
+    }
+
+    h4 {
+      color: ${(props) => props.theme.primary500};
+      font-weight: 600;
+      font-size: 16px;
+      line-height: 1.5;
+    }
+
+    p {
+      line-height: 1.6;
+      color: ${(props) => props.theme.black700};
+    }
+
+    ${Button} {
+      width: 100%;
+      border-radius: ${(props) => props.theme.borderRadius};
+      font-weight: 600;
+      margin: 1rem 0 0;
+      justify-content: center;
+
+      @media (min-width: ${(props) => props.theme.collapse}px) {
+        justify-content: flex-start;
+        display: inline-flex;
+        width: auto;
+        margin-right: 1rem;
+      }
+    }
+  }
+`;
+
+const RequiredDocumentCard = (props) => {
+  const { type, onUpload, onDismiss } = props;
+
+  if (!type || !EVENT_DOCUMENT_TYPES[type]) {
+    return null;
+  }
+
+  const { image, name, description, templateLink } = EVENT_DOCUMENT_TYPES[type];
+
+  return (
+    <StyledCard $img={image}>
+      <div>
+        <h4>{name}</h4>
+        <Spacer size="0.5rem" />
+        <p>{description}</p>
+        <Button
+          onClick={() => onUpload(type)}
+          color={onDismiss ? "primary" : "default"}
+        >
+          <RawFeatherIcon name="upload" style={{ marginRight: "0.5rem" }} />
+          Ajouter un justificatif
+        </Button>
+        {templateLink && (
+          <Button as="a" href="" download={templateLink}>
+            <RawFeatherIcon name="file" style={{ marginRight: "0.5rem" }} />
+            Modèle vierge
+          </Button>
+        )}
+        <br />
+        {onDismiss && (
+          <Button small color="link" onClick={() => onDismiss(type)}>
+            Non appliquable
+          </Button>
+        )}
+      </div>
+    </StyledCard>
+  );
+};
+
+RequiredDocumentCard.propTypes = {
+  type: PropTypes.string.isRequired,
+  onUpload: PropTypes.func.isRequired,
+  onDismiss: PropTypes.func,
+};
+
+export default RequiredDocumentCard;
---
+++ agir/events/components/eventRequiredDocuments/RequiredDocumentCard.stories.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+import RequiredDocumentCard from "./RequiredDocumentCard";
+
+import { EVENT_DOCUMENT_TYPES } from "./config";
+
+export default {
+  component: RequiredDocumentCard,
+  title: "Event Required Documents/RequiredDocumentCard",
+  argTypes: {
+    type: {
+      options: Object.keys(EVENT_DOCUMENT_TYPES),
+      control: { type: "radio" },
+    },
+  },
+};
+
+const Template = (args) => (
+  <div style={{ minWidth: 372, maxWidth: "90%", margin: "24px auto" }}>
+    <RequiredDocumentCard {...args} />
+  </div>
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  type: Object.keys(EVENT_DOCUMENT_TYPES)[0],
+};
+
+export const Optional = Template.bind({});
+Optional.args = {
+  ...Default.args,
+  onDismiss: null,
+};
