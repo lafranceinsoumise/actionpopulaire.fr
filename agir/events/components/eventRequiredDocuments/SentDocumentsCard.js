eef41d627ae5b3544f014d74a1845101edc2ae72
Giuseppe De Ponte <giuseppedeponte@gmail.com>
2021-07-09 12:07:05 +0200

    feat: create SentDocumentsCard component

---
+++ agir/events/components/eventRequiredDocuments/SentDocumentsCard.js
@@ -0,0 +1,97 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+import Button from "@agir/front/genericComponents/Button";
+import { RawFeatherIcon } from "@agir/front/genericComponents/FeatherIcon";
+import Spacer from "@agir/front/genericComponents/Spacer";
+
+const StyledCard = styled.figure`
+  width: 100%;
+  margin: 0;
+  padding: 1rem;
+  border-radius: ${(props) => props.theme.borderRadius};
+  box-shadow: ${(props) => props.theme.cardShadow};
+
+  & > * {
+    margin: 0;
+  }
+
+  h4 {
+    font-weight: 600;
+    font-size: 16px;
+    line-height: 1.5;
+    color: ${(props) => props.theme.primary500};
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+    margin: 0;
+
+    li {
+      display: flex;
+      align-items: center;
+      justify-content: flex-start;
+
+      span {
+        font-weight: 500;
+        flex: 1 1 auto;
+        text-align: left;
+        padding: 0 0.625rem;
+        white-space: nowrap;
+        overflow-x: hidden;
+        text-overflow: ellipsis;
+      }
+
+      ${Button} {
+        height: 2rem;
+        border-radius: ${(props) => props.theme.borderRadius};
+        font-weight: 600;
+        font-size: 0.813rem;
+      }
+    }
+
+    li + li {
+      padding-top: 0.5rem;
+    }
+  }
+`;
+
+const SentDocumentsCard = (props) => {
+  const { documents } = props;
+
+  if (!Array.isArray(documents) || documents.length === 0) {
+    return null;
+  }
+
+  return (
+    <StyledCard>
+      <h4>Documents envoyés</h4>
+      <Spacer size="1rem" />
+      <ul>
+        {documents.map((doc) => (
+          <li key={doc.id}>
+            <RawFeatherIcon as="i" name="file-text" />
+            <span>{doc.name}</span>
+            <Button small as="a" href={doc.link} color="primary">
+              Voir
+            </Button>
+          </li>
+        ))}
+      </ul>
+    </StyledCard>
+  );
+};
+
+SentDocumentsCard.propTypes = {
+  documents: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      link: PropTypes.string.isRequired,
+    })
+  ),
+};
+
+export default SentDocumentsCard;
