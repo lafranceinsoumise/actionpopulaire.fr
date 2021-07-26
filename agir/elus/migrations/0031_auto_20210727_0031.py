09e9bd143252396f95f98522fb49a88ea25b0a6f
Arthur Cheysson <arthur@cheysson.fr>
2021-07-27 00:43:09 +0200

    chore: black reformatting

--- agir/elus/migrations/0031_auto_20210727_0031.py
+++ agir/elus/migrations/0031_auto_20210727_0031.py
@@ -8,19 +8,34 @@ import dynamic_filenames
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('data_france', '0029_deputes_europeens'),
-        ('elus', '0030_auto_20210726_1656'),
+        ("data_france", "0029_deputes_europeens"),
+        ("elus", "0030_auto_20210726_1656"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='rechercheparrainage',
-            name='formulaire',
-            field=models.FileField(blank=True, null=True, upload_to=dynamic_filenames.FilePattern(filename_pattern='elus/parrainages/{uuid:.30base32}{ext}'), verbose_name='Formulaire de promesse signé'),
+            model_name="rechercheparrainage",
+            name="formulaire",
+            field=models.FileField(
+                blank=True,
+                null=True,
+                upload_to=dynamic_filenames.FilePattern(
+                    filename_pattern="elus/parrainages/{uuid:.30base32}{ext}"
+                ),
+                verbose_name="Formulaire de promesse signé",
+            ),
         ),
         migrations.AlterField(
-            model_name='rechercheparrainage',
-            name='maire',
-            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, related_name='parrainages', related_query_name='parrainage', to='data_france.elumunicipal', verbose_name='Maire'),
+            model_name="rechercheparrainage",
+            name="maire",
+            field=models.ForeignKey(
+                blank=True,
+                null=True,
+                on_delete=django.db.models.deletion.SET_NULL,
+                related_name="parrainages",
+                related_query_name="parrainage",
+                to="data_france.elumunicipal",
+                verbose_name="Maire",
+            ),
         ),
     ]
