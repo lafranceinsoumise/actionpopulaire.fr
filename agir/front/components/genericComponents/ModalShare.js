c22ea57ca31712ab87b6e67e1eba8ab6ae928d71
maxGoaz <mgoaziou@gmail.com>
2021-09-15 11:53:41 +0200

    styles: modal share

--- agir/front/components/genericComponents/ModalShare.js
+++ agir/front/components/genericComponents/ModalShare.js
@@ -12,9 +12,7 @@ import { FaWhatsapp, FaFacebook, FaTelegramPlane } from "react-icons/fa";
 
 const ModalContainer = styled.div`
   background: white;
-  height: 50%;
-  min-height: 365px;
-  width: 40%;
+  width: fit-content;
   margin: 5% auto;
   display: flex;
   align-items: center;
@@ -24,8 +22,6 @@ const ModalContainer = styled.div`
 
   @media (max-width: ${style.collapse}px) {
     width: 90%;
-    height: 70%;
-    min-height: 400px;
   }
 `;
 
