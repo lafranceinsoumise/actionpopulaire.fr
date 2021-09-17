c4d4587a2ef1a2eba7e03ecaa1bd7db1ab826160
maxGoaz <mgoaziou@gmail.com>
2021-09-17 15:39:37 +0200

    fix: remove some containers that have been refactored into layout

--- agir/events/components/agendaPage/AgendaPage.js
+++ agir/events/components/agendaPage/AgendaPage.js
@@ -1,6 +1,5 @@
 import { Helmet } from "react-helmet";
 import React from "react";
-import styled from "styled-components";
 
 import style from "@agir/front/genericComponents/_variables.scss";
 
@@ -10,14 +9,6 @@ import { getIsSessionLoaded } from "@agir/front/globalContext/reducers";
 import Agenda from "@agir/events/agendaPage/Agenda";
 import Layout from "@agir/front/dashboardComponents/Layout";
 
-const StyledWrapper = styled.div`
-  padding-top: 72px;
-
-  @media (max-width: ${style.collapse}px) {
-    padding-top: 56px;
-  }
-`;
-
 const AgendaPage = (props) => {
   const isSessionLoaded = useSelector(getIsSessionLoaded);
 
@@ -26,16 +17,12 @@ const AgendaPage = (props) => {
   }
 
   return (
-    <>
-      <StyledWrapper>
-        <Layout active="events" smallBackgroundColor={style.black25} hasBanner>
-          <Helmet>
-            <title>Événements - Action populaire</title>
-          </Helmet>
-          <Agenda {...props} />
-        </Layout>
-      </StyledWrapper>
-    </>
+    <Layout active="events" smallBackgroundColor={style.black25} hasBanner>
+      <Helmet>
+        <title>Événements - Action populaire</title>
+      </Helmet>
+      <Agenda {...props} />
+    </Layout>
   );
 };
 
