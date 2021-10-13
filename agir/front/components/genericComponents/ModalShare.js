94fef528e6507a6d2596c79d89cf3f8472e29eb0
maxGoaz <mgoaziou@gmail.com>
2021-10-13 16:11:29 +0200

    refactor: add ShareContentUrl component, show ShareGroup in BottomSheet on mobile

--- agir/front/components/genericComponents/ModalShare.js
+++ agir/front/components/genericComponents/ModalShare.js
@@ -3,12 +3,9 @@ import React from "react";
 import styled from "styled-components";
 
 import Button from "@agir/front/genericComponents/Button";
-import { RawFeatherIcon } from "@agir/front/genericComponents/FeatherIcon";
-
 import Modal from "@agir/front/genericComponents/Modal";
-import ShareLink from "@agir/front/genericComponents/ShareLink";
 import style from "@agir/front/genericComponents/_variables.scss";
-import { FaWhatsapp, FaFacebook, FaTelegramPlane } from "react-icons/fa";
+import ShareContentUrl from "@agir/front/genericComponents/ShareContentUrl";
 
 const ModalContainer = styled.div`
   background: white;
@@ -44,74 +41,12 @@ const ModalContent = styled.div`
 const ModalShare = (props) => {
   const { shouldShow = false, onClose, url = window.location.href } = props;
 
-  const urlEncoded = encodeURIComponent(url);
-
   return (
     <Modal shouldShow={shouldShow} onClose={onClose}>
       <ModalContainer>
         <ModalContent>
           <h1>Partager la page</h1>
-
-          <Button
-            style={{ backgroundColor: style.facebook }}
-            type="button"
-            link
-            target="_blank"
-            rel="noopener noreferrer"
-            href={`https://www.facebook.com/sharer/sharer.php?u=${urlEncoded}`}
-          >
-            <FaFacebook
-              size="1.5rem"
-              height="32"
-              width="32"
-              color={style.white}
-            />
-            &nbsp; Partager sur Facebook
-          </Button>
-          <Button
-            style={{ backgroundColor: style.twitter }}
-            type="button"
-            link
-            target="_blank"
-            rel="noopener noreferrer"
-            href={`https://twitter.com/intent/tweet?text=${urlEncoded}`}
-          >
-            <RawFeatherIcon name="twitter" width="1.5rem" height="1.5rem" />
-            &nbsp; Partager sur Twitter
-          </Button>
-          <Button
-            style={{ backgroundColor: style.telegram }}
-            type="button"
-            link
-            target="_blank"
-            rel="noopener noreferrer"
-            href={`https://t.me/share/url?url=${urlEncoded}`}
-          >
-            <FaTelegramPlane
-              size="1.5rem"
-              height="32"
-              width="32"
-              color={style.white}
-            />
-            &nbsp; Partager sur Telegram
-          </Button>
-          <Button
-            style={{ backgroundColor: style.whatsapp }}
-            type="button"
-            link
-            target="_blank"
-            rel="noopener noreferrer"
-            href={`https://wa.me/?text=${urlEncoded}`}
-          >
-            <FaWhatsapp
-              size="1.5rem"
-              height="32"
-              width="32"
-              color={style.white}
-            />
-            &nbsp; Partager sur Whatsapp
-          </Button>
-          <ShareLink $wrap label="Copier le lien" url={url} />
+          <ShareContentUrl url={url} />
         </ModalContent>
       </ModalContainer>
     </Modal>
---
+++ agir/front/components/genericComponents/ShareContentUrl.js
@@ -0,0 +1,83 @@
+import PropTypes from "prop-types";
+import React from "react";
+
+import styled from "styled-components";
+
+import Button from "@agir/front/genericComponents/Button";
+import { RawFeatherIcon } from "@agir/front/genericComponents/FeatherIcon";
+import ShareLink from "@agir/front/genericComponents/ShareLink";
+import style from "@agir/front/genericComponents/_variables.scss";
+import { FaWhatsapp, FaFacebook, FaTelegramPlane } from "react-icons/fa";
+
+const StyledContainer = styled.div`
+  display: inline-flex;
+  flex-direction: column;
+  > ${Button} {
+    margin-bottom: 1rem;
+    color: white;
+  }
+`;
+
+const ShareContentUrl = ({ url }) => {
+  const urlEncoded = encodeURIComponent(url);
+
+  return (
+    <StyledContainer>
+      <Button
+        style={{ backgroundColor: style.facebook }}
+        type="button"
+        link
+        target="_blank"
+        rel="noopener noreferrer"
+        href={`https://www.facebook.com/sharer/sharer.php?u=${urlEncoded}`}
+      >
+        <FaFacebook size="1.5rem" height="32" width="32" color={style.white} />
+        &nbsp; Partager sur Facebook
+      </Button>
+      <Button
+        style={{ backgroundColor: style.twitter }}
+        type="button"
+        link
+        target="_blank"
+        rel="noopener noreferrer"
+        href={`https://twitter.com/intent/tweet?text=${urlEncoded}`}
+      >
+        <RawFeatherIcon name="twitter" width="1.5rem" height="1.5rem" />
+        &nbsp; Partager sur Twitter
+      </Button>
+      <Button
+        style={{ backgroundColor: style.telegram }}
+        type="button"
+        link
+        target="_blank"
+        rel="noopener noreferrer"
+        href={`https://t.me/share/url?url=${urlEncoded}`}
+      >
+        <FaTelegramPlane
+          size="1.5rem"
+          height="32"
+          width="32"
+          color={style.white}
+        />
+        &nbsp; Partager sur Telegram
+      </Button>
+      <Button
+        style={{ backgroundColor: style.whatsapp }}
+        type="button"
+        link
+        target="_blank"
+        rel="noopener noreferrer"
+        href={`https://wa.me/?text=${urlEncoded}`}
+      >
+        <FaWhatsapp size="1.5rem" height="32" width="32" color={style.white} />
+        &nbsp; Partager sur Whatsapp
+      </Button>
+      <ShareLink $wrap label="Copier le lien" url={url} />
+    </StyledContainer>
+  );
+};
+ShareContentUrl.propTypes = {
+  url: PropTypes.string,
+};
+
+export default ShareContentUrl;
