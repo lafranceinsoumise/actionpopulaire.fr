a26d8dfadbeacb7f488481ff29a0a9ad1fb9accd
maxGoaz <mgoaziou@gmail.com>
2021-10-20 16:05:51 +0200

    refactor: uniformize style for native button and StyledLink

--- agir/groups/components/groupPage/GroupUserActions/SecondaryActions.js
+++ agir/groups/components/groupPage/GroupUserActions/SecondaryActions.js
@@ -1,9 +1,9 @@
 import PropTypes from "prop-types";
 import React, { useState, useCallback } from "react";
 import styled from "styled-components";
+import style from "@agir/front/genericComponents/_variables.scss";
 
 import Link from "@agir/front/app/Link";
-import Button from "@agir/front/genericComponents/Button";
 import { RawFeatherIcon } from "@agir/front/genericComponents/FeatherIcon";
 import ShareLink from "@agir/front/genericComponents/ShareLink";
 import Spacer from "@agir/front/genericComponents/Spacer";
@@ -18,20 +18,32 @@ const StyledWrapper = styled.div`
   margin-top: 1rem;
 `;
 
+const StyledLink = styled(Link)``;
+
 const StyledContainer = styled.div`
   display: flex;
   flex-align: row;
+  flex-direction: row;
   justify-content: center;
+  width: 100%;
+  justify-content: space-around;
 
-  ${Button} {
+  button,
+  ${StyledLink} {
     background-color: white;
     border: none;
     font-size: 0.875rem;
-
-    & > span {
-      flex-direction: column;
-      width: inherit;
-    }
+    cursor: pointer;
+    display: inline-flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    color: ${style.black1000};
+    font-weight: 500;
+  }
+  button:hover,
+  ${StyledLink}:hover {
+    text-decoration: underline;
   }
 `;
 
@@ -48,24 +60,21 @@ const SecondaryActions = ({ id, isCertified, routes, contact }) => {
     <StyledWrapper>
       <StyledContainer>
         {contact?.email && (
-          <Button type="button" onClick={handleContactOpen}>
+          <button type="button" onClick={handleContactOpen}>
             <RawFeatherIcon name="mail" width="1.5rem" height="1.5rem" />
             Contacter
-          </Button>
+          </button>
         )}
         {isCertified && (
-          <Link route="donations" params={{ group: id }}>
-            <Button type="button">
-              <RawFeatherIcon name="upload" width="1.5rem" height="1.5rem" />
-              Financer
-            </Button>
-          </Link>
+          <StyledLink route="donations" params={{ group: id }}>
+            <RawFeatherIcon name="upload" width="1.5rem" height="1.5rem" />
+            Financer
+          </StyledLink>
         )}
-        <Button type="button" onClick={handleShareOpen}>
+        <button type="button" onClick={handleShareOpen}>
           <RawFeatherIcon name="share-2" width="1.5rem" height="1.5rem" />
           Partager
-        </Button>
-
+        </button>
         <ModalConfirmation
           shouldShow={isShareOpen}
           onClose={handleShareClose}
