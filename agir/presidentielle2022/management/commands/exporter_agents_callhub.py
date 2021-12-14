533442393ae1b6ae0cde76b9c7f1e3996279b644
Arthur Cheysson <arthur@cheysson.fr>
2021-12-14 11:28:22 +0100

    fix(callhub): changer paramètres rejeu et timeout pour robustesse++

--- agir/presidentielle2022/management/commands/exporter_agents_callhub.py
+++ agir/presidentielle2022/management/commands/exporter_agents_callhub.py
@@ -16,8 +16,9 @@ from agir.people.person_forms.models import PersonForm
 logger = logging.getLogger(__name__)
 
 RATE_LIMIT = 13
-CONCURRENT_REQUESTS = 20
-HTTP_TIMEOUT = 5
+CONCURRENT_REQUESTS = 15
+HTTP_TIMEOUT = 15
+MAX_RETRIES = 10
 
 OUTREMER = {
     "971": "ZA",
@@ -87,7 +88,7 @@ class Command(LoggingCommand):
     @tenacity.retry(
         sleep=trio.sleep,
         wait=tenacity.wait_random_exponential(),
-        stop=tenacity.stop_after_attempt(3),
+        stop=tenacity.stop_after_attempt(MAX_RETRIES),
         retry=tenacity.retry_if_exception_type((trio.TooSlowError, AsksException)),
     )
     async def request(self, method, path, raise_for_status=True, **kwargs):
