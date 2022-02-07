86165fc741c983625442457a03790933dc8263ac
Giuseppe De Ponte <giuseppedeponte@gmail.com>
2022-02-07 17:49:44 +0100

    fix: useIsOffline forced re-rendering

--- agir/front/components/offline/hooks.js
+++ agir/front/components/offline/hooks.js
@@ -10,24 +10,31 @@ const pinger = async (url) => {
   }
 };
 
+const pingerConfig = {
+  refreshInterval: 10000,
+  dedupingInterval: 10000,
+  focusThrottleInterval: 10000,
+  refreshWhenOffline: true,
+  shouldRetryOnError: false,
+  revalidateIfStale: false,
+};
+
 export const useIsOffline = () => {
-  const { data: isOnline, isValidating } = useSWR("/api/ping/", pinger, {
-    refreshInterval: 10000,
-    dedupingInterval: 10000,
-    focusThrottleInterval: 10000,
-    refreshWhenOffline: true,
-    shouldRetryOnError: false,
-    revalidateIfStale: false,
-  });
+  const { data: isOnline, isValidating } = useSWR(
+    "/api/ping/",
+    pinger,
+    pingerConfig
+  );
 
   const [isUnloading, setIsUnloading] = useState(false);
+
   useEffect(() => {
     let handleBeforeUnload = () => setIsUnloading(true);
     window.addEventListener("beforeunload", handleBeforeUnload);
     return () => {
       window.removeEventListener("beforeunload", handleBeforeUnload);
     };
-  });
+  }, []);
 
   return !isUnloading && typeof isOnline === "boolean" ? !isOnline : null;
 };
