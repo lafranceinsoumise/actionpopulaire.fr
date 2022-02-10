985f4ed2652fe7fbae30499dc50e06d08aa8aa84
maxGoaz <mgoaziou@gmail.com>
2022-02-10 11:31:01 +0100

    fix: use general delete_rsvp rule

--- agir/events/admin/panels.py
+++ agir/events/admin/panels.py
@@ -617,7 +617,7 @@ class RSVPAdmin(admin.ModelAdmin):
         return False
 
     def has_delete_permission(self, request, obj=None):
-        return request.user.has_perm("events.delete_rsvp_for_event")
+        return request.user.has_perm("events.delete_rsvp")
 
     def has_view_permission(self, request, obj=None):
         return request.user.has_perm("events.view_rsvp")
