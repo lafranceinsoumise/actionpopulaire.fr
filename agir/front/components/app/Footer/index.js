0a29e570b56c6862fcb6342b5d95ae12139fd7df
Giuseppe De Ponte <giuseppedeponte@gmail.com>
2022-02-11 16:12:15 +0100

    feat: extract TopBar and Footer from Router Switch

--- agir/front/components/app/Footer/index.js
+++ agir/front/components/app/Footer/index.js
@@ -1,48 +1,48 @@
 import PropTypes from "prop-types";
-import React, { Suspense } from "react";
+import React, { Suspense, useMemo } from "react";
+import { useLocation } from "react-router-dom";
 
 import { lazy } from "@agir/front/app/utils";
-
-import { useSelector } from "@agir/front/globalContext/GlobalContext";
 import {
   getIsSessionLoaded,
-  getUser,
+  getIsConnected,
 } from "@agir/front/globalContext/reducers";
+import routes from "@agir/front/app/routes.config";
 import { useMobileApp } from "@agir/front/app/hooks";
+import { useSelector } from "@agir/front/globalContext/GlobalContext";
 
 const Footer = lazy(() => import("./Footer"));
 
-const ConnectedFooter = (props) => {
-  const { hideBanner, displayOnMobileApp } = props;
-
+const ConnectedFooter = () => {
+  const { pathname } = useLocation();
   const isSessionLoaded = useSelector(getIsSessionLoaded);
-  const user = useSelector(getUser);
+  const isConnected = useSelector(getIsConnected);
   const { isMobileApp } = useMobileApp();
 
-  if (isMobileApp && !displayOnMobileApp) {
-    return null;
-  }
+  const route = useMemo(
+    () => routes.find((route) => route.match(pathname)),
+    [pathname]
+  );
 
   if (!isSessionLoaded) {
+    return null;
+  }
+  if (!route || route.hideFooter) {
+    return null;
+  }
+  if (isMobileApp && !route.displayFooterOnMobileApp) {
     return null;
   }
 
   return (
     <Suspense fallback={<div />}>
       <Footer
-        {...props}
-        isSignedIn={!!user}
+        isSignedIn={isConnected}
         isMobileApp={isMobileApp}
-        hasBanner={!hideBanner && !user}
+        hasBanner={!route.hideFooterBanner && !isConnected}
       />
     </Suspense>
   );
-};
-
-ConnectedFooter.propTypes = {
-  isSignedIn: PropTypes.bool,
-  hideBanner: PropTypes.bool,
-  displayOnMobileApp: PropTypes.bool,
 };
 
 export default ConnectedFooter;
