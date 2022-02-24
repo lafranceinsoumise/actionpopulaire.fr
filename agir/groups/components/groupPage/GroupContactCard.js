2bc50c5f6567942cae1ae4f01f8bbb76bc72d680
Giuseppe De Ponte <giuseppedeponte@gmail.com>
2022-02-24 18:15:31 +0100

    feat: remove referents from GroupContactCard

--- agir/groups/components/groupPage/GroupContactCard.js
+++ agir/groups/components/groupPage/GroupContactCard.js
@@ -1,51 +1,13 @@
 import PropTypes from "prop-types";
-import React, { useMemo } from "react";
+import React from "react";
 import styled from "styled-components";
 
 import style from "@agir/front/genericComponents/_variables.scss";
-import { getGenderedWord } from "@agir/lib/utils/display";
-
-import Avatar from "@agir/front/genericComponents/Avatar";
-import { RawFeatherIcon } from "@agir/front/genericComponents/FeatherIcon";
-import Link from "@agir/front/app/Link";
 
 import Card from "./GroupPageCard";
+import Link from "@agir/front/app/Link";
+import { RawFeatherIcon } from "@agir/front/genericComponents/FeatherIcon";
 
-const StyledReferentSection = styled.section`
-  margin-bottom: 1.5rem;
-
-  &:empty {
-    display: none;
-    margin: 0;
-  }
-
-  ${Avatar} {
-    width: ${({ $length }) => ($length > 3 ? "3rem" : "4rem")};
-    height: ${({ $length }) => ($length > 3 ? "3rem" : "4rem")};
-  }
-
-  ${Avatar} + ${Avatar} {
-    margin-left: 4px;
-  }
-
-  && p {
-    font-size: 0.875rem;
-    line-height: 1.5;
-    margin: 0;
-
-    &:nth-child(2) {
-      font-size: 1.125rem;
-
-      @media (max-width: ${style.collapse}px) {
-        font-size: 1rem;
-      }
-    }
-
-    && strong {
-      font-weight: 600;
-    }
-  }
-`;
 const StyledContactSection = styled.p`
   font-size: 14px;
   line-height: 1.5;
@@ -68,35 +30,14 @@ const StyledContactSection = styled.p`
 `;
 
 const GroupContactCard = (props) => {
-  const { referents, contact, editLinkTo } = props;
+  const { contact, editLinkTo } = props;
 
-  const referentTitle = useMemo(() => {
-    if (!Array.isArray(referents) || referents.length === 0) {
-      return "";
-    }
-    const gender = referents.reduce((genders, referent) => {
-      const gender = referent.gender || "?";
-      return gender === genders ? genders : genders + gender;
-    }, "");
-    return `${getGenderedWord(
-      gender,
-      "Animateur·ice",
-      "Animatrice",
-      "Animateur"
-    )}${referents.length > 1 ? "s" : ""}`;
-  }, [referents]);
-
-  if (!referents && !contact) {
+  if (!contact) {
     return null;
   }
 
   return (
     <Card>
-      {Array.isArray(referents) && referents.length > 0 ? (
-        <StyledReferentSection>
-          <p>{referentTitle} du groupe</p>
-        </StyledReferentSection>
-      ) : null}
       {contact ? (
         <StyledContactSection>
           {contact.name && (
@@ -130,17 +71,13 @@ const GroupContactCard = (props) => {
 };
 
 GroupContactCard.propTypes = {
-  referents: PropTypes.arrayOf(
-    PropTypes.shape({
-      image: PropTypes.string,
-      gender: PropTypes.string,
-    })
-  ),
   contact: PropTypes.shape({
     name: PropTypes.string,
     email: PropTypes.string,
     phone: PropTypes.string,
+    hidePhone: PropTypes.bool,
   }),
   editLinkTo: PropTypes.string,
 };
+
 export default GroupContactCard;
