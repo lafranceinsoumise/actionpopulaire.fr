e29e3ff5118d0495beed1106d68869f73c29b70e
maxGoaz <mgoaziou@gmail.com>
2022-03-22 15:30:01 +0100

    feat: add optional link to GroupItems

--- agir/events/components/EventSettings/EventParticipants.js
+++ agir/events/components/EventSettings/EventParticipants.js
@@ -98,6 +98,7 @@ const EventParticipants = (props) => {
                 id={group.id}
                 name={group.name}
                 image={group.image}
+                isLinked
               />
             ))}
           </GroupList>
