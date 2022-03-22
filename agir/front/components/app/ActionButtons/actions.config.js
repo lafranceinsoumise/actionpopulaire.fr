aa3ed788b897163727da5428967f945bff2e829e
Giuseppe De Ponte <giuseppedeponte@gmail.com>
2022-03-22 17:55:51 +0100

    feat: add polling station assessor form action button

--- agir/front/components/app/ActionButtons/actions.config.js
+++ agir/front/components/app/ActionButtons/actions.config.js
@@ -119,28 +119,49 @@ const ACTIONS = {
       />
     ),
   },
+  "pollingStationAssessorForm": {
+    key: "pollingStationAssessorForm",
+    route: "pollingStationAssessorForm",
+    label: ["Assesseurs/délégués", "Devenir assesseur·e ou délégué·e"],
+    icon: (
+      <span style={{ backgroundColor: style.referralPink }}>
+        <svg width="25" height="24" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <g clipPath="url(#clip0_5242_40128)">
+            <path
+              d="M3.5 15H2C1.44772 15 1 15.4477 1 16V21C1 21.5523 1.44772 22 2 22H22C22.5523 22 23 21.5523 23 21V16.5C23 15.9477 22.5523 15.5 22 15.5H20.5M5.5 15.5V3C5.5 2.44772 5.94772 2 6.5 2H17.5C18.0523 2 18.5 2.44771 18.5 3V15.5C18.5 16.0523 18.0523 16.5 17.5 16.5H6.5C5.94772 16.5 5.5 16.0523 5.5 15.5Z"
+              stroke="white"
+              strokeWidth="2"/>
+            <path d="M9 8.6L10.299 10.1588C10.6754 10.6105 11.3585 10.6415 11.7743 10.2257L15 7" stroke="white" strokeWidth="2"/>
+          </g>
+          <defs>
+            <clipPath id="clip0_5242_40128">
+              <rect width="24" height="24" fill="white" transform="translate(0.5)"/>
+            </clipPath>
+          </defs>
+        </svg>
+        </span>
+      ),
+  }
 };
 
 const DEFAULT_ACTION_ORDER = [
-  "groups",
   "donations",
   "createEvent",
-  "coupDeFil",
   "materiel",
+  "pollingStationAssessorForm",
   "createContact",
+  "coupDeFil",
   "toktokPreview",
-  "votingProxy",
   "actionTools",
 ];
 const GROUP_MANAGER_ACTION_ORDER = [
   "createEvent",
   "materiel",
+  "pollingStationAssessorForm",
   "createContact",
   "toktokPreview",
   "coupDeFil",
   "donations",
-  "groups",
-  "votingProxy",
   "actionTools",
 ];
 
