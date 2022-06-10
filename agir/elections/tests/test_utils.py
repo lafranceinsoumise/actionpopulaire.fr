df037116b7e1467123b172aef4a27157130c3b7d
Giuseppe De Ponte <giuseppedeponte@gmail.com>
2022-06-10 11:23:42 +0200

    feat: refactor electoral truce checks

--- agir/elections/tests/test_utils.py
+++ agir/elections/tests/test_utils.py
@@ -13,73 +13,101 @@ class IsForbiddenDuringTreveEventTestCase(TestCase):
         (self.unauthorized_subtype, created) = EventSubtype.objects.get_or_create(
             label="porte-a-porte"
         )
+        self.treve_country_code = TREVE_ELECTORALE[0][2][0]
+        self.authorized_country_code = "UK"
         self.treve_start, self.treve_end, *rest = TREVE_ELECTORALE[0]
 
     def test_before_treve_event(self):
-        event = Event.objects.create(
-            start_time=self.treve_start - timedelta(days=5),
-            end_time=self.treve_start - timedelta(days=4),
-            subtype_id=self.unauthorized_subtype.id,
-        )
-        result = is_forbidden_during_treve_event(event)
+        event_data = {
+            "start_time": self.treve_start - timedelta(days=5),
+            "end_time": self.treve_start - timedelta(days=4),
+            "subtype": self.unauthorized_subtype,
+            "location_country": self.treve_country_code,
+        }
+        result = is_forbidden_during_treve_event(event_data)
         self.assertFalse(result)
 
     def test_after_treve_event(self):
-        event = Event.objects.create(
-            start_time=self.treve_end + timedelta(days=4),
-            end_time=self.treve_end + timedelta(days=5),
-            subtype_id=self.unauthorized_subtype.id,
-        )
-        result = is_forbidden_during_treve_event(event)
+        event_data = {
+            "start_time": self.treve_end + timedelta(days=4),
+            "end_time": self.treve_end + timedelta(days=5),
+            "subtype": self.unauthorized_subtype,
+            "location_country": self.treve_country_code,
+        }
+        result = is_forbidden_during_treve_event(event_data)
         self.assertFalse(result)
 
-    def test_during_treve_event(self):
-        event = Event.objects.create(
-            start_time=self.treve_start + timedelta(minutes=4),
-            end_time=self.treve_end - timedelta(minutes=4),
-            subtype_id=self.unauthorized_subtype.id,
-        )
-        result = is_forbidden_during_treve_event(event)
+    def test_during_treve_event_creation(self):
+        event_data = {
+            "start_time": self.treve_start + timedelta(minutes=4),
+            "end_time": self.treve_end - timedelta(minutes=4),
+            "subtype": self.unauthorized_subtype,
+            "location_country": self.treve_country_code,
+        }
+        result = is_forbidden_during_treve_event(event_data)
         self.assertTrue(result)
-        event = Event.objects.create(
-            start_time=self.treve_start + timedelta(minutes=4),
-            end_time=self.treve_end + timedelta(minutes=4),
-            subtype_id=self.unauthorized_subtype.id,
-        )
-        result = is_forbidden_during_treve_event(event)
+        event_data = {
+            "start_time": self.treve_start + timedelta(minutes=4),
+            "end_time": self.treve_end + timedelta(minutes=4),
+            "subtype": self.unauthorized_subtype,
+            "location_country": self.treve_country_code,
+        }
+        result = is_forbidden_during_treve_event(event_data)
         self.assertTrue(result)
-        event = Event.objects.create(
-            start_time=self.treve_start - timedelta(minutes=4),
-            end_time=self.treve_end - timedelta(minutes=4),
-            subtype_id=self.unauthorized_subtype.id,
-        )
-        result = is_forbidden_during_treve_event(event)
+        event_data = {
+            "start_time": self.treve_start - timedelta(minutes=4),
+            "end_time": self.treve_end - timedelta(minutes=4),
+            "subtype": self.unauthorized_subtype,
+            "location_country": self.treve_country_code,
+        }
+        result = is_forbidden_during_treve_event(event_data)
         self.assertTrue(result)
 
-    def test_authorized_event_during_treve(self):
-        event = Event.objects.create(
-            start_time=self.treve_start + timedelta(minutes=4),
-            end_time=self.treve_end - timedelta(minutes=4),
-            subtype_id=self.authorized_subtype.id,
-        )
-        result = is_forbidden_during_treve_event(event)
+    def test_during_treve_event_update(self):
+        event_data = {
+            "start_time": self.treve_start + timedelta(minutes=4),
+            "end_time": self.treve_end - timedelta(minutes=4),
+            "subtype_id": self.unauthorized_subtype.id,
+            "location_country": self.treve_country_code,
+        }
+        event = Event.objects.create(**event_data)
+        event_data = {"id": event.pk}
+        result = is_forbidden_during_treve_event(event_data)
+        self.assertTrue(result)
+        event_data = {
+            **event_data,
+            "start_time": self.treve_start + timedelta(minutes=4),
+            "end_time": self.treve_end + timedelta(minutes=4),
+        }
+        result = is_forbidden_during_treve_event(event_data)
+        self.assertTrue(result)
+        event_data = {
+            **event_data,
+            "start_time": self.treve_start - timedelta(minutes=4),
+            "end_time": self.treve_end - timedelta(minutes=4),
+        }
+        result = is_forbidden_during_treve_event(event_data)
+        self.assertTrue(result)
+
+    def test_authorized_event_subtype_during_treve(self):
+        event_data = {
+            "start_time": self.treve_start + timedelta(minutes=4),
+            "end_time": self.treve_end - timedelta(minutes=4),
+            "subtype": self.authorized_subtype,
+            "location_country": self.treve_country_code,
+        }
+        result = is_forbidden_during_treve_event(event_data)
         self.assertFalse(result)
 
-    def test_uuid_event_argument(self):
-        event = Event.objects.create(
-            start_time=self.treve_start + timedelta(minutes=4),
-            end_time=self.treve_end - timedelta(minutes=4),
-            subtype_id=self.authorized_subtype.id,
-        )
-        result = is_forbidden_during_treve_event(event.pk)
-        self.assertFalse(result)
-
-    def test_uuid_dict_argument(self):
-        result = is_forbidden_during_treve_event(
-            {
-                "start_time": self.treve_start + timedelta(minutes=4),
-                "end_time": self.treve_end - timedelta(minutes=4),
-                "subtype": self.authorized_subtype,
-            }
-        )
+    def test_authorized_country_codes_during_treve(self):
+        event_data = {
+            "start_time": self.treve_start + timedelta(minutes=4),
+            "end_time": self.treve_end - timedelta(minutes=4),
+            "subtype": self.unauthorized_subtype,
+            "location_country": self.treve_country_code,
+        }
+        result = is_forbidden_during_treve_event(event_data)
+        self.assertTrue(result)
+        event_data = {**event_data, "location_country": self.authorized_country_code}
+        result = is_forbidden_during_treve_event(event_data)
         self.assertFalse(result)
--- agir/elections/utils.py
+++ agir/elections/utils.py
@@ -1,34 +1,63 @@
-from uuid import UUID
-
 from iso8601 import iso8601
 
 from agir.events.models import Event
+from agir.lib.geo import FRENCH_COUNTRY_CODES
 
-TREVE_ELECTORALE = [
-    # Législatives 2022
-    ("2022-06-11 00:00:00+0200", "2022-06-12 20:00:00+0200", ("soiree-electorale",)),
-    ("2022-06-18 00:00:00+0200", "2022-06-19 20:00:00+0200", ("soiree-electorale",)),
-]
 TREVE_ELECTORALE = [
     (iso8601.parse_date(start), iso8601.parse_date(end), *rest)
-    for (start, end, *rest) in TREVE_ELECTORALE
+    for (start, end, *rest) in [
+        # Législatives 2022
+        (
+            "2022-06-11 00:00:00+0200",  # Start date
+            "2022-06-12 20:00:00+0200",  # End date
+            (FRENCH_COUNTRY_CODES[0],),  # Limited to country codes
+            ("soiree-electorale",),  # Authorized event subtypes
+        ),
+        (
+            "2022-06-18 00:00:00+0200",
+            "2022-06-19 20:00:00+0200",
+            None,
+            ("soiree-electorale",),
+        ),
+    ]
 ]
 
 
-def is_forbidden_during_treve_event(event):
-    if isinstance(event, UUID):
-        event = Event.objects.get(pk=event)
-    if isinstance(event, dict):
-        start_time = event.get("start_time", None)
-        end_time = event.get("end_time", None)
-        subtype = event.get("subtype", None)
-        if not start_time and not end_time and not subtype:
-            return False
-        event = Event(start_time=start_time, end_time=end_time, subtype=subtype)
+def is_forbidden_during_treve_event(event_data):
+    event_pk = event_data.get("id", None)
+    if event_pk:
+        event = Event.objects.get(pk=event_pk)
+        start_time = event_data.get("start_time", event.start_time)
+        end_time = event_data.get("end_time", event.end_time)
+        subtype = event_data.get("subtype", event.subtype)
+        country = event_data.get("location_country", event.location_country)
+    else:
+        start_time = event_data.get("start_time", None)
+        end_time = event_data.get("end_time", None)
+        subtype = event_data.get("subtype", None)
+        country = event_data.get("location_country", None)
 
-    for treve_start, treve_end, authorized_subtype_labels in TREVE_ELECTORALE:
+    event = Event(
+        start_time=start_time,
+        end_time=end_time,
+        subtype=subtype,
+        location_country=country,
+    )
+
+    for (
+        treve_start,
+        treve_end,
+        country_codes,
+        authorized_subtype_labels,
+    ) in TREVE_ELECTORALE:
         if (
-            hasattr(event, "subtype")
+            country_codes
+            and event.location_country
+            and event.location_country.code not in country_codes
+        ):
+            continue
+        if (
+            authorized_subtype_labels
             and event.subtype
             and event.subtype.label in authorized_subtype_labels
         ):
