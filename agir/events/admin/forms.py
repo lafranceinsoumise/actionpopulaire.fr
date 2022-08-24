2d9d1177e3975e71071261a1b7215e83f3f8eb05
Salomé Cheysson <salome@cheysson.fr>
2022-08-24 16:34:39 +0200

    feat(event): ne plus imposer adresse pour paiement dans admin

--- agir/events/admin/forms.py
+++ agir/events/admin/forms.py
@@ -312,17 +312,6 @@ class NewParticipantForm(BasePersonForm):
                 if f not in self.cleaned_data:
                     self.data[f] = getattr(self.cleaned_data["existing_person"], f)
 
-        if self.cleaned_data.get("payment_mode", None) not in (
-            PAYMENT_MODES["tpe"],
-            PAYMENT_MODES["money"],
-            None,
-        ):
-            for f in BILLING_FIELDS:
-                if f != "location_address2" and not self.cleaned_data.get(f):
-                    self.add_error(
-                        f, ValidationError("Champ requis pour ce mode de paiement.")
-                    )
-
         return self.cleaned_data
 
     @property
