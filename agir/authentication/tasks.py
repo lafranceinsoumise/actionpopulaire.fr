2358a96f2c3b2f9708372f28bed8f27204b30706
Salomé Cheysson <salome@cheysson.fr>
2022-11-23 14:54:01 +0100

    fix(authentication): envoi du message pour se login

--- agir/authentication/tasks.py
+++ agir/authentication/tasks.py
@@ -22,7 +22,7 @@ def send_login_email(email, short_code, expiry_time):
 
     code = interleave_spaces(short_code)
     send_template_email(
-        template_name="authentication/LOGIN_MESSAGE.html",
+        template_name="authentication/login_email.html",
         from_email=settings.EMAIL_FROM,
         bindings={
             "code": code,
