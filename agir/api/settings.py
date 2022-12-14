0f316cb0badd698691193e0d018b645b98e9ad9f
Salomé Cheysson <salome@cheysson.fr>
2022-12-14 14:24:57 +0100

    fix(cagnottes): CORS sur le compteur

--- agir/api/settings.py
+++ agir/api/settings.py
@@ -684,7 +684,7 @@ CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIAL = False
 # TODO: tous les points API n'ont pas vocation à être accédés depuis n'importe quel site
 # à mon avis il faudrait restreindre
-CORS_URLS_REGEX = r"^(?:/legacy/|/communes/chercher/|/api/|/caisse/compteur/)"
+CORS_URLS_REGEX = r"^(?:/legacy/|/communes/chercher/|/api/|/cagnottes/[^/]+/compteur/$)"
 
 SECURE_CONTENT_TYPE_NOSNIFF = True
 SECURE_BROWSER_XSS_FILTER = True
