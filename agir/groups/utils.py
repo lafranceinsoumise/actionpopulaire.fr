7895b50e0334e7318a2c0e1e7ed9d91c80fe98eb
Salomé Cheysson <salome@cheysson.fr>
2022-12-22 19:18:50 +0100

    commentaires

--- agir/groups/utils.py
+++ agir/groups/utils.py
@@ -12,11 +12,17 @@ DAYS_SINCE_LAST_EVENT_WARNING = 45
 
 
 def is_new_group_filter():
+    # âge du groupe = maintenant - date de création <= durée max
+    # ==>
+    # date de création > maintenant - durée max
     limit_group_creation_date = now() - timedelta(days=DAYS_SINCE_GROUP_CREATION_LIMIT)
     return Q(created__gte=limit_group_creation_date)
 
 
 def has_recent_event_filter():
+    # âge de l'événement = maintenant - date événement <= durée max
+    # =>
+    # date événement >= maintenant - durée max
     limit_event_date = now() - timedelta(days=DAYS_SINCE_LAST_EVENT_LIMIT)
     return Q(
         organized_events__start_time__gte=limit_event_date,
@@ -29,6 +35,9 @@ def is_active_group_filter():
 
 
 def get_soon_to_be_inactive_groups(exact=False):
+    # âge dernier événement = maintenant - date événement =/<= durée d'avertissement
+    # ==>
+    # date événement =/>= maintenant - durée d'avertissement = date limite
     limit_date = (now() - timedelta(days=DAYS_SINCE_LAST_EVENT_WARNING)).date()
     qs = (
         SupportGroup.objects.active()
@@ -41,7 +50,7 @@ def get_soon_to_be_inactive_groups(exact=False):
             )
         )
     )
-    if exact is True:
+    if exact:
         # Keep only groups :
         # - with no events whose creation date was exactly n days ago
         # - whose last event start date was n days ago
