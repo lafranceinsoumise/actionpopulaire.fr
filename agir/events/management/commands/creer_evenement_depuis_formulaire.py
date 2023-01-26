79d31e817768a5148483beedcc24d763e1a8fc68
Salomé Cheysson <salome@cheysson.fr>
2023-01-26 18:20:23 +0100

    feat: améliore la commande pour créer des événements

--- agir/events/management/commands/creer_evenement_depuis_formulaire.py
+++ agir/events/management/commands/creer_evenement_depuis_formulaire.py
@@ -2,28 +2,16 @@ import logging
 from datetime import timedelta
 
 from django.db import transaction
-from django.db.models import (
-    DateTimeField,
-    Exists,
-    OuterRef,
-    Func,
-    F,
-    Value,
-    IntegerField,
-)
+from django.db.models import Func, F, Value, UUIDField
 from django.db.models.functions import Cast
-from django.utils import timezone
 from iso8601 import parse_date
 
 from agir.events.models import Event, EventSubtype, OrganizerConfig
 from agir.events.tasks import geocode_event
-from agir.gestion.models import Projet
-from agir.gestion.typologies import TypeProjet
 from agir.groups.models import SupportGroup
 from agir.lib.management_utils import LoggingCommand
 from agir.people.person_forms.display import default_person_form_display
 from agir.people.person_forms.models import PersonForm
-
 
 logger = logging.getLogger(__name__)
 
@@ -58,57 +46,70 @@ def description_from_submission(s):
 class Command(LoggingCommand):
     def add_arguments(self, parser):
         parser.add_argument("form_slug")
+        parser.add_argument("event_subtype_label")
+        parser.add_argument("-d", "--define", nargs=2, action="append")
 
-    def handle(self, *args, form_slug, **options):
-        now = timezone.now()
+    def handle(self, *args, form_slug, event_subtype_label, define, **options):
+        command_values = dict(define or ())
         form = PersonForm.objects.get(slug=form_slug)
-        reunion_publique = EventSubtype.objects.get(label="reunion-publique")
+        event_subtype = EventSubtype.objects.get(label=event_subtype_label)
         submissions = form.submissions.annotate(
-            projet_cree=Exists(
-                Projet.objects.annotate(
-                    submission_id=Cast(
-                        Func(
-                            F("details"),
-                            Value("submission_id"),
-                            function="jsonb_extract_path_text",
-                        ),
-                        output_field=IntegerField(),
-                    )
-                ).filter(submission_id=OuterRef("id"))
-            ),
-            possible_date=Cast(
+            evenement_existe=Cast(
                 Func(
                     F("data"),
-                    Value("possible_date"),
+                    Value("_event_id"),
                     function="jsonb_extract_path_text",
                 ),
-                output_field=DateTimeField(),
+                output_field=UUIDField(),
             ),
-        ).filter(projet_cree=False, possible_date__gt=now)
+        ).filter(evenement_existe__isnull=True)
 
-        logger.info(f"Création de {submissions.count()} nouveaux projets")
+        logger.info(f"Création de {submissions.count()} nouveaux événements")
         # créer les projets correspondants
-        for s in submissions.filter(projet_cree=False):
-            name = f'Réunion publique à {s.data["location_city"]}'
-            date = parse_date(s.data["possible_date"])
+        for s in submissions:
+            values = {**s.data, **command_values}
 
-            if s.data.get("organizer_group"):
+            name = values.pop("name", None)
+            if not name:
+                name = f"{event_subtype.description} à {values['location_city']}"
+
+            start_time = parse_date(values.pop("start_time"))
+            end_time = (
+                parse_date(values.pop("end_time"))
+                if values.get("end_time")
+                else start_time + timedelta(hours=2)
+            )
+
+            if "organizer_group" in values:
                 try:
-                    group = SupportGroup.objects.get(id=s.data["organizer_group"])
+                    group = SupportGroup.objects.get(id=s.data.pop("organizer_group"))
                 except SupportGroup.DoesNotExist:
                     group = None
             else:
                 group = None
+
+            if values.pop("publish_contact_information"):
+                if "contact_name" not in values:
+                    values[
+                        "contact_name"
+                    ] = f"{values.pop('first_name')} {values.pop('last_name')}"
+
+                if "email" in values:
+                    values["contact_email"] = values.pop("email")
+
+                values["contact_hide_phone"] = False
+            else:
+                values["contact_hide_phone"] = True
 
             logger.debug(f"Création projet et événement « {name} »")
 
             with transaction.atomic():
                 event = Event.objects.create(
                     name=name[: Event._meta.get_field("name").max_length],
-                    visibility=Event.VISIBILITY_ORGANIZER,
-                    subtype=reunion_publique,
-                    start_time=date,
-                    end_time=date + timedelta(hours=2),
+                    visibility=Event.VISIBILITY_ADMIN,
+                    subtype=event_subtype,
+                    start_time=start_time,
+                    end_time=end_time,
                     **{
                         k: v[: Event._meta.get_field(k).max_length]
                         for k, v in s.data.items()
@@ -118,6 +119,10 @@ class Command(LoggingCommand):
                             "location_address1",
                             "location_zip",
                             "location_city",
+                            "contact_name",
+                            "contact_email",
+                            "contact_phone",
+                            "contact_hide_phone",
                         ]
                     },
                 )
@@ -126,14 +131,7 @@ class Command(LoggingCommand):
                     event=event, person=s.person, as_group=group
                 )
 
-                Projet.objects.create(
-                    titre=name[: Projet._meta.get_field("titre").max_length],
-                    event=event,
-                    type=TypeProjet.REUNION_PUBLIQUE_ORATEUR,
-                    origine=Projet.Origin.REUNION_PUBLIQUE,
-                    etat=Projet.Etat.CREE_PLATEFORME,
-                    description=description_from_submission(s),
-                    details={"submission_id": s.id},
-                )
+                s.data["_event_id"] = event.id
+                s.save(update_fields=["data"])
 
             geocode_event.delay(event.pk)
