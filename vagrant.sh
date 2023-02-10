4aac84a008fa1f07016401d75943be706185bb9c
Salomé Cheysson <salome@cheysson.fr>
2023-02-10 15:40:47 +0100

    fix: quelques bugs dans vagrant.sh

--- vagrant.sh
+++ vagrant.sh
@@ -35,6 +35,16 @@ sudo localectl set-locale LANG=fr_FR.UTF-8
 echo "## Update packages"
 sudo apt-get update -qq &> /dev/null
 
+echo "## Install postgresql..."
+if ! dpkg -s postgresql-12 &> /dev/null; then
+    sudo apt-get -yqq install postgresql-12 postgresql-12-postgis-3 postgresql-12-postgis-3-scripts libpq-dev &> /dev/null
+fi
+sudo -u postgres psql -c "CREATE ROLE api WITH PASSWORD 'password';" || echo "PostgreSQL role already exists"
+sudo -u postgres psql -c "CREATE DATABASE api WITH owner api;" || echo "PostgreSQL database already exists"
+sudo -u postgres psql -c "ALTER ROLE api WITH superuser;"
+sudo -u postgres psql -c "ALTER ROLE api WITH login;"
+sudo -u postgres psql -c "CREATE EXTENSION postgis;" || echo "Postgis extension already installed"
+
 echo "## Install Python..."
 if ! (dpkg -s python3.9 && dpkg -s python3.9-dev) &> /dev/null; then
     sudo apt-get -yqq install python3.9 python3.9-dev python3.9-venv python3.9-distutils python3-pip libsystemd-dev &> /dev/null
@@ -68,16 +78,6 @@ if [ ! -x MailHog ]; then
     wget --quiet -O MailHog https://github.com/mailhog/MailHog/releases/download/v1.0.0/MailHog_linux_amd64
     chmod +x MailHog
 fi
-
-echo "## Install postgresql..."
-if ! dpkg -s postgresql-12 &> /dev/null; then
-    sudo apt-get -yqq install postgresql-12 postgresql-12-postgis-3 postgresql-12-postgis-3-scripts libpq-dev &> /dev/null
-fi
-sudo -u postgres psql -c "CREATE ROLE api WITH PASSWORD 'password';" || echo "PostgreSQL role already exists"
-sudo -u postgres psql -c "CREATE DATABASE api WITH owner api;" || echo "PostgreSQL database already exists"
-sudo -u postgres psql -c "ALTER ROLE api WITH superuser;"
-sudo -u postgres psql -c "ALTER ROLE api WITH login;"
-sudo -u postgres psql -c "CREATE EXTENSION postgis;" || echo "Postgis extension already installed"
 
 echo "## Migrate and populate test database..."
 (cd /vagrant && $POETRY_HOME/bin/poetry run ./manage.py migrate && ($POETRY_HOME/bin/poetry run ./manage.py load_fake_data || true)) &> /dev/null
@@ -190,7 +190,7 @@ sudo systemctl start webpack
 
 echo "## Installing manage script"
 sudo bash -c "cat > /usr/local/bin/manage" <<'EOT'
-PYTHON=$(cd /vagrant && $POETRY_HOME/bin/poetry env list --full-path)/bin/python
+PYTHON=$(cd /vagrant && $POETRY_HOME/bin/poetry env info --path)/bin/python
 
 $PYTHON /vagrant/manage.py "$@"
 EOT
