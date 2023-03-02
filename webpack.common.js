7f1ed8158046631b0350914fb4a98ecb83320d13
Giuseppe De Ponte <giuseppedeponte@gmail.com>
2023-03-02 12:26:26 +0100

    fix: update webpack asset cache key

--- webpack.common.js
+++ webpack.common.js
@@ -322,7 +322,7 @@ module.exports = (type = CONFIG_TYPES.ES5) => ({
     library: ["Agir", "[name]"],
     filename: `[name]-[chunkhash].${
       type === CONFIG_TYPES.ES2015 ? "mjs" : "js"
-    }?cv=5`,
+    }?cv=6`,
     path: DISTPATH,
   },
   module: {
