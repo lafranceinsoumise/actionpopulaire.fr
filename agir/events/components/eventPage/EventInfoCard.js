edf4b09c08adabddb5407dc3ddd173c67d43ab4c
Giuseppe De Ponte <giuseppedeponte@gmail.com>
2023-03-06 16:26:36 +0100

    feat: event-page => link to same group/type event creation

--- agir/events/components/eventPage/EventInfoCard.js
+++ agir/events/components/eventPage/EventInfoCard.js
@@ -10,15 +10,30 @@ import {
 } from "@agir/front/genericComponents/FeatherIcon";
 
 import Link from "@agir/front/app/Link";
+import Button from "@agir/front/genericComponents/Button";
+import FaIcon from "@agir/front/genericComponents/FaIcon";
+import Spacer from "@agir/front/genericComponents/Spacer";
 
-const EventInfoCard = ({ groups, participantCount, subtype }) => (
+const EventInfoCard = ({ groups, subtype, isManager }) => (
   <Card>
     <IconList>
-      {groups.length > 0 && (
+      {subtype.description && (
+        <IconListItem>
+          <FaIcon
+            style={{ color: subtype.color }}
+            icon={subtype.iconName || "folder"}
+          />
+          <span>
+            {subtype.description[0].toUpperCase()}
+            {subtype.description.slice(1)}
+          </span>
+        </IconListItem>
+      )}
+      {groups.length > 1 && (
         <IconListItem name="users">
-          Organisé par{" "}
+          Organisé par&nbsp;:
           {groups.map(({ name, id }, key) => (
-            <React.Fragment key={key}>
+            <div key={name + id}>
               {id ? (
                 <Link to={routeConfig.groupDetails.getLink({ groupPk: id })}>
                   {name}
@@ -26,38 +41,62 @@ const EventInfoCard = ({ groups, participantCount, subtype }) => (
               ) : (
                 name
               )}
-              {key < groups.length - 2
-                ? ", "
-                : key === groups.length - 2
-                ? " et "
-                : ""}
+              {groups[key + 1] && ","}
+            </div>
+          ))}
+        </IconListItem>
+      )}
+      {groups.length === 1 && (
+        <IconListItem name="users">
+          Organisé par{" "}
+          {groups.map(({ name, id }) => (
+            <React.Fragment key={name + id}>
+              {id ? (
+                <Link to={routeConfig.groupDetails.getLink({ groupPk: id })}>
+                  {name}
+                </Link>
+              ) : (
+                name
+              )}
             </React.Fragment>
           ))}
         </IconListItem>
       )}
-      {subtype?.description && (
-        <IconListItem name="folder">
-          {subtype.description[0].toUpperCase()}
-          {subtype.description.slice(1)}
-        </IconListItem>
-      )}
-      {participantCount > 1 && (
-        <IconListItem name="user-check">
-          {participantCount} participant⋅es
-        </IconListItem>
-      )}
     </IconList>
+    <Spacer size="1rem" />
+    {isManager && (
+      <Button
+        icon="plus"
+        small
+        link
+        route="createEvent"
+        params={{
+          group: groups[0]?.id,
+          subtype: subtype.label,
+        }}
+        color="primary"
+      >
+        Créer un événement similaire
+      </Button>
+    )}
   </Card>
 );
 
 EventInfoCard.propTypes = {
   groups: PropTypes.arrayOf(
-    PropTypes.shape({ name: PropTypes.string, url: PropTypes.string })
+    PropTypes.shape({
+      id: PropTypes.string,
+      name: PropTypes.string,
+      url: PropTypes.string,
+    })
   ),
   subtype: PropTypes.shape({
+    label: PropTypes.string,
     description: PropTypes.string,
+    iconName: PropTypes.string,
+    color: PropTypes.string,
   }),
-  participantCount: PropTypes.number,
+  isManager: PropTypes.bool,
 };
 
 export default EventInfoCard;
