0591bd0f9d708f7d3adc4a9917426611d43e0244
Giuseppe De Ponte <giuseppedeponte@gmail.com>
2023-04-18 18:17:12 +0200

    chore: update Storybook to v7

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,36 +1,37 @@
 const webpackConfig = require("../webpack.common");
-
 const baseConfig = webpackConfig(webpackConfig.CONFIG_TYPES.DEV);
-
 module.exports = {
-  core: {
-    builder: "webpack5",
-  },
   stories: ["../agir/*/components/**/*.stories.js"],
   addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
-  webpackFinal: (config) => {
+  webpackFinal: config => {
     config.module.rules.push({
       test: /\.scss$/,
       exclude: [/theme\/theme.scss/],
-      use: [
-        "style-loader",
-        {
-          loader: "css-loader",
-          options: {
-            modules: { mode: "icss", auto: /\.scss$/i },
-          },
-        },
-        "sass-loader",
-      ],
+      use: ["style-loader", {
+        loader: "css-loader",
+        options: {
+          modules: {
+            mode: "icss",
+            auto: /\.scss$/i
+          }
+        }
+      }, "sass-loader"]
     });
-
     config.resolve = {
       ...config.resolve,
-      alias: { ...config.resolve.alias, ...baseConfig.resolve.alias },
+      alias: {
+        ...config.resolve.alias,
+        ...baseConfig.resolve.alias
+      }
     };
-
     config.devtool = "eval";
-
     return config;
   },
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {}
+  },
+  docs: {
+    autodocs: true
+  }
 };
