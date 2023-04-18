077d4a161a96b52aa552e9f4c989e6593e7bec91
Giuseppe De Ponte <giuseppedeponte@gmail.com>
2023-04-18 09:45:12 +0200

    fix: EventSuggestion ActionRadiusField layout

--- agir/events/components/agendaPage/ActionRadiusField.js
+++ agir/events/components/agendaPage/ActionRadiusField.js
@@ -4,43 +4,35 @@ import styled from "styled-components";
 
 import Link from "@agir/front/app/Link";
 import RangeField from "@agir/front/formComponents/RangeField";
-import FeatherIcon from "@agir/front/genericComponents/FeatherIcon";
 import PageFadeIn from "@agir/front/genericComponents/PageFadeIn";
 
-const StyledLink = styled(Link)``;
 const StyledRangeField = styled(RangeField)``;
 const StyledField = styled.div`
   margin: 1.5rem 0;
   position: relative;
-
-  ${StyledLink} {
-    position: absolute;
-    top: 0;
-    right: 0;
-    font-size: 0.875rem;
-    font-weight: 600;
-  }
 `;
 
 const ActionRadiusField = (props) => {
-  const { value, onChange, disabled } = props;
+  const { value = null, onChange, disabled } = props;
 
   return (
-    <PageFadeIn ready={!!value}>
+    <PageFadeIn ready={value !== null}>
       <StyledField>
         <StyledRangeField
           value={value}
           onChange={onChange}
           disabled={disabled}
           label="Proposer des événements dans un rayon de :"
+          helpText={
+            <p style={{ margin: "0.25rem 0 0", textAlign: "right" }}>
+              Un doute sur la localisation utilisée&nbsp;?&ensp;
+              <Link route="personalInformation">Vérifiez ici</Link>
+            </p>
+          }
           min={1}
           max={500}
           step={1}
         />
-        <StyledLink route="personalInformation">
-          Changer ma localisation{" "}
-          <FeatherIcon name="external-link" small inline />
-        </StyledLink>
       </StyledField>
     </PageFadeIn>
   );
