0612700512ae9ec36dc5f635d6663ffce5cde27d
Giuseppe De Ponte <giuseppedeponte@gmail.com>
2023-06-21 16:52:12 +0200

    feat: update person form json schema

--- agir/lib/components/adminJsonWidget/index.js
+++ agir/lib/components/adminJsonWidget/index.js
@@ -1,7 +1,28 @@
 import JsonEditor from "jsoneditor";
 
 import "jsoneditor/dist/jsoneditor.css";
+
 import onDOMReady from "@agir/lib/utils/onDOMReady";
+
+const DEFAULT_TEMPLATES = [
+  {
+    text: "Groupe de champs",
+    title: "Insérer un groupe de champs",
+    value: {
+      title: "Mon groupe de champs",
+      fields: [],
+    },
+  },
+  {
+    text: "Champ",
+    title: "Insérer un champ",
+    value: {
+      id: "identifiant_du_champ",
+      label: "Label du champ",
+      type: "short_text",
+    },
+  },
+];
 
 const loaded = [];
 
@@ -11,12 +32,13 @@ const initAdminJsonWidget = () => {
   }
 
   document.querySelectorAll(".jsoneditordiv").forEach((e) => {
-    console.log("Boom");
-
     if (loaded.includes(e.dataset.fieldname)) return;
 
-    let schema = "";
+    let schema;
+    let templates;
     let data = "";
+
+    e.closest(".form-row").style.overflow = "visible";
 
     if (document.getElementById(e.dataset.fieldname + "-schema")) {
       try {
@@ -27,6 +49,18 @@ const initAdminJsonWidget = () => {
       } catch (e) {
         console.log(e);
         schema = undefined;
+      }
+    }
+
+    if (document.getElementById(e.dataset.fieldname + "-templates")) {
+      try {
+        templates = document.getElementById(
+          e.dataset.fieldname + "-templates"
+        ).textContent;
+        templates = JSON.parse(templates);
+      } catch (e) {
+        console.log(e);
+        templates = undefined;
       }
     }
 
@@ -42,35 +76,20 @@ const initAdminJsonWidget = () => {
     }
 
     const editor = new JsonEditor(e, {
+      language: "fr-FR",
       onChangeText: (content) => {
         document.getElementById(`id_${e.dataset.fieldname}`).value = content;
       },
-      templates: [
-        {
-          text: "Groupe de champs",
-          title: "Insérer un groupe de champs",
-          value: {
-            title: "Mon groupe de champs",
-            fields: [],
-          },
-        },
-        {
-          text: "Champ",
-          title: "Insérer un champ",
-          value: {
-            id: "identifiant_du_champ",
-            label: "Label du champ",
-            type: "short_text",
-          },
-        },
-      ],
+      templates: templates || DEFAULT_TEMPLATES,
       mode: typeof data === "string" ? "code" : "tree",
       modes: ["tree", "code", "preview"],
       schema: schema,
+      schemaRefs: {},
+      allowSchemaSuggestions: true,
     });
 
     typeof data === "string" ? editor.setText(data) : editor.set(data);
-    typeof data !== "string" && editor.expandAll();
+    editor.expandAll && editor.expandAll();
     loaded.push(e.dataset.fieldname);
 
     window.AgirAdminJsonWidgetEditor = editor;
--- agir/people/person_forms/schema.py
+++ agir/people/person_forms/schema.py
@@ -1,60 +1,399 @@
+import django_countries.data
+from data_france.models import Commune
+
+from agir.people.models import Person
 from agir.people.person_forms import fields
+from agir.people.person_forms.fields import PREDEFINED_CHOICES
+
+uuid = {
+    "type": "string",
+    "pattern": "^[0-9a-f]{8}-?[0-9a-f]{4}-?[0-9a-f]{4}-?[0-9a-f]{4}-?[0-9a-f]{12}$",
+    "minLength": 32,
+}
+
+
+choices = {
+    "predefined": {"type": "string", "enum": list(PREDEFINED_CHOICES.keys())},
+    "values": {"type": "array", "items": {"type": ["string", "number"]}},
+    "labels": {
+        "type": "array",
+        "maxItems": 2,
+        "minItems": 2,
+        "items": {"type": ["string", "number"]},
+    },
+    "categories": {
+        "type": "array",
+        "items": {
+            "oneOf": [
+                {"type": "string"},
+                {
+                    "type": "array",
+                    "maxItems": 2,
+                    "minItems": 2,
+                    "items": {"type": ["string", "number"]},
+                },
+            ]
+        },
+    },
+}
+
+field_commons = {
+    "properties": {
+        "id": {
+            "description": "L'identifiant unique du champ",
+            "type": "string",
+        },
+        "label": {
+            "description": "Le libellé du champ",
+            "type": "string",
+        },
+        "person_field": {
+            "description": "Le champ doit-il être enregistré sur la personne",
+            "type": "boolean",
+            "const": True,
+        },
+        "type": {
+            "description": "Le type de champ",
+            "type": "string",
+            "enum": list(fields.FIELDS.keys()),
+        },
+        "required": {
+            "description": "Le champ est obligatoire",
+            "type": "boolean",
+        },
+        "disabled": {
+            "description": "Le champ est en lecture-seule",
+            "type": "boolean",
+        },
+        "initial": {
+            "description": "La valeur initiale du champ",
+            "type": [
+                "number",
+                "string",
+                "boolean",
+                "object",
+                "array",
+                "null",
+            ],
+        },
+    }
+}
+
+field_types = [
+    {
+        "properties": {
+            "type": {
+                "const": "short_text",
+            },
+            "initial": {"type": "string"},
+            "min_length": {
+                "type": "integer",
+            },
+            "max_length": {
+                "type": "integer",
+            },
+            "strip": {
+                "type": "boolean",
+            },
+        },
+    },
+    {
+        "properties": {
+            "type": {
+                "const": "long_text",
+            },
+            "initial": {"type": "string"},
+            "min_length": {"type": "integer"},
+            "max_length": {"type": "integer"},
+            "strip": {"type": "boolean"},
+        },
+    },
+    {
+        "properties": {
+            "type": {
+                "const": "choice",
+            },
+            "initial": {"type": ["string", "number"]},
+            "default_label": {"type": "string"},
+            "empty_value": {"type": "boolean"},
+            "choices": {"oneOf": list(choices.values())},
+        },
+        "required": ["choices"],
+    },
+    {
+        "properties": {
+            "type": {
+                "const": "radio_choice",
+            },
+            "initial": {"type": ["string", "number"]},
+            "choices": {
+                "oneOf": [
+                    choices["predefined"],
+                    choices["values"],
+                    choices["labels"],
+                ]
+            },
+        },
+        "required": ["choices"],
+    },
+    {
+        "properties": {
+            "type": {
+                "const": "autocomplete_choice",
+            },
+            "initial": {"type": ["string", "number"]},
+            "default_label": {"type": "string"},
+            "empty_value": {"type": "boolean"},
+            "choices": {"oneOf": list(choices.values())},
+        },
+        "required": ["choices"],
+    },
+    {
+        "properties": {
+            "type": {
+                "const": "autocomplete_multiple_choice",
+            },
+            "initial": {"type": ["string", "number"]},
+            "max_items": {"type": "integer"},
+            "choices": {"oneOf": list(choices.values())},
+        },
+        "required": ["choices"],
+    },
+    {
+        "properties": {
+            "type": {
+                "const": "multiple_choice",
+            },
+            "initial": {"type": ["string", "number"]},
+            "choices": {
+                "oneOf": [
+                    choices["predefined"],
+                    choices["values"],
+                    choices["labels"],
+                ],
+            },
+        },
+        "required": ["choices"],
+    },
+    {
+        "properties": {
+            "type": {"const": "email_address"},
+            "initial": {"type": "string"},
+        }
+    },
+    {
+        "properties": {
+            "type": {
+                "const": "phone_number",
+            },
+            "initial": {"type": "string"},
+            "region": {"type": "string"},
+        }
+    },
+    {
+        "properties": {
+            "type": {
+                "const": "url",
+            },
+            "initial": {"type": "string"},
+        }
+    },
+    {
+        "properties": {
+            "type": {
+                "const": "file",
+            },
+            "max_size": {"type": "integer"},
+            "allowed_extensions": {"type": "array", "items": {"type": "string"}},
+            "multiple": {"type": "boolean"},
+        }
+    },
+    {
+        "properties": {
+            "type": {
+                "const": "boolean",
+            },
+            "initial": {"type": "boolean"},
+        }
+    },
+    {
+        "properties": {
+            "type": {"const": "integer"},
+            "initial": {"type": "integer"},
+            "min_value": {"type": "integer"},
+            "max_value": {"type": "integer"},
+        }
+    },
+    {
+        "properties": {
+            "type": {"const": "decimal"},
+            "initial": {"type": "number"},
+            "min_value": {"type": "number"},
+            "max_value": {"type": "number"},
+            "max_digits": {"type": "integer"},
+            "decimal_places": {"type": "integer"},
+        }
+    },
+    {
+        "properties": {
+            "type": {"const": "datetime"},
+            "initial": {"type": "string"},
+        }
+    },
+    {
+        "properties": {
+            "type": {"const": "date"},
+            "initial": {"type": "string"},
+            "min_value": {"type": "string"},
+            "max_value": {"type": "string"},
+        }
+    },
+    {
+        "properties": {
+            "type": {"const": "dates"},
+            "initial": {"type": "string"},
+            "min_value": {"type": "string"},
+            "max_value": {"type": "string"},
+            "min_length": {"type": "integer"},
+            "max_length": {"type": "integer"},
+            "min_delta": {"type": "integer"},
+            "max_delta": {"type": "integer"},
+        }
+    },
+    {
+        "properties": {
+            "type": {"const": "datetimes"},
+            "initial": {"type": "string"},
+            "min_value": {"type": "string"},
+            "max_value": {"type": "string"},
+            "min_length": {"type": "integer"},
+            "max_length": {"type": "integer"},
+            "min_delta": {"type": "integer"},
+            "max_delta": {"type": "integer"},
+        }
+    },
+    {
+        "properties": {
+            "type": {"const": "person"},
+            "initial": uuid,
+            "allow_self": {"type": "boolean"},
+            "allow_inactive": {"type": "boolean"},
+        }
+    },
+    {
+        "properties": {
+            "type": {"const": "iban"},
+            "initial": {"type": "string"},
+            "allowed_countries": {
+                "type": "array",
+                "items": {
+                    "type": "string",
+                    "enum": list(django_countries.data.COUNTRIES.keys()),
+                },
+            },
+        }
+    },
+    {
+        "properties": {
+            "type": {"const": "commune"},
+            "initial": {"type": "integer"},
+            "types": {
+                "type": "array",
+                "items": {
+                    "type": "string",
+                    "enum": list(Commune.TypeCommune.values),
+                },
+            },
+        }
+    },
+    {
+        "properties": {
+            "type": {"enum": ["group", "multiple_groups"]},
+            "initial": uuid,
+            "choices": {
+                "oneOf": [
+                    {
+                        "type": "string",
+                        "enum": [
+                            "animateur",
+                            "animatrice",
+                            "animator",
+                            "referent",
+                            "manager",
+                            "gestionnaire",
+                            "membre",
+                            "member",
+                        ],
+                    },
+                    {
+                        "type": "array",
+                        "items": uuid,
+                    },
+                ]
+            },
+        },
+        "required": ["choices"],
+    },
+    {
+        "properties": {
+            "type": {"const": "newsletters"},
+            "initial": {
+                "type": "array",
+                "items": {
+                    "type": "string",
+                    "enum": [n[0] for n in Person.NEWSLETTERS_CHOICES],
+                },
+            },
+            "choices": {
+                "type": "array",
+                "items": {
+                    "type": "string",
+                    "enum": [n[0] for n in Person.NEWSLETTERS_CHOICES],
+                },
+            },
+        },
+        "required": ["choices"],
+    },
+    {
+        "properties": {
+            "type": {"const": "event_theme"},
+            "initial": uuid,
+            "event_theme_type": {"type": ["string", "number"]},
+        },
+    },
+    {
+        "properties": {
+            "type": {"const": "person_tag"},
+            "initial": uuid,
+            "choices": {"type": "array", "items": {"type": ["string", "number"]}},
+            "multiple": {"type": "boolean"},
+        },
+        "required": ["choices"],
+    },
+]
+
+field = {
+    "type": "object",
+    "allOf": [field_commons, {"oneOf": field_types}],
+}
 
 schema = {
+    "title": "Person Form",
+    "description": "A custom person form definition",
     "type": "array",
     "items": {
         "type": "object",
         "properties": {
-            "title": {"description": "Le titre du groupe de champs", "type": "string"},
+            "title": {"description": "Le titre du groupe de champ", "type": "string"},
             "intro_html": {
                 "description": "Texte introductif au format HTML à afficher sous le titre",
                 "type": "string",
             },
             "fields": {
-                "description": "La liste des champs",
+                "description": "La liste des champ",
                 "type": "array",
                 "minItems": 1,
-                "items": {
-                    "type": "object",
-                    "properties": {
-                        "id": {
-                            "description": "L'identifiant court du champ",
-                            "type": "string",
-                        },
-                        "person_field": {
-                            "description": "Le champ doit-il être enregistré sur la personne",
-                            "type": "boolean",
-                            "const": True,
-                        },
-                        "type": {
-                            "description": "Le type du champ",
-                            "type": "string",
-                            "enum": list(fields.FIELDS.keys()),
-                        },
-                        "choices": {
-                            "description": "La liste des choix",
-                            "anyOf": [
-                                {
-                                    "type": "array",
-                                    "items": {
-                                        "anyOf": [
-                                            {
-                                                "type": "array",
-                                                "maxItems": 2,
-                                                "minItems": 2,
-                                                "items": {"type": "string"},
-                                            },
-                                            {"type": "string"},
-                                        ]
-                                    },
-                                },
-                                {"type": "string"},
-                                {"type": "number"},
-                            ],
-                        },
-                    },
-                    "required": ["id"],
-                },
+                "items": field,
             },
         },
         "required": ["title", "fields"],
