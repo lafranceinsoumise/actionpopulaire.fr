e2786955ee5565e0773390ce30648082ee12e89a
Giuseppe De Ponte <giuseppedeponte@gmail.com>
2023-07-20 16:55:20 +0200

    feat: create spending request TypeRadioField component

---
+++ agir/donations/components/spendingRequest/common/TypeRadioField.stories.js
@@ -0,0 +1,62 @@
+import React from "react";
+
+import TypeRadioField from "./TypeRadioField";
+import { TYPE_OPTIONS } from "./config";
+
+export default {
+  component: TypeRadioField,
+  title: "Donations/SpendingRequest/TypeRadioField",
+  argTypes: {
+    onChange: { table: { disable: true } },
+  },
+  parameters: {
+    layout: "padded",
+  },
+};
+
+const Template = (args) => {
+  const [value, setValue] = React.useState(args.value);
+  const handleChange = React.useCallback((value) => {
+    setValue(value);
+  }, []);
+
+  return <TypeRadioField {...args} value={value} onChange={handleChange} />;
+};
+
+export const Default = Template.bind({});
+Default.args = {
+  value: false,
+  id: "type",
+  label: "Catégorie de dépense",
+  disabled: false,
+};
+
+export const Unchecked = Template.bind({});
+Unchecked.args = {
+  ...Default.args,
+  value: undefined,
+};
+
+export const Checked = Template.bind({});
+Checked.args = {
+  ...Default.args,
+  value: Object.values(TYPE_OPTIONS)[3].value,
+};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+  ...Default.args,
+  disabled: true,
+};
+
+export const DisabledChecked = Template.bind({});
+DisabledChecked.args = {
+  ...Checked.args,
+  disabled: true,
+};
+
+export const WithError = Template.bind({});
+WithError.args = {
+  ...Checked.args,
+  error: "Veuillez choisir une catégorie",
+};
---
+++ agir/donations/components/spendingRequest/common/config.js
@@ -0,0 +1,47 @@
+export const TYPE_OPTIONS = {
+  IMPRESSIONS: {
+    value: "IM",
+    label: "Impressions",
+    icon: "printer",
+  },
+  CONSOMMABLES: {
+    value: "CO",
+    label: "Achat de consommables (colles, feutres, … )",
+    icon: "pen-tool",
+  },
+  ACHAT: {
+    value: "AC",
+    label: "Achat de matériel (quincaillerie, matériel de collage, … )",
+    icon: "package",
+  },
+  DEPLACEMENT: {
+    value: "DE",
+    label: "Déplacement",
+    icon: "map",
+  },
+  HEBERGEMENT: {
+    value: "HE",
+    label: "Hébergement",
+    icon: "home",
+  },
+  SALLE: {
+    value: "SA",
+    label: "Location de salle",
+    icon: "map-pin",
+  },
+  LOCATION: {
+    value: "MA",
+    label: "Location de matériel (mobilier, vaisselle, … )",
+    icon: "shopping-bag",
+  },
+  TECHNIQUE: {
+    value: "TE",
+    label: "Location de matériel technique (sono, vidéo)",
+    icon: "video",
+  },
+  VEHICULE: {
+    value: "VE",
+    label: "Location de véhicule",
+    icon: "truck",
+  },
+};
