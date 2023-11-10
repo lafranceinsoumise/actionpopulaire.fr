8a6dfe0d4e569be0e4ba712ef230ad7cfc86c177
Giuseppe De Ponte <giuseppedeponte@gmail.com>
2023-11-10 14:22:32 +0100

    fix: statistics create / update from date actions

--- agir/statistics/actions.py
+++ agir/statistics/actions.py
@@ -7,12 +7,12 @@ from tqdm import tqdm
 from agir.statistics.models import (
     AbsoluteStatistics,
     MaterielStatistics,
-    # CommuneStatistics,
+    CommuneStatistics,
 )
 from agir.statistics.utils import (
     get_absolute_statistics,
     get_materiel_statistics,
-    # get_commune_statistics,
+    get_commune_statistics,
 )
 
 
@@ -51,7 +51,9 @@ def get_largest_campaign_statistics(start, end):
     )
 
 
-def create_statistics_from_date(date=None, silent=False):
+def create_statistics_from_date(
+    date=None, silent=False, absolute=True, materiel=True, commune=True
+):
     today = datetime.date.today()
     if date is None:
         # defaults to current year start
@@ -67,20 +69,23 @@ def create_statistics_from_date(date=None, silent=False):
     while date < today:
         progress.set_description_str(str(date))
 
-        # Create AbsoluteStatistics
-        abs_kwargs = get_absolute_statistics(date=date, as_kwargs=True)
-        AbsoluteStatistics.objects.update_or_create(
-            date=abs_kwargs.pop("date"), defaults=abs_kwargs
-        )
+        if absolute:
+            # Create AbsoluteStatistics
+            abs_kwargs = get_absolute_statistics(date=date, as_kwargs=True)
+            AbsoluteStatistics.objects.update_or_create(
+                date=abs_kwargs.pop("date"), defaults=abs_kwargs
+            )
 
-        # Create MaterielStatistics
-        mat_kwargs = get_materiel_statistics(date=date)
-        MaterielStatistics.objects.update_or_create(
-            date=mat_kwargs.pop("date"), defaults=mat_kwargs
-        )
+        if materiel:
+            # Create MaterielStatistics
+            mat_kwargs = get_materiel_statistics(date=date)
+            MaterielStatistics.objects.update_or_create(
+                date=mat_kwargs.pop("date"), defaults=mat_kwargs
+            )
 
-        # Create CommuneStatistics
-        CommuneStatistics.objects.update_or_create_all_for_date(date=date)
+        if commune:
+            # Create CommuneStatistics
+            CommuneStatistics.objects.update_or_create_all_for_date(date=date)
 
         date += datetime.timedelta(days=1)
         progress.update(1)
@@ -88,7 +93,9 @@ def create_statistics_from_date(date=None, silent=False):
     progress.clear()
 
 
-def update_statistics_from_date(date=None, silent=False, columns=None):
+def update_statistics_from_date(
+    date=None, silent=False, columns=None, absolute=True, materiel=True, commune=True
+):
     today = datetime.date.today()
     if date is None:
         # defaults to current year start
@@ -104,24 +111,29 @@ def update_statistics_from_date(date=None, silent=False, columns=None):
     while date < today:
         progress.set_description_str(str(date))
 
-        # Update AbsoluteStatistics
-        abs_kwargs = get_absolute_statistics(date=date, as_kwargs=True, columns=columns)
-        if abs_kwargs:
-            AbsoluteStatistics.objects.filter(date=date).update(**abs_kwargs)
-
-        # Update MaterielStatistics
-        mat_kwargs = get_materiel_statistics(date=date, columns=columns)
-        if mat_kwargs:
-            MaterielStatistics.objects.filter(date=date).update(**mat_kwargs)
-
-        # Update CommuneStatistics
-        commune_kwargs = get_commune_statistics(
-            date=date, columns=columns, as_kwargs=True
-        )
-        if commune_kwargs:
-            CommuneStatistics.objects.update_all_for_date(
-                date=date, defaults=commune_kwargs
+        if absolute:
+            # Update AbsoluteStatistics
+            abs_kwargs = get_absolute_statistics(
+                date=date, as_kwargs=True, columns=columns
             )
+            if abs_kwargs:
+                AbsoluteStatistics.objects.filter(date=date).update(**abs_kwargs)
+
+        if materiel:
+            # Update MaterielStatistics
+            mat_kwargs = get_materiel_statistics(date=date, columns=columns)
+            if mat_kwargs:
+                MaterielStatistics.objects.filter(date=date).update(**mat_kwargs)
+
+        if commune:
+            # Update CommuneStatistics
+            commune_kwargs = get_commune_statistics(
+                date=date, columns=columns, as_kwargs=True
+            )
+            if commune_kwargs:
+                CommuneStatistics.objects.update_all_for_date(
+                    date=date, defaults=commune_kwargs
+                )
 
         date += datetime.timedelta(days=1)
         progress.update(1)
