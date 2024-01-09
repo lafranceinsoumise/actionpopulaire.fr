699ebf542884a7a437eacf0409ec0cacf56ec6a6
Salomé Cheysson <salome@cheysson.fr>
2024-01-09 16:59:27 +0100

    Put celery task priority behind a feature flag
    use env var CELERY_ENABLE_PRIORITY to enable priority handling.
    
    This reverts commit ff443c3dfba7b6809c13c2f492f5e96822fbcca5.

--- agir/api/settings.py
+++ agir/api/settings.py
@@ -751,7 +751,25 @@ CRISPY_TEMPLATE_PACK = "bootstrap3"
 # CELERY
 CELERY_BROKER_URL = os.environ.get("CELERY_BROKER_URL", "redis://")
 # make sure there is a max_retries option
-CELERY_BROKER_TRANSPORT_OPTIONS = {"max_retries": 4}
+CELERY_BROKER_TRANSPORT_OPTIONS = {
+    "max_retries": 4,
+}
+if os.environ.get("CELERY_ENABLE_PRIORITY"):
+    CELERY_BROKER_TRANSPORT_OPTIONS.update(
+        {
+            "priority_steps": list(range(3)),
+            "sep": ":",
+            "queue_order_strategy": "priority",
+        }
+    )
+
+
+# configure priorities
+CELERY_TASK_PRIORITY_HIGH = 0
+CELERY_TASK_PRIORITY_NORMAL = 1
+CELERY_TASK_PRIORITY_LOW = 2
+CELERY_TASK_DEFAULT_PRIORITY = CELERY_TASK_PRIORITY_NORMAL
+
 # make sure celery does not mess with the root logger
 CELERY_WORKER_HIJACK_ROOT_LOGGER = DEBUG
 # enable worker events to allow monitoring
--- agir/authentication/tasks.py
+++ agir/authentication/tasks.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 from django.utils.http import urlencode
 
 from agir.authentication.tokens import subscription_confirmation_token_generator
-from agir.lib.celery import emailing_task
+from agir.lib.celery import emailing_task, TASK_PRIORITY_HIGH
 from agir.lib.mailing import send_mosaico_email, send_template_email
 from agir.lib.utils import front_url
 from agir.people.actions.subscription import SUBSCRIPTION_TYPE_AP
@@ -13,7 +13,7 @@ def interleave_spaces(s, n=3):
     return " ".join([s[i : i + n] for i in range(0, len(s), n)])
 
 
-@emailing_task()
+@emailing_task(priority=TASK_PRIORITY_HIGH)
 def send_login_email(email, short_code, expiry_time):
     utc_expiry_time = timezone.make_aware(
         timezone.datetime.utcfromtimestamp(expiry_time), timezone.utc
@@ -32,7 +32,7 @@ def send_login_email(email, short_code, expiry_time):
     )
 
 
-@emailing_task()
+@emailing_task(priority=TASK_PRIORITY_HIGH)
 def send_no_account_email(email, subscription_type=SUBSCRIPTION_TYPE_AP, **kwargs):
     subscription_token = subscription_confirmation_token_generator.make_token(
         email=email, type=subscription_type, **kwargs
