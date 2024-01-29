c6e424e7b28f3847d165622ae8660c3d6dab5001
Benjamin Lemaire <benjamin.lemaire@franceinsoumise.org>
2024-01-29 16:17:38 +0100

    reformat metrics files

--- agir/mailing/metrics.py
+++ agir/mailing/metrics.py
@@ -1,4 +1,4 @@
 from prometheus_client import Counter
 
-ses_bounced_metric = Counter('ses_bounced_metric', '')
-sendgrid_bounced_metric = Counter('sendgrid_bounced_metric', '')
+ses_bounced_metric = Counter("ses_bounced_metric", "")
+sendgrid_bounced_metric = Counter("sendgrid_bounced_metric", "")
