19743a0a8437079d93597649a480eeabbbd2f65a
Giuseppe De Ponte <giuseppedeponte@gmail.com>
2024-02-16 12:50:27 +0100

    feat: grant TokTok data access to all group active members

--- agir/presidentielle2022/rules.py
+++ agir/presidentielle2022/rules.py
@@ -27,6 +27,21 @@ def is_certified_group_manager(role, person):
     )
 
 
-rules.add_perm(
-    "toktok.access_data", is_authenticated_person & is_certified_group_manager
-)
+@rules.predicate
+def is_group_active_member(role, person):
+    if hasattr(person, "groups_with_active_membership"):
+        return any(
+            group.membership_type >= Membership.MEMBERSHIP_TYPE_MEMBER
+            for group in person.groups_with_active_membership
+        )
+
+    return (
+        SupportGroup.objects.filter(memberships__person_id=person.id)
+        .active()
+        .annotate(membership_type=F("memberships__membership_type"))
+        .filter(membership_type__gte=Membership.MEMBERSHIP_TYPE_MEMBER)
+        .exists()
+    )
+
+
+rules.add_perm("toktok.access_data", is_authenticated_person & is_group_active_member)
