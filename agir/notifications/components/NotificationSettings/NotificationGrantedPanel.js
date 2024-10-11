db345298c37eebb041eea37e7e210d0ecba7f7ca
Alexandra <bug.graph@gmail.com>
2024-10-11 18:20:15 +0200

    fix: notification response data

--- agir/notifications/components/NotificationSettings/NotificationGrantedPanel.js
+++ agir/notifications/components/NotificationSettings/NotificationGrantedPanel.js
@@ -1,8 +1,7 @@
-import React, {useEffect, useState} from "react";
+import React, {useState} from "react";
 import {useMobileApp, useNotificationGrant} from "../../../front/components/app/hooks";
 import NotificationRationaleModal from "../../../events/components/agendaPage/NotificationRationaleModal";
 import ActionCard from "../../../front/components/genericComponents/ActionCard";
-import {useError} from "react-use";
 
 export default function NotificationGrantedPanel() {
     const {isMobileApp} = useMobileApp();
